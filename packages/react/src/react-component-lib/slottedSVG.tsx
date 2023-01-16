053c027ffd2e14d3b1b899f1a3b3961c7289e599
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2023-01-16 09:21:42 +0000

    feat(react): add slotted svg component to react library
    Add slotted svg component to react library in order to remove remove error that appears in
    typescript applications.
    
    . #280

---
+++ packages/react/src/react-component-lib/slottedSVG.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+const defaultProps = {
+  xmlns: "http://www.w3.org/2000/svg",
+};
+
+function slot(name = "") {
+  return { ref: (e: any) => (e ? e.setAttribute("slot", name) : null) };
+}
+
+export const SlottedSVG: FC<any> = ({ props, path, slot: slotName }) => (
+  <svg {...slot(slotName)} {...props} {...defaultProps}>
+    <path d={path} />
+  </svg>
+);
