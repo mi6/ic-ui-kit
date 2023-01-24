754888531d85ca290ee504f34c37d8ae5ef0a57f
gd2910 <122624946+gd2910@users.noreply.github.com>
2023-01-24 10:50:36 +0000

    feat(web-components): announced prop added to ic-status-tag
    When the status-tag component is announced it will act as an aria-live region and have an aria-label
    attached. Unit tests and stories added
    
    feature #20

--- packages/web-components/src/components/ic-status-tag/ic-status-tag.test.a11y.ts
+++ packages/web-components/src/components/ic-status-tag/ic-status-tag.test.a11y.ts
@@ -4,7 +4,7 @@ import { checkShadowElementRendersCorrectly } from "../../utils/testa11y.helpers
 
 describe("ic-status-tag", () => {
   it("passes accessibility", async () => {
-    const el = await fixture("<ic-status-tag />");
+    const el = await fixture("<ic-status-tag label='Neutral' />");
     checkShadowElementRendersCorrectly(el);
     expect(await axe(el)).toHaveNoViolations();
   });
