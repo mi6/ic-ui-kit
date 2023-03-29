7245f73741df75720ddb5722e9a836337e4c311f
jd - Legend <116016939+jd239@users.noreply.github.com>
2023-03-29 16:11:08 +0000

    fix(web-components): moved slotted navigation method to set menu items to visible on expanded
    Passed through menu expanded state so menu items will appear if menu is expanded and slotted menu items are set

--- packages/web-components/src/components/ic-side-navigation/ic-side-navigation-test-examples.ts
+++ packages/web-components/src/components/ic-side-navigation/ic-side-navigation-test-examples.ts
@@ -322,3 +322,94 @@ export const icSideNavigationTheme = `
       <footer>Footer</footer>
     </div>
   </div>`;
+
+export const icSideNavigationSlottedExpanded = `
+    <ic-side-navigation app-title="Application Name" version="v0.0.0" status="BETA" expanded="true">
+      <svg
+        slot="app-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z"
+        />
+      </svg>
+      <ic-navigation-item slot="primary-navigation">
+        <a
+          slot="navigation-item"
+          class="active"
+          href="/child-item-2"
+          aria-label="Daily Tippers"
+        >
+          <svg
+            slot="icon"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 6.19L17 10.69V18.5H15V12.5H9V18.5H7V10.69L12 6.19ZM12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L12 3.5Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          Daily Tippers
+        </a>
+      </ic-navigation-item>
+      <ic-navigation-group
+        slot="primary-navigation"
+        label="Second navigation group"
+        expandable="true"
+      >
+        <ic-navigation-item>
+          <a
+            slot="navigation-item"
+            href="/child-item-2"
+            aria-label="Daily Tippers"
+          >
+            <svg
+              slot="icon"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 6.19L17 10.69V18.5H15V12.5H9V18.5H7V10.69L12 6.19ZM12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L12 3.5Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+            Daily Tippers
+          </a>
+        </ic-navigation-item>
+      </ic-navigation-group>
+      <ic-navigation-item slot="secondary-navigation">
+        <a
+          slot="navigation-item"
+          href="/bar"
+          aria-label="bar"
+        >
+          <svg
+            slot="icon"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 6.19L17 10.69V18.5H15V12.5H9V18.5H7V10.69L12 6.19ZM12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L12 3.5Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          bar
+        </a>
+      </ic-navigation-item>
+    </ic-side-navigation>
+`;
