1c6ac5f6096fd72f8ba9e86ca45f58c00e0da48d
gd2910 <122624946+gd2910@users.noreply.github.com>
2023-04-28 09:10:00 +0000

    feat(web-components): truncation
    Ic-typogrpahy truncation design pattern including stories tests and ic-typo test cleanup

--- packages/web-components/src/components/ic-typography/ic-typography.tsx
+++ packages/web-components/src/components/ic-typography/ic-typography.tsx
@@ -1,6 +1,7 @@
-import { Component, Prop, h, Element, Host } from "@stencil/core";
+import { Component, Prop, h, Element, Host, State, Watch } from "@stencil/core";
 
 import { IcTypographyVariants } from "../../utils/types";
+import { checkResizeObserver } from "../../utils/helpers";
 
 @Component({
   styleUrl: "ic-typography.css",
@@ -20,8 +21,124 @@ export class Typography {
    */
   @Prop() applyVerticalMargins?: boolean = false;
 
+  /**
+   * The number of lines to display before truncating the text, only used for the 'body' variant.
+   */
+  @Prop() maxLines?: number;
+
+  @State() expanded: boolean = false;
+  @State() truncated: boolean = false;
+  @State() truncButtonFocussed: boolean = false;
+
+  private truncWrapperEl: Element;
+  private resizeObserver: ResizeObserver = null;
+
+  private resizeInterval: number;
+  private lastWidth: number = 0;
+  private lastMarkerTop: number = 0;
+  private truncatedHeight: number = 0;
+  private marker: HTMLElement;
+  private focusBtnFromKeyboard: boolean = true;
+
+  @Watch("expanded")
+  watchExpandedHandler(): void {
+    this.el.setAttribute(
+      "style",
+      `--truncation-max-lines: ${this.expanded ? "initial" : this.maxLines}`
+    );
+  }
+
+  private toggleExpanded = () => {
+    this.expanded = !this.expanded;
+  };
+
+  private checkMaxLines = (height: number) => {
+    //24 is the height of a single line
+    const numLines = Math.floor(height / 24);
+    if (numLines > this.maxLines) {
+      this.el.setAttribute("style", `--truncation-max-lines: ${this.maxLines}`);
+      this.truncatedHeight = this.el.clientHeight;
+      this.truncated = true;
+    }
+  };
+
+  private checkMarkerPosition = (elTop: number, markerTop: number) => {
+    if (markerTop - elTop < this.truncatedHeight) {
+      this.truncated = false;
+      this.expanded = false;
+    } else {
+      this.truncated = true;
+    }
+  };
+
+  private getElementTop = (el: HTMLElement) => {
+    return el.getClientRects ? el.getClientRects()[0].top : 0;
+  };
+
+  private runResizeObserver = () => {
+    this.resizeObserver = new ResizeObserver(() => {
+      clearTimeout(this.resizeInterval);
+      this.resizeInterval = window.setTimeout(this.resizeObserverCallback, 50);
+    });
+    this.resizeObserver.observe(this.truncWrapperEl);
+  };
+
+  private resizeObserverCallback = () => {
+    if (this.lastWidth === this.el.clientWidth) {
+      return;
+    }
+
+    if (this.truncatedHeight === 0) {
+      this.checkMaxLines(this.el.clientHeight);
+      return;
+    }
+
+    const markerTop = this.getElementTop(this.marker);
+    if (markerTop === this.lastMarkerTop) {
+      return;
+    }
+
+    this.checkMarkerPosition(this.getElementTop(this.el), markerTop);
+    this.lastMarkerTop = markerTop;
+    this.lastWidth = this.el.clientWidth;
+  };
+
+  private truncButtonFocus = (): void => {
+    if (this.focusBtnFromKeyboard) {
+      this.truncButtonFocussed = true;
+    }
+  };
+
+  private truncButtonBlur = (): void => {
+    this.focusBtnFromKeyboard = true;
+    this.truncButtonFocussed = false;
+  };
+
+  private truncButtonFocusFromMouse = (): void => {
+    this.focusBtnFromKeyboard = false;
+  };
+
+  componentDidLoad(): void {
+    if (this.variant === "body" && this.maxLines > 0) {
+      const marker = document.createElement("span");
+      marker.style.visibility = "hidden";
+      this.el.appendChild(marker);
+      this.marker = marker;
+      this.lastWidth = this.el.clientWidth;
+      this.checkMaxLines(this.el.clientHeight);
+      checkResizeObserver(this.runResizeObserver);
+    }
+  }
+
+  disconnectedCallback(): void {
+    if (this.resizeObserver !== null) {
+      this.resizeObserver.disconnect();
+    }
+  }
+
   render() {
-    const { variant, applyVerticalMargins } = this;
+    const { variant, applyVerticalMargins, maxLines, truncated, expanded } =
+      this;
 
     return (
       <Host
@@ -30,7 +147,24 @@ export class Typography {
           [`ic-typography-vertical-margins-${variant}`]: applyVerticalMargins,
         }}
       >
-        <slot />
+        {variant === "body" && maxLines > 0 ? (
+          <div class="trunc-wrapper" ref={(el) => (this.truncWrapperEl = el)}>
+            <slot />
+          </div>
+        ) : (
+          <slot />
+        )}
+        {variant === "body" && maxLines > 0 && truncated && (
+          <button
+            class={{ "trunc-btn": true, focus: this.truncButtonFocussed }}
+            onFocus={this.truncButtonFocus}
+            onBlur={this.truncButtonBlur}
+            onMouseDown={this.truncButtonFocusFromMouse}
+            onClick={this.toggleExpanded}
+          >
+            {expanded ? "See less" : "See more"}
+          </button>
+        )}
       </Host>
     );
   }
