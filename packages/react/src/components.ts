236ff781b8bb4e4b39fde73f9704e1d1f1f679e9
MI6-451 <113018147+MI6-451@users.noreply.github.com>
2023-05-16 15:47:47 +0100

    feat(react): adds initial data table component
    Adds the react changes for the initial v1 data table component.

--- packages/react/src/components.ts
+++ packages/react/src/components.ts
@@ -21,6 +21,7 @@ export const IcClassificationBanner = /*@__PURE__*/createReactComponent<JSX.IcCl
 export const IcDataEntity = /*@__PURE__*/createReactComponent<JSX.IcDataEntity, HTMLIcDataEntityElement>('ic-data-entity');
 export const IcDataRow = /*@__PURE__*/createReactComponent<JSX.IcDataRow, HTMLIcDataRowElement>('ic-data-row');
 export const IcDialog = /*@__PURE__*/createReactComponent<JSX.IcDialog, HTMLIcDialogElement>('ic-dialog');
+export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
 export const IcDivider = /*@__PURE__*/createReactComponent<JSX.IcDivider, HTMLIcDividerElement>('ic-divider');
 export const IcEmptyState = /*@__PURE__*/createReactComponent<JSX.IcEmptyState, HTMLIcEmptyStateElement>('ic-empty-state');
 export const IcFooter = /*@__PURE__*/createReactComponent<JSX.IcFooter, HTMLIcFooterElement>('ic-footer');
