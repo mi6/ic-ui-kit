57e6e6ce7b336876eef8c8b7c6b7ddb35706ade5
ad9242 <116102637+ad9242@users.noreply.github.com>
2023-06-05 11:36:19 +0000

    feat(react): adds react stories for ic-dialog
    adds react storybook examples for dialog component

---
+++ packages/react/src/components.ts
@@ -0,0 +1,68 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ukic/web-components';
+
+import { defineCustomElements } from '@ukic/web-components/loader';
+
+defineCustomElements();
+export const IcAlert = /*@__PURE__*/createReactComponent<JSX.IcAlert, HTMLIcAlertElement>('ic-alert');
+export const IcBackToTop = /*@__PURE__*/createReactComponent<JSX.IcBackToTop, HTMLIcBackToTopElement>('ic-back-to-top');
+export const IcBreadcrumb = /*@__PURE__*/createReactComponent<JSX.IcBreadcrumb, HTMLIcBreadcrumbElement>('ic-breadcrumb');
+export const IcBreadcrumbGroup = /*@__PURE__*/createReactComponent<JSX.IcBreadcrumbGroup, HTMLIcBreadcrumbGroupElement>('ic-breadcrumb-group');
+export const IcButton = /*@__PURE__*/createReactComponent<JSX.IcButton, HTMLIcButtonElement>('ic-button');
+export const IcCard = /*@__PURE__*/createReactComponent<JSX.IcCard, HTMLIcCardElement>('ic-card');
+export const IcCheckbox = /*@__PURE__*/createReactComponent<JSX.IcCheckbox, HTMLIcCheckboxElement>('ic-checkbox');
+export const IcCheckboxGroup = /*@__PURE__*/createReactComponent<JSX.IcCheckboxGroup, HTMLIcCheckboxGroupElement>('ic-checkbox-group');
+export const IcChip = /*@__PURE__*/createReactComponent<JSX.IcChip, HTMLIcChipElement>('ic-chip');
+export const IcClassificationBanner = /*@__PURE__*/createReactComponent<JSX.IcClassificationBanner, HTMLIcClassificationBannerElement>('ic-classification-banner');
+export const IcDataEntity = /*@__PURE__*/createReactComponent<JSX.IcDataEntity, HTMLIcDataEntityElement>('ic-data-entity');
+export const IcDataRow = /*@__PURE__*/createReactComponent<JSX.IcDataRow, HTMLIcDataRowElement>('ic-data-row');
+export const IcDialog = /*@__PURE__*/createReactComponent<JSX.IcDialog, HTMLIcDialogElement>('ic-dialog');
+export const IcDivider = /*@__PURE__*/createReactComponent<JSX.IcDivider, HTMLIcDividerElement>('ic-divider');
+export const IcFooter = /*@__PURE__*/createReactComponent<JSX.IcFooter, HTMLIcFooterElement>('ic-footer');
+export const IcFooterLink = /*@__PURE__*/createReactComponent<JSX.IcFooterLink, HTMLIcFooterLinkElement>('ic-footer-link');
+export const IcFooterLinkGroup = /*@__PURE__*/createReactComponent<JSX.IcFooterLinkGroup, HTMLIcFooterLinkGroupElement>('ic-footer-link-group');
+export const IcHero = /*@__PURE__*/createReactComponent<JSX.IcHero, HTMLIcHeroElement>('ic-hero');
+export const IcHorizontalScroll = /*@__PURE__*/createReactComponent<JSX.IcHorizontalScroll, HTMLIcHorizontalScrollElement>('ic-horizontal-scroll');
+export const IcInputComponentContainer = /*@__PURE__*/createReactComponent<JSX.IcInputComponentContainer, HTMLIcInputComponentContainerElement>('ic-input-component-container');
+export const IcInputContainer = /*@__PURE__*/createReactComponent<JSX.IcInputContainer, HTMLIcInputContainerElement>('ic-input-container');
+export const IcInputLabel = /*@__PURE__*/createReactComponent<JSX.IcInputLabel, HTMLIcInputLabelElement>('ic-input-label');
+export const IcInputValidation = /*@__PURE__*/createReactComponent<JSX.IcInputValidation, HTMLIcInputValidationElement>('ic-input-validation');
+export const IcLink = /*@__PURE__*/createReactComponent<JSX.IcLink, HTMLIcLinkElement>('ic-link');
+export const IcLoadingIndicator = /*@__PURE__*/createReactComponent<JSX.IcLoadingIndicator, HTMLIcLoadingIndicatorElement>('ic-loading-indicator');
+export const IcMenu = /*@__PURE__*/createReactComponent<JSX.IcMenu, HTMLIcMenuElement>('ic-menu');
+export const IcMenuGroup = /*@__PURE__*/createReactComponent<JSX.IcMenuGroup, HTMLIcMenuGroupElement>('ic-menu-group');
+export const IcMenuItem = /*@__PURE__*/createReactComponent<JSX.IcMenuItem, HTMLIcMenuItemElement>('ic-menu-item');
+export const IcNavigationButton = /*@__PURE__*/createReactComponent<JSX.IcNavigationButton, HTMLIcNavigationButtonElement>('ic-navigation-button');
+export const IcNavigationGroup = /*@__PURE__*/createReactComponent<JSX.IcNavigationGroup, HTMLIcNavigationGroupElement>('ic-navigation-group');
+export const IcNavigationItem = /*@__PURE__*/createReactComponent<JSX.IcNavigationItem, HTMLIcNavigationItemElement>('ic-navigation-item');
+export const IcNavigationMenu = /*@__PURE__*/createReactComponent<JSX.IcNavigationMenu, HTMLIcNavigationMenuElement>('ic-navigation-menu');
+export const IcPageHeader = /*@__PURE__*/createReactComponent<JSX.IcPageHeader, HTMLIcPageHeaderElement>('ic-page-header');
+export const IcPagination = /*@__PURE__*/createReactComponent<JSX.IcPagination, HTMLIcPaginationElement>('ic-pagination');
+export const IcPaginationItem = /*@__PURE__*/createReactComponent<JSX.IcPaginationItem, HTMLIcPaginationItemElement>('ic-pagination-item');
+export const IcPopoverMenu = /*@__PURE__*/createReactComponent<JSX.IcPopoverMenu, HTMLIcPopoverMenuElement>('ic-popover-menu');
+export const IcRadioGroup = /*@__PURE__*/createReactComponent<JSX.IcRadioGroup, HTMLIcRadioGroupElement>('ic-radio-group');
+export const IcRadioOption = /*@__PURE__*/createReactComponent<JSX.IcRadioOption, HTMLIcRadioOptionElement>('ic-radio-option');
+export const IcSearchBar = /*@__PURE__*/createReactComponent<JSX.IcSearchBar, HTMLIcSearchBarElement>('ic-search-bar');
+export const IcSectionContainer = /*@__PURE__*/createReactComponent<JSX.IcSectionContainer, HTMLIcSectionContainerElement>('ic-section-container');
+export const IcSelect = /*@__PURE__*/createReactComponent<JSX.IcSelect, HTMLIcSelectElement>('ic-select');
+export const IcSideNavigation = /*@__PURE__*/createReactComponent<JSX.IcSideNavigation, HTMLIcSideNavigationElement>('ic-side-navigation');
+export const IcSkeleton = /*@__PURE__*/createReactComponent<JSX.IcSkeleton, HTMLIcSkeletonElement>('ic-skeleton');
+export const IcStatusTag = /*@__PURE__*/createReactComponent<JSX.IcStatusTag, HTMLIcStatusTagElement>('ic-status-tag');
+export const IcStep = /*@__PURE__*/createReactComponent<JSX.IcStep, HTMLIcStepElement>('ic-step');
+export const IcStepper = /*@__PURE__*/createReactComponent<JSX.IcStepper, HTMLIcStepperElement>('ic-stepper');
+export const IcSwitch = /*@__PURE__*/createReactComponent<JSX.IcSwitch, HTMLIcSwitchElement>('ic-switch');
+export const IcTab = /*@__PURE__*/createReactComponent<JSX.IcTab, HTMLIcTabElement>('ic-tab');
+export const IcTabContext = /*@__PURE__*/createReactComponent<JSX.IcTabContext, HTMLIcTabContextElement>('ic-tab-context');
+export const IcTabGroup = /*@__PURE__*/createReactComponent<JSX.IcTabGroup, HTMLIcTabGroupElement>('ic-tab-group');
+export const IcTabPanel = /*@__PURE__*/createReactComponent<JSX.IcTabPanel, HTMLIcTabPanelElement>('ic-tab-panel');
+export const IcTextField = /*@__PURE__*/createReactComponent<JSX.IcTextField, HTMLIcTextFieldElement>('ic-text-field');
+export const IcTheme = /*@__PURE__*/createReactComponent<JSX.IcTheme, HTMLIcThemeElement>('ic-theme');
+export const IcToast = /*@__PURE__*/createReactComponent<JSX.IcToast, HTMLIcToastElement>('ic-toast');
+export const IcToastRegion = /*@__PURE__*/createReactComponent<JSX.IcToastRegion, HTMLIcToastRegionElement>('ic-toast-region');
+export const IcTooltip = /*@__PURE__*/createReactComponent<JSX.IcTooltip, HTMLIcTooltipElement>('ic-tooltip');
+export const IcTopNavigation = /*@__PURE__*/createReactComponent<JSX.IcTopNavigation, HTMLIcTopNavigationElement>('ic-top-navigation');
+export const IcTypography = /*@__PURE__*/createReactComponent<JSX.IcTypography, HTMLIcTypographyElement>('ic-typography');
