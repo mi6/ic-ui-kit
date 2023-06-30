6698dff1ed32b2e4cbab1cd9e2cb9c543603f775
MI6-451 <113018147+MI6-451@users.noreply.github.com>
2023-06-30 11:10:27 +0100

    feat(web-components): add pagination and sorting to data table
    Adds pagination and sorting functionality to data table with some minor changes to pagination bar.

--- packages/web-components/src/components/ic-pagination-bar/ic-pagination-bar.tsx
+++ packages/web-components/src/components/ic-pagination-bar/ic-pagination-bar.tsx
@@ -17,65 +17,33 @@ import { IcThemeForeground } from "../../interface";
   shadow: true,
 })
 export class PaginationBar {
+  private PAGINATION = "ic-pagination";
+  private TEXT_FIELD = "ic-text-field";
+  private TOOLTIP = "ic-tooltip";
+
+  private INVALID_PAGE_ERROR = "Please enter a valid page";
+  private NAN_ERROR = "Please enter a number";
+
+  private resizeObserver: ResizeObserver = null;
+  private paginationBarEl: HTMLElement;
+
   @Element() el: HTMLIcPaginationBarElement;
 
-  /**
-   * Emitted when a page is navigated to via the 'go to' input.
-   */
-  @Event() icPageChange: EventEmitter<{ value: number }>;
+  @State() currentPage: number = 1;
 
-  /**
-   * Emitted when the items per page option is changed.
-   */
-  @Event() icItemsPerPageChange: EventEmitter<{ value: number }>;
+  @State() inputError: string = "Please enter a valid page";
 
-  /**
-   * Total number of items to be displayed across all pages.
-   */
-  @Prop() totalItems!: number;
+  @State() itemsPerPage: string;
 
-  /**
-   * Whether total number of items and current item range or total number of pages and current page is displayed.
-   */
-  @Prop() paginationType?: "data" | "page" = "page";
+  @State() lowerBound: number = 1;
 
-  /**
-   * Whether the displayed pagination is simple or complex.
-   */
-  @Prop() paginationControl?: "simple" | "complex" = "simple";
+  @State() paginationWidth: number;
 
-  /**
-   * What label will be used in place of 'items' if paginationType is data, should be capitalised.
-   */
-  @Prop() itemLabel?: string = "Item";
+  @State() paginationWrapped: boolean = false;
 
-  /**
-   * What label will be used in place of 'Page' if paginationType is page, should be capitalised.
-   */
-  @Prop() pageLabel?: string = "Page";
+  @State() totalPages: number;
 
-  /**
-   * What options will be displayed for 'items per page' select input, maximum of 4 options including a required 'All' option with value equal to total number of items.
-   */
-  @Prop({ mutable: true }) itemsPerPageOptions?: {
-    label: string;
-    value: string;
-  }[];
-
-  /**
-   * If `true`, the select input to control 'items per page' should be displayed.
-   */
-  @Prop() showItemsPerPageControl?: boolean = false;
-
-  /**
-   * If `true`, the number of total items and current item range or number of total pages and current page should be displayed.
-   */
-  @Prop() showItemsPerPage?: boolean = true;
-
-  /**
-   * If `true`, the 'go to page' control should be displayed.
-   */
-  @Prop() showGoToPageControl?: boolean = false;
+  @State() upperBound: number;
 
   /**
    * Sets the alignment of the items in the pagination bar.
@@ -87,98 +55,111 @@ export class PaginationBar {
    */
   @Prop() appearance?: IcThemeForeground = "default";
 
+  /**
+   * What label will be used in place of 'items' if paginationType is data, should be capitalised.
+   */
+  @Prop() itemLabel?: string = "Item";
+
+  /**
+   * What options will be displayed for 'items per page' select input, maximum of 4 options including a required 'All' option with value equal to total number of items.
+   */
+  @Prop({ mutable: true }) itemsPerPageOptions?: {
+    label: string;
+    value: string;
+  }[];
+
+  /**
+   * Whether the displayed pagination is simple or complex.
+   */
+  @Prop() paginationControl?: "simple" | "complex" = "simple";
+
+  /**
+   * Whether total number of items and current item range or total number of pages and current page is displayed.
+   */
+  @Prop() paginationType?: "data" | "page" = "page";
+
+  /**
+   * What label will be used in place of 'Page' if paginationType is page, should be capitalised.
+   */
+  @Prop() pageLabel?: string = "Page";
+
+  /**
+   * If `true`, the 'go to page' control should be displayed.
+   */
+  @Prop() showGoToPageControl?: boolean = false;
+
+  /**
+   * If `true`, the number of total items and current item range or number of total pages and current page should be displayed.
+   */
+  @Prop() showItemsPerPage?: boolean = true;
+
+  /**
+   * If `true`, the select input to control 'items per page' should be displayed.
+   */
+  @Prop() showItemsPerPageControl?: boolean = false;
+
+  /**
+   * Total number of items to be displayed across all pages.
+   */
+  @Prop() totalItems!: number;
+
+  /**
+   * Emitted when a page is navigated to via the 'go to' input.
+   */
+  @Event() icPageChange: EventEmitter<{ value: number }>;
+
+  /**
+   * Emitted when the items per page option is changed.
+   */
+  @Event() icItemsPerPageChange: EventEmitter<{ value: number }>;
+
+  disconnectedCallback(): void {
+    if (this.resizeObserver !== null) {
+      this.resizeObserver.disconnect();
+    }
+  }
+
+  componentWillLoad(): void {
+    if (
+      this.itemsPerPageOptions === undefined ||
+      this.itemsPerPageOptions === null
+    ) {
+      this.setDefaultItemsPerPageOptions();
+    }
+    this.trimItemsPerPageOptions();
+    this.setDefaultItemsPerPage();
+    this.setNumberPages();
+    this.setUpperBound();
+  }
+
+  componentDidLoad(): void {
+    this.paginationWidth = this.paginationBarEl.clientWidth;
+    checkResizeObserver(this.runResizeObserver);
+    this.setGoToPageInputStyles();
+    this.paginationShouldWrap();
+  }
+
   @Listen("icPageChange")
   pageChangeHandler(ev: CustomEvent) {
     const page = ev.detail.value;
     this.changePage(page);
   }
 
-  @State() itemsPerPage: string;
-
-  @State() totalPages: number;
-
-  @State() currentPage: number = 1;
-
-  @State() lowerBound: number = 1;
-
-  @State() upperBound: number;
-
-  @State() inputError: string = "Please enter a valid page";
-
-  @State() paginationWrapped: boolean = false;
-
-  @State() paginationWidth: number;
-
-  private PAGINATION = "ic-pagination";
-  private TEXT_FIELD = "ic-text-field";
-  private TOOLTIP = "ic-tooltip";
-
-  private INVALID_PAGE_ERROR = "Please enter a valid page";
-  private NAN_ERROR = "Please enter a number";
-
-  private setDefaultItemsPerPageOptions = () => {
-    this.itemsPerPageOptions =
-      this.totalItems <= 100
-        ? [
-            { label: "10", value: "10" },
-            { label: "25", value: "25" },
-            { label: "50", value: "50" },
-            { label: "All", value: String(this.totalItems) },
-          ]
-        : [
-            { label: "25", value: "25" },
-            { label: "100", value: "100" },
-            { label: "1000", value: "1000" },
-            { label: "All", value: String(this.totalItems) },
-          ];
-  };
-
-  private trimItemsPerPageOptions = () => {
-    this.itemsPerPageOptions = this.itemsPerPageOptions.slice(0, 3);
-    this.itemsPerPageOptions.push({
-      label: "All",
-      value: String(this.totalItems),
-    });
-
-    for (let i = 0; i < this.itemsPerPageOptions.length - 1; i++) {
-      if (this.totalItems < Number(this.itemsPerPageOptions[i].value)) {
-        this.itemsPerPageOptions.splice(
-          i,
-          this.itemsPerPageOptions.length - (i + 1)
-        );
-      }
-    }
-  };
-
-  private setDefaultItemsPerPage = () => {
-    this.itemsPerPage = this.itemsPerPageOptions[0].value;
-  };
-
-  private setNumberPages = () => {
-    this.totalPages = Math.ceil(this.totalItems / Number(this.itemsPerPage));
-  };
-
-  private setUpperBound = () => {
-    this.upperBound = Math.min(
-      this.lowerBound + Number(this.itemsPerPage) - 1,
-      this.totalItems
-    );
-  };
-
   private changeItemsPerPage = () => {
     const select = this.el.shadowRoot.querySelector("ic-select");
     const value = select.value;
     this.itemsPerPage = value;
     this.setNumberPages();
-    this.lowerBound = 1;
     this.setUpperBound();
     const pagination = this.el.shadowRoot.querySelector(
       this.PAGINATION
     ) as HTMLIcPaginationElement;
-    pagination.setCurrentPage(1);
-    this.currentPage = 1;
-    this.icPageChange.emit({ value: 1 });
+    if (this.currentPage > this.totalPages) {
+      pagination.setCurrentPage(this.totalPages);
+      this.currentPage = this.totalPages;
+    }
     this.icItemsPerPageChange.emit({ value: Number(this.itemsPerPage) });
+    this.icPageChange.emit({ value: this.currentPage });
   };
 
   private changePage = (page: number) => {
@@ -217,6 +198,52 @@ export class PaginationBar {
     }
   };
 
+  private handleBlur = () => {
+    const textField = this.el.shadowRoot?.querySelector(
+      this.TEXT_FIELD
+    ) as HTMLIcTextFieldElement;
+    const tooltip = this.el.shadowRoot?.querySelector(
+      this.TOOLTIP
+    ) as HTMLIcTooltipElement;
+    if (
+      (Number(textField.value) <= this.totalPages &&
+        Number(textField.value) > 0) ||
+      textField.value === ""
+    ) {
+      textField.validationStatus = "";
+    }
+    tooltip.displayTooltip(false, false);
+  };
+
+  private handleFocus = () => {
+    const textField = this.el.shadowRoot?.querySelector(
+      this.TEXT_FIELD
+    ) as HTMLIcTextFieldElement;
+    const tooltip = this.el.shadowRoot?.querySelector(
+      this.TOOLTIP
+    ) as HTMLIcTooltipElement;
+    if (textField.validationStatus === "error") {
+      tooltip.displayTooltip(true, true);
+    }
+  };
+
+  private handleInputChange = () => {
+    const tooltip = this.el.shadowRoot.querySelector(
+      this.TOOLTIP
+    ) as HTMLIcTooltipElement;
+    const textField = this.el.shadowRoot.querySelector(
+      this.TEXT_FIELD
+    ) as HTMLIcTextFieldElement;
+    const inputValue = parseInt(textField.value);
+
+    if (inputValue > this.totalPages || inputValue <= 0) {
+      this.inputError = this.INVALID_PAGE_ERROR;
+      tooltip.displayTooltip(true, true);
+      textField.validationStatus = "error";
+      textField.focus();
+    }
+  };
+
   private handleKeydown = (ev: KeyboardEvent) => {
     const tooltip = this.el.shadowRoot.querySelector(
       this.TOOLTIP
@@ -250,67 +277,16 @@ export class PaginationBar {
     ) as HTMLIcTextFieldElement;
     const inputValue = parseInt(textField.value);
 
-    if (Number.isNaN(inputValue) && ev.key !== "Backspace") {
+    if (
+      Number.isNaN(inputValue) &&
+      ev.key !== "Backspace" &&
+      ev.key !== "Enter" &&
+      ev.key !== "Tab" &&
+      ev.key !== "Shift"
+    ) {
       this.inputError = this.NAN_ERROR;
       tooltip.displayTooltip(true, false);
       textField.validationStatus = "error";
-    }
-  };
-
-  private handleInputChange = () => {
-    const tooltip = this.el.shadowRoot.querySelector(
-      this.TOOLTIP
-    ) as HTMLIcTooltipElement;
-    const textField = this.el.shadowRoot.querySelector(
-      this.TEXT_FIELD
-    ) as HTMLIcTextFieldElement;
-    const inputValue = parseInt(textField.value);
-
-    if (inputValue > this.totalPages || inputValue <= 0) {
-      this.inputError = this.INVALID_PAGE_ERROR;
-      tooltip.displayTooltip(true, true);
-      textField.validationStatus = "error";
-      textField.focus();
-    }
-  };
-
-  private setGoToPageInputStyles = () => {
-    const textField = this.el.shadowRoot?.querySelector(".go-to-page-input");
-    if (textField !== undefined) {
-      const input = textField?.shadowRoot?.querySelector("input");
-      if (input !== undefined) {
-        input.style.textAlign = "center";
-        input.style.padding = "0";
-      }
-    }
-  };
-
-  private handleBlur = () => {
-    const textField = this.el.shadowRoot?.querySelector(
-      this.TEXT_FIELD
-    ) as HTMLIcTextFieldElement;
-    const tooltip = this.el.shadowRoot?.querySelector(
-      this.TOOLTIP
-    ) as HTMLIcTooltipElement;
-    if (
-      (Number(textField.value) <= this.totalPages &&
-        Number(textField.value) > 0) ||
-      textField.value === ""
-    ) {
-      textField.validationStatus = "";
-    }
-    tooltip.displayTooltip(false, false);
-  };
-
-  private handleFocus = () => {
-    const textField = this.el.shadowRoot?.querySelector(
-      this.TEXT_FIELD
-    ) as HTMLIcTextFieldElement;
-    const tooltip = this.el.shadowRoot?.querySelector(
-      this.TOOLTIP
-    ) as HTMLIcTooltipElement;
-    if (textField.validationStatus === "error") {
-      tooltip.displayTooltip(true, true);
     }
   };
 
@@ -326,10 +302,6 @@ export class PaginationBar {
       }
     }
   };
-
-  private resizeObserver: ResizeObserver = null;
-
-  private paginationBarEl: HTMLElement;
 
   private resizeObserverCallback = (currSize: number) => {
     if (
@@ -350,31 +322,65 @@ export class PaginationBar {
     this.resizeObserver.observe(this.paginationBarEl);
   };
 
-  componentWillLoad(): void {
-    if (
-      this.itemsPerPageOptions === undefined ||
-      this.itemsPerPageOptions === null
-    ) {
-      this.setDefaultItemsPerPageOptions();
-    }
-    this.trimItemsPerPageOptions();
-    this.setDefaultItemsPerPage();
-    this.setNumberPages();
-    this.setUpperBound();
-  }
+  private setDefaultItemsPerPage = () => {
+    this.itemsPerPage = this.itemsPerPageOptions[0].value;
+  };
 
-  componentDidLoad(): void {
-    this.paginationWidth = this.paginationBarEl.clientWidth;
-    checkResizeObserver(this.runResizeObserver);
-    this.setGoToPageInputStyles();
-    this.paginationShouldWrap();
-  }
+  private setDefaultItemsPerPageOptions = () => {
+    this.itemsPerPageOptions =
+      this.totalItems <= 100
+        ? [
+            { label: "10", value: "10" },
+            { label: "25", value: "25" },
+            { label: "50", value: "50" },
+            { label: "All", value: String(this.totalItems) },
+          ]
+        : [
+            { label: "25", value: "25" },
+            { label: "100", value: "100" },
+            { label: "1000", value: "1000" },
+            { label: "All", value: String(this.totalItems) },
+          ];
+  };
 
-  disconnectedCallback(): void {
-    if (this.resizeObserver !== null) {
-      this.resizeObserver.disconnect();
+  private setGoToPageInputStyles = () => {
+    const textField = this.el.shadowRoot?.querySelector(".go-to-page-input");
+    if (textField !== undefined) {
+      const input = textField?.shadowRoot?.querySelector("input");
+      if (input !== undefined) {
+        input.style.textAlign = "center";
+        input.style.padding = "0";
+      }
     }
-  }
+  };
+
+  private setNumberPages = () => {
+    this.totalPages = Math.ceil(this.totalItems / Number(this.itemsPerPage));
+  };
+
+  private setUpperBound = () => {
+    this.upperBound = Math.min(
+      this.lowerBound + Number(this.itemsPerPage) - 1,
+      this.totalItems
+    );
+  };
+
+  private trimItemsPerPageOptions = () => {
+    this.itemsPerPageOptions = this.itemsPerPageOptions.slice(0, 3);
+    this.itemsPerPageOptions.push({
+      label: "All",
+      value: String(this.totalItems),
+    });
+
+    for (let i = 0; i < this.itemsPerPageOptions.length - 1; i++) {
+      if (this.totalItems <= Number(this.itemsPerPageOptions[i].value)) {
+        this.itemsPerPageOptions.splice(
+          i,
+          this.itemsPerPageOptions.length - (i + 1)
+        );
+      }
+    }
+  };
 
   render() {
     const {
@@ -427,9 +433,11 @@ export class PaginationBar {
                   ["item-pagination-label"]: true,
                 }}
                 variant="label"
+                aria-live="polite"
               >
                 {this.lowerBound} - {this.upperBound} of {this.totalItems}{" "}
-                {this.itemLabel.toLowerCase()}s
+                {this.itemLabel.toLowerCase()}
+                {this.totalItems > 1 ? "s" : ""}
               </ic-typography>
             ) : showItemsPerPage ? (
               <ic-typography
@@ -438,6 +446,7 @@ export class PaginationBar {
                   ["page-pagination-label"]: true,
                 }}
                 variant="label"
+                aria-live="polite"
               >
                 {this.pageLabel} {this.currentPage} of {this.totalPages}
               </ic-typography>
