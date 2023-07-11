ce1b530a529fabc9b45bb05a881ea3b952ef1e3c
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2023-07-11 16:19:15 +0100

    feat(root): added latest stencil eslint plugin rules
    added stencil-community plugin for eslint to ui-kit

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:@stencil/recommended",
+        "plugin:@stencil-community/recommended",
         "plugin:sonarjs/recommended"
     ],
     "parser": "@typescript-eslint/parser",
@@ -22,7 +22,33 @@ module.exports = {
         "@typescript-eslint/no-inferrable-types": ["warn", { ignoreProperties: true }],
         "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^(?:h|Component|Element|Prop|Listen|Watch|Method|State|Event)$" }],
         "@typescript-eslint/explicit-module-boundary-types": ["warn", { allowedNames: ["render"] }],
-        "@stencil/async-methods": "off",
+        "@stencil-community/async-methods": "off",
+        "@stencil-community/decorators-context": "error",
+        "@stencil-community/decorators-style": [
+            "error", {
+            "prop": "inline",
+            "state": "inline",
+            "element": "inline",
+            "event": "inline",
+            "method": "multiline",
+            "watch": "multiline",
+            "listen": "multiline"
+            }
+        ],
+        "@stencil-community/element-type": "error",
+        "@stencil-community/methods-must-be-public": "error",
+        "@stencil-community/no-unused-watch": "error",
+        "@stencil-community/own-methods-must-be-private": "error",
+        "@stencil-community/own-props-must-be-private": "error",
+        "@stencil-community/prefer-vdom-listener": "error",
+        "@stencil-community/props-must-be-public": "error",
+        "@stencil-community/render-returns-host": "error",
+        "@stencil-community/required-jsdoc": "error",
+        "@stencil-community/required-prefix": ["error", ["ic-"]],
+        "@stencil-community/reserved-member-names": "error",
+        "@stencil-community/single-export": "error",
+        "@stencil-community/strict-boolean-conditions": "off",
+        "@stencil-community/strict-mutable": "error",
         "sonarjs/cognitive-complexity": "off",
     },
     "overrides": [
