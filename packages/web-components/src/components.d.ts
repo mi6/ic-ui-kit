91cc6436abafc78d5c764df0fe2f312fe7013fbc
jd239 <116016939+jd239@users.noreply.github.com>
2023-08-31 12:55:30 +0000

    chore(web-components): tidy up after rebase
    Rebuild of auto generated files after large rebase

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -2033,11 +2033,7 @@ export namespace Components {
     }
     interface IcTooltip {
         /**
-          * If `true`, the tooltip will not be displayed on click, it will require hover or using the display method.
-         */
-        "disableClick"?: boolean;
-        /**
-          * If `true`, the tooltip will not be displayed on hover, it will require a click or using the display method.
+          * If `true`, the tooltip will not be displayed on hover, it will require a click.
          */
         "disableHover"?: boolean;
         /**
@@ -4763,11 +4759,7 @@ declare namespace LocalJSX {
     }
     interface IcTooltip {
         /**
-          * If `true`, the tooltip will not be displayed on click, it will require hover or using the display method.
-         */
-        "disableClick"?: boolean;
-        /**
-          * If `true`, the tooltip will not be displayed on hover, it will require a click or using the display method.
+          * If `true`, the tooltip will not be displayed on hover, it will require a click.
          */
         "disableHover"?: boolean;
         /**
