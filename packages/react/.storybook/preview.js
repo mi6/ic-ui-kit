4979ceaf9af6f21f6e53c1b346c701d8ee8b0ffc
Vidhanvyrs <vidhanvyrs@gmail.com>
2023-09-08 21:48:16 +0530

    resolved-changes#991

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,6 +1,6 @@
 import "@ukic/web-components/dist/core/normalize.css";
 import "@ukic/fonts/dist/fonts.css";
-import "../storybook.css";
+import "./storybook.css";
 import "@ukic/web-components/dist/core/core.css";
 
 export const parameters = {
