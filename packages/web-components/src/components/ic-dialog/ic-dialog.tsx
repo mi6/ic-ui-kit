6509836238b82699641455e7037e3717639d67b2
dz333dz <147149405+dz333dz@users.noreply.github.com>
2023-11-14 16:43:29 +0000

    fix(web-components): added ability to hide the close button, equalised call to action button spacing

--- packages/web-components/src/components/ic-dialog/ic-dialog.tsx
+++ packages/web-components/src/components/ic-dialog/ic-dialog.tsx
@@ -88,6 +88,11 @@ export class Dialog {
   @Prop() dismissLabel?: string = "Dismiss";
 
   /**
+   * If `true`, the close button will not be displayed.
+   */
+  @Prop() hideCloseButton?: boolean = false;
+
+  /**
    * Sets the heading for the dialog.
    */
   @Prop() heading!: string;
@@ -475,6 +480,7 @@ export class Dialog {
       status,
       destructive,
       dismissLabel,
+      hideCloseButton,
     } = this;
 
     return (
@@ -504,15 +510,17 @@ export class Dialog {
                 </slot>
               </div>
             </div>
-            <ic-button
-              class="close-icon"
-              variant="icon"
-              aria-label={dismissLabel}
-              onClick={this.closeIconClick}
-              data-gets-focus={destructive || !buttons ? "" : null}
-            >
-              <span class="close-icon-svg" innerHTML={closeIcon} />
-            </ic-button>
+            {!hideCloseButton && (
+              <ic-button
+                class="close-icon"
+                variant="icon"
+                aria-label={dismissLabel}
+                onClick={this.closeIconClick}
+                data-gets-focus={destructive || !buttons ? "" : null}
+              >
+                <span class="close-icon-svg" innerHTML={closeIcon} />
+              </ic-button>
+            )}
           </div>
           <div class="content-area">
             {isSlotUsed(this.el, "alert") ? (
