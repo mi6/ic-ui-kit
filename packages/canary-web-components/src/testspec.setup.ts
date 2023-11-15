eabc018ffe8b5a75aad2b86c780239e345d750ae
jd239 <116016939+jd239@users.noreply.github.com>
2023-11-15 14:34:27 +0000

    feat(canary-web-components): added support for adding icons to cells and headers
    added ability to either pass icons through or to slot them into cells, headers or whole columns

--- packages/canary-web-components/src/testspec.setup.ts
+++ packages/canary-web-components/src/testspec.setup.ts
@@ -1,7 +1,7 @@
 import * as helpers from "@ukic/web-components";
 
 //common setup to be used across all spec tests
-import { DEVICE_SIZES as device_sizes } from "@ukic/web-components";
+import { DEVICE_SIZES as device_sizes } from "@ukic/web-components/dist/types/utils";
 
 type TestKeyboardEvent = {
   key: string;
