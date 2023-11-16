e7f49aa28deccf8828811b3e3ad538ceffc05433
jd239 <116016939+jd239@users.noreply.github.com>
2023-11-16 09:30:17 +0000

    feat(canary-react): added icTitleBar to canary-react package
    Regenerated canary react component index

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -10,3 +10,4 @@ import { defineCustomElements } from '@ukic/canary-web-components/loader';
 defineCustomElements();
 export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
 export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
+export const IcTitleBar = /*@__PURE__*/createReactComponent<JSX.IcTitleBar, HTMLIcTitleBarElement>('ic-title-bar');
