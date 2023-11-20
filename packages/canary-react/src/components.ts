7f7eb5d4457c924fc2c446fc839d1212c582c907
jd239 <116016939+jd239@users.noreply.github.com>
2023-11-20 14:31:36 +0000

    build(react): rebuilding components file
    Rebuilt components file to include new data table title bar component rename

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -9,5 +9,5 @@ import { defineCustomElements } from '@ukic/canary-web-components/loader';
 
 defineCustomElements();
 export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
+export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
 export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
-export const IcTitleBar = /*@__PURE__*/createReactComponent<JSX.IcTitleBar, HTMLIcTitleBarElement>('ic-title-bar');
