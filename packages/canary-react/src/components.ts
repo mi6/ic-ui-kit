347e8cf0c93c5b8e5c4de07e2557df3d20d0272e
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2023-11-23 17:11:53 +0000

    fix(canary-react): update imports for stories, prettier fixes, add pagination bar story

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -1,13 +1,23 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
+import { createReactComponent } from "./react-component-lib";
 
-import type { JSX } from '@ukic/canary-web-components';
+import type { JSX } from "@ukic/canary-web-components";
 
-import { defineCustomElements } from '@ukic/canary-web-components/loader';
+import { defineCustomElements } from "@ukic/canary-web-components/loader";
 
 defineCustomElements();
-export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
-export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
-export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
+export const IcDataTable =
+  /*@__PURE__*/ createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>(
+    "ic-data-table"
+  );
+export const IcDataTableTitleBar = /*@__PURE__*/ createReactComponent<
+  JSX.IcDataTableTitleBar,
+  HTMLIcDataTableTitleBarElement
+>("ic-data-table-title-bar");
+export const IcPaginationBar =
+  /*@__PURE__*/ createReactComponent<
+    JSX.IcPaginationBar,
+    HTMLIcPaginationBarElement
+  >("ic-pagination-bar");
