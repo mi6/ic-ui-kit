00b5427b08914b244276ed39e5c602268907c6cc
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2023-11-30 12:32:39 +0000

    fix(canary-web-components): prettier fix, add new data tables type to docs, make fonts a peer dep

--- packages/canary-web-components/src/utils/helpers.ts
+++ packages/canary-web-components/src/utils/helpers.ts
@@ -53,7 +53,6 @@ export const getSlotElements = (
   }
 };
 
-
 export const getSlotContent = (
   element: HTMLElement,
   name: string
@@ -64,4 +63,30 @@ export const getSlotContent = (
   }
 
   return null;
+};
+
+/**
+ * Used to inherit global attributes set on the host. Called in componentWillLoad and assigned
+ * to a variable that is later used in the render function.
+ *
+ * This does not need to be reactive as changing attributes on the host element
+ * does not trigger a re-render.
+ */
+export const inheritAttributes = (
+  element: HTMLElement,
+  attributes: string[] = []
+): { [key: string]: unknown } => {
+  const attributeObject: { [key: string]: unknown } = {};
+
+  attributes.forEach((attr) => {
+    if (element.hasAttribute(attr)) {
+      const value = element.getAttribute(attr);
+      if (value !== null) {
+        attributeObject[attr] = element.getAttribute(attr);
+      }
+      element.removeAttribute(attr);
+    }
+  });
+
+  return attributeObject;
 };
