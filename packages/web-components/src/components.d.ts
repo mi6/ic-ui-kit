a957b056335e3d0cd3129fa24c91e30ec9ca0780
jd239 <116016939+jd239@users.noreply.github.com>
2023-12-12 14:50:13 +0000

    feat(web-components): updated web-components to include props required for canary components
    - Added internal props required for canary components
    - Added CSS custom properties for border light
    - Moved fonts to peer dep instead of deps
    - Regenerated package-lock due to package.json update

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -2150,6 +2150,7 @@ export namespace Components {
         "setVisible": (toast: HTMLIcToastElement) => Promise<void>;
     }
     interface IcTooltip {
+        "disableClick"?: boolean;
         /**
           * If `true`, the tooltip will not be displayed on hover, it will require a click.
          */
@@ -5339,6 +5340,7 @@ declare namespace LocalJSX {
         "openToast"?: HTMLIcToastElement;
     }
     interface IcTooltip {
+        "disableClick"?: boolean;
         /**
           * If `true`, the tooltip will not be displayed on hover, it will require a click.
          */
