21741c02e5e975832fde60c9f1c252113dfc15aa
jd239 <116016939+jd239@users.noreply.github.com>
2023-12-13 15:57:40 +0000

    feat(canary-web-components): added support for adding icons to cells and headers
    added ability to either pass icons through or to slot them into cells, headers or whole columns

--- packages/canary-web-components/src/utils/helpers.ts
+++ packages/canary-web-components/src/utils/helpers.ts
@@ -23,3 +23,45 @@ export const DEVICE_SIZES = {
   XL: Number(getCssProperty("--ic-breakpoint-xl").replace("px", "")), // 1200
   UNDEFINED: 1200,
 };
+
+export const isSlotUsed = (element: HTMLElement, slotName: string): boolean => {
+  return Array.from(element.children).some(
+    (child) => child.getAttribute("slot") === slotName
+  );
+};
+
+export const getSlot = (element: HTMLElement, name: string): Element | null => {
+  if (element && element.querySelector) {
+    return element.querySelector(`[slot="${name}"]`);
+  }
+  return null;
+};
+
+export const getSlotElements = (
+  slot: Element
+): NodeListOf<ChildNode> | Element[] => {
+  const slotContent = slot.firstElementChild as HTMLSlotElement;
+
+  if (slotContent !== null) {
+    const elements = slotContent.assignedElements
+      ? slotContent.assignedElements()
+      : slotContent.childNodes;
+    return elements.length ? elements : null;
+  } else {
+    //check for single element
+    return slot === null ? null : [slot];
+  }
+};
+
+
+export const getSlotContent = (
+  element: HTMLElement,
+  name: string
+): Element[] | NodeListOf<ChildNode> | null => {
+  const slot = getSlot(element, name);
+  if (slot) {
+    return getSlotElements(slot);
+  }
+
+  return null;
+};
