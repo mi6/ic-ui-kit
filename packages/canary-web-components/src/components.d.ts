c2ee934b07b131623e0d25eabc401a62a7dab0b3
jd239 <116016939+jd239@users.noreply.github.com>
2023-12-13 15:56:17 +0000

    feat(canary-web-components): added empty state functionality
    Added default and slotted empty state functionality

--- packages/canary-web-components/src/components.d.ts
+++ packages/canary-web-components/src/components.d.ts
@@ -54,7 +54,12 @@ export namespace Components {
     max?: number;
     min?: number;
     progress?: number;
+    showBackground?: boolean;
   };
+        /**
+          * The minimum amount of time the `loading` state displays for before showing the data. Used to prevent flashing in the component.
+         */
+        "minimumLoadingDisplayDuration"?: number;
         /**
           * Sets the props for the pagination bar.
          */
@@ -217,7 +222,12 @@ declare namespace LocalJSX {
     max?: number;
     min?: number;
     progress?: number;
+    showBackground?: boolean;
   };
+        /**
+          * The minimum amount of time the `loading` state displays for before showing the data. Used to prevent flashing in the component.
+         */
+        "minimumLoadingDisplayDuration"?: number;
         /**
           * Sets the props for the pagination bar.
          */
