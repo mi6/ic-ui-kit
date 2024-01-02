1348a53edb86139a36df980767b44f7c8e27d6ce
jd239 <116016939+jd239@users.noreply.github.com>
2024-01-02 14:57:53 +0000

    test(react): refactoring select tests
    - Refactoring select tests to reduce code
    - Reduce testing time by moving repeated screenshots
    - Removed separate visual regression block
    - Separated select and searchable

---
+++ packages/react/src/component-tests/IcSelect/IcSelectConstants.ts
@@ -0,0 +1,20 @@
+export const IC_INPUT_CONTAINER = "ic-input-component-container";
+export const IC_MENU_LI = "ic-menu ul li";
+export const IC_MENU_UL = "ic-menu ul";
+export const SC_IC_MENU_TYPOGRAPHY = ".sc-ic-menu ic-typography";
+export const DISABLED_OPTION_MENU = "option disabled-option sc-ic-menu";
+export const IC_TYPOGRAPHY = "ic-typography";
+export const ID_CLEAR_BUTTON = "#clear-button";
+export const ARIA_SELECTED = "aria-selected";
+
+export const TYPE_DOWN_ARROW = "{downArrow}";
+export const TYPE_ENTER = "{enter}";
+export const TYPE_BACKSPACE = "{backspace}";
+export const DATA_VALUE_ESPRESSO = "[data-value='espresso']";
+export const DATA_VALUE_CAP = '[data-value="Cap"]';
+export const DATA_LABEL_CAPUCCINO = '[data-label="Cappuccino"]';
+export const DATA_LABEL_ESPRESSO = '[data-label="Espresso"]';
+export const INPUT_TYPE_HIDDEN = "input[type='hidden']";
+export const NO_RESULTS_FOUND = "No results found";
+
+export const DEFAULT_TEST_THRESHOLD = 0.05;
