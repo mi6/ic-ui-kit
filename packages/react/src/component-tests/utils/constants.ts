76ada891c7526695851b3211b3063cd2bca30ff3
jd239 <116016939+jd239@users.noreply.github.com>
2024-01-02 14:57:53 +0000

    test(react): refactoring select tests
    - Refactoring select tests to reduce code
    - Reduce testing time by moving repeated screenshots
    - Removed separate visual regression block
    - Separated select and searchable

---
+++ packages/react/src/component-tests/utils/constants.ts
@@ -0,0 +1,11 @@
+export const HAVE_LENGTH = "have.length";
+export const HAVE_BEEN_CALLED_ONCE = "have.been.calledOnce";
+export const HAVE_VALUE = "have.value";
+export const HAVE_CLASS = "have.class";
+export const HAVE_CSS = "have.css";
+export const BE_VISIBLE = "be.visible";
+export const HAVE_FOCUS = "have.focus";
+export const HAVE_ATTR = "have.attr";
+export const CONTAIN_VALUE = "contain.value";
+export const NOT_BE_VISIBLE = "not.be.visible";
+export const NOT_BE_CALLED_ONCE = "not.be.calledOnce";
