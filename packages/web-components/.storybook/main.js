ebf6038012ff1a231d44a88b8ab0a964af9ace52
zxh722 <151852539+zxh722@users.noreply.github.com>
2024-01-12 10:58:35 +0000

    feat(web-components): update web-components storybook to v7.6.7
    Storybook-static folder is now being served on a http-server to interact with Loki, update MDX files
    from MDX1 to MDX2. Prettier fixes to files.
    
    . #977

--- packages/web-components/.storybook/main.js
+++ packages/web-components/.storybook/main.js
@@ -1,17 +1,27 @@
 module.exports = {
   "core": {
-    builder: "webpack5",
-    disableTelemetry: true,
+    disableTelemetry: true
   },
+
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-postcss",
     "@storybook/addon-a11y",
+    "@storybook/addon-mdx-gfm"
   ],
-  "framework": "@storybook/web-components"
+
+  "framework": {
+    name: "@storybook/web-components-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 }
