2f0dfd04b1b2c1433d1564d6b3d8f1d9c2f098cd
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-01-16 08:46:00 +0000

    test(react): add cypress visual and a11y tests for badge
    Add cypress visual and a11y tests for badge to replace removed loki and a11y tests
    
    . #1370

---
+++ packages/react/src/component-tests/IcBadge/IcBadge.cy.tsx
@@ -0,0 +1,272 @@
+/// <reference types="Cypress" />
+
+import React from "react";
+import {
+  IcBadge,
+  IcButton,
+  IcChip,
+  IcTab,
+  IcTabContext,
+  IcTabGroup,
+  IcTabPanel,
+} from "../../components";
+import { HAVE_CLASS } from "../utils/constants";
+import { CYPRESS_AXE_OPTIONS } from "../../../cypress/utils/a11y";
+import { mount } from "cypress/react";
+import { ShowHideBadge, badgeTypes } from "./IcBadgeTestData";
+
+const DEFAULT_TEST_THRESHOLD = 0.06;
+
+describe("IcBadge", () => {
+  beforeEach(() => {
+    cy.injectAxe();
+  });
+
+  afterEach(() => {
+    cy.task("generateReport");
+  });
+
+  it("should show and hide the badge", () => {
+    mount(
+      <div style={{ margin: "16px" }}>
+        <ShowHideBadge />
+      </div>
+    );
+
+    cy.get("ic-button").click();
+    cy.get("ic-badge").should(HAVE_CLASS, "hide");
+    cy.get("ic-button").click();
+    cy.get("ic-badge").should(HAVE_CLASS, "show");
+  });
+
+  it("should have more descriptive aria-label if prop set", () => {
+    mount(
+      <IcButton variant="secondary" style={{ margin: "16px" }}>
+        <IcBadge
+          text-label="1"
+          slot="badge"
+          size="large"
+          accessible-label="1 notification found"
+        />
+        Default
+      </IcButton>
+    );
+
+    cy.get(`[aria-label="badge displaying 1 notification found"]`).should(
+      "exist"
+    );
+  });
+
+  it("renders success", () => {
+    mount(badgeTypes("success"));
+
+    cy.compareSnapshot("success", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders error", () => {
+    mount(badgeTypes("error"));
+
+    cy.compareSnapshot("error", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders warning", () => {
+    mount(badgeTypes("warning"));
+
+    cy.compareSnapshot("warning", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders neutral", () => {
+    mount(badgeTypes("neutral"));
+
+    cy.compareSnapshot("neutral", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders light", () => {
+    mount(badgeTypes("light"));
+
+    cy.compareSnapshot("light", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders info", () => {
+    mount(badgeTypes("info"));
+
+    cy.compareSnapshot("info", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders custom", () => {
+    mount(badgeTypes("custom", "#F8C8DC"));
+
+    cy.compareSnapshot("custom", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders max number", () => {
+    mount(
+      <div>
+        <div style={{ padding: "16px" }}>
+          <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+            <IcBadge
+              text-label="1000"
+              slot="badge"
+              size="small"
+              max-number="9"
+              variant="success"
+            />
+            Small
+          </IcButton>
+          <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+            <IcBadge
+              text-label="1000"
+              max-number="9"
+              slot="badge"
+              variant="success"
+            />
+            Default
+          </IcButton>
+          <IcButton variant="secondary">
+            <IcBadge
+              text-label="1000"
+              slot="badge"
+              size="large"
+              max-number="9"
+              variant="success"
+            />
+            Large
+          </IcButton>
+        </div>
+        <div style={{ padding: "16px" }}>
+          <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+            <IcBadge
+              text-label="1000"
+              slot="badge"
+              size="small"
+              max-number="99"
+              variant="success"
+            />
+            Small
+          </IcButton>
+          <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+            <IcBadge
+              text-label="1000"
+              max-number="99"
+              slot="badge"
+              variant="success"
+            />
+            Default
+          </IcButton>
+          <IcButton variant="secondary">
+            <IcBadge
+              text-label="1000"
+              slot="badge"
+              size="large"
+              max-number="99"
+              variant="success"
+            />
+            Large
+          </IcButton>
+        </div>
+      </div>
+    );
+
+    cy.compareSnapshot("max-number", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders position near", () => {
+    mount(
+      <div>
+        <div style={{ padding: "16px" }}>
+          <IcChip label="Small" style={{ marginRight: "16px" }}>
+            <IcBadge text-label="1" slot="badge" position="near" size="small" />
+          </IcChip>
+          <IcChip label="Default" style={{ marginRight: "16px" }}>
+            <IcBadge text-label="1" slot="badge" position="near" />
+          </IcChip>
+          <IcChip label="Large">
+            <IcBadge text-label="1" slot="badge" position="near" size="large" />
+          </IcChip>
+        </div>
+        <div style={{ padding: "16px" }}>
+          <IcChip label="Small Dot" style={{ marginRight: "16px" }}>
+            <IcBadge type="dot" slot="badge" position="near" size="small" />
+          </IcChip>
+          <IcChip label="Default Dot" style={{ marginRight: "16px" }}>
+            <IcBadge type="dot" slot="badge" position="near" />
+          </IcChip>
+          <IcChip label="Large Dot">
+            <IcBadge type="dot" slot="badge" position="near" size="large" />
+          </IcChip>
+        </div>
+      </div>
+    );
+
+    cy.compareSnapshot("position-near", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("renders position inline", () => {
+    cy.viewport(700, 500);
+    mount(
+      <IcTabContext>
+        <IcTabGroup label="Example tab group">
+          <IcTab>
+            Small
+            <IcBadge
+              text-label="1"
+              slot="badge"
+              position="inline"
+              size="small"
+              accessibleLabel="1 notification found"
+            />
+          </IcTab>
+          <IcTab>
+            Default
+            <IcBadge
+              text-label="1"
+              slot="badge"
+              position="inline"
+              accessibleLabel="1 notification found"
+            />
+          </IcTab>
+          <IcTab>
+            Large
+            <IcBadge
+              text-label="1"
+              slot="badge"
+              position="inline"
+              size="large"
+              accessibleLabel="1 notification found"
+            />
+          </IcTab>
+          <IcTab>
+            Small Dot
+            <IcBadge type="dot" slot="badge" position="inline" size="small" />
+          </IcTab>
+          <IcTab>
+            Dot
+            <IcBadge type="dot" slot="badge" position="inline" />
+          </IcTab>
+          <IcTab>
+            Large Dot
+            <IcBadge type="dot" slot="badge" position="inline" size="large" />
+          </IcTab>
+        </IcTabGroup>
+        <IcTabPanel>Default badge</IcTabPanel>
+        <IcTabPanel>Small badge</IcTabPanel>
+        <IcTabPanel>Large badge</IcTabPanel>
+        <IcTabPanel>Small dot badge</IcTabPanel>
+        <IcTabPanel>Default dot badge</IcTabPanel>
+        <IcTabPanel>Large dot badge</IcTabPanel>
+      </IcTabContext>
+    );
+
+    cy.wait(100).compareSnapshot("position-inline", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+});
---
+++ packages/react/src/component-tests/IcBadge/IcBadgeTestData.tsx
@@ -0,0 +1,155 @@
+/* eslint-disable react/jsx-no-bind */
+import { IcBadgeVariants, IcColor } from "@ukic/web-components";
+import { SlottedSVG, IcBadge, IcButton } from "../../";
+import React from "react";
+
+export const badgeTypes = (variant: IcBadgeVariants, customColor?: IcColor) => {
+  return (
+    <div>
+      <div style={{ padding: "16px" }}>
+        <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+          <IcBadge
+            text-label="1"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+            size="small"
+          />
+          Small
+        </IcButton>
+        <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+          <IcBadge
+            text-label="1"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+          />
+          Default
+        </IcButton>
+        <IcButton variant="secondary">
+          <IcBadge
+            text-label="1"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+            size="large"
+          />
+          Large
+        </IcButton>
+      </div>
+      <div style={{ padding: "16px" }}>
+        <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+          <IcBadge
+            type="dot"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+            size="small"
+          />
+          Small Dot
+        </IcButton>
+        <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+          <IcBadge
+            type="dot"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+          />
+          Default Dot
+        </IcButton>
+        <IcButton variant="secondary">
+          <IcBadge
+            type="dot"
+            slot="badge"
+            variant={variant}
+            size="large"
+            customColor={customColor}
+          />
+          Large Dot
+        </IcButton>
+      </div>
+      <div style={{ padding: "16px" }}>
+        <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+          <IcBadge
+            type="icon"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+            size="small"
+          >
+            <SlottedSVG
+              slot="badge-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+            </SlottedSVG>
+          </IcBadge>
+          Small Icon
+        </IcButton>
+        <IcButton variant="secondary" style={{ marginRight: "16px" }}>
+          <IcBadge
+            type="icon"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+          >
+            <SlottedSVG
+              slot="badge-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+            </SlottedSVG>
+          </IcBadge>
+          Default Icon
+        </IcButton>
+        <IcButton variant="secondary">
+          <IcBadge
+            type="icon"
+            slot="badge"
+            variant={variant}
+            customColor={customColor}
+            size="large"
+          >
+            <SlottedSVG
+              slot="badge-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+            </SlottedSVG>
+          </IcBadge>
+          Large Icon
+        </IcButton>
+      </div>
+    </div>
+  );
+};
+
+export const ShowHideBadge = () => {
+  const badgeVisibility = () => {
+    const badge: any = document.querySelector("ic-badge");
+    if (badge.classList.contains("show")) {
+      badge.visible = false;
+    } else {
+      badge.visible = true;
+    }
+  };
+
+  return (
+    <IcButton variant="secondary" onClick={badgeVisibility}>
+      <IcBadge text-label="1" slot="badge" />
+      Hide/Show badge
+    </IcButton>
+  );
+};
