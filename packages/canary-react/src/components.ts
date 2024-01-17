3624513e9d67a7534f9ea99e3257405bdc991b40
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-01-17 15:17:28 +0000

    style(canary-react): prettier fixes

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -8,16 +8,15 @@ import type { JSX } from "@ukic/canary-web-components";
 import { defineCustomElements } from "@ukic/canary-web-components/loader";
 
 defineCustomElements();
-export const IcDataTable =
-  /*@__PURE__*/ createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>(
-    "ic-data-table"
-  );
+export const IcDataTable = /*@__PURE__*/ createReactComponent<
+  JSX.IcDataTable,
+  HTMLIcDataTableElement
+>("ic-data-table");
 export const IcDataTableTitleBar = /*@__PURE__*/ createReactComponent<
   JSX.IcDataTableTitleBar,
   HTMLIcDataTableTitleBarElement
 >("ic-data-table-title-bar");
-export const IcPaginationBar =
-  /*@__PURE__*/ createReactComponent<
-    JSX.IcPaginationBar,
-    HTMLIcPaginationBarElement
-  >("ic-pagination-bar");
+export const IcPaginationBar = /*@__PURE__*/ createReactComponent<
+  JSX.IcPaginationBar,
+  HTMLIcPaginationBarElement
+>("ic-pagination-bar");
