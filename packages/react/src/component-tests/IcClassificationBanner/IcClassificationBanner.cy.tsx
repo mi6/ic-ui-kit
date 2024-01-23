9e4c57e9ff7dedd2db3c56f17ee68ab65f193312
gd2910 <122624946+gd2910@users.noreply.github.com>
2024-01-23 16:06:05 +0000

    test(react): visual and a11y tests added
    Migrating tests to cypress, class banner has no e2e tests but visual and a11y tests have been
    created or migrated

---
+++ packages/react/src/component-tests/IcClassificationBanner/IcClassificationBanner.cy.tsx
@@ -0,0 +1,113 @@
+/* eslint-disable react/jsx-no-bind */
+/// <reference types="Cypress" />
+
+import React from "react";
+import { IcClassificationBanner } from "../../components";
+import { CYPRESS_AXE_OPTIONS } from "../../../cypress/utils/a11y";
+import { mount } from "cypress/react";
+
+const DEFAULT_TEST_THRESHOLD = 0.03;
+
+describe("IcClassificationButton visual and a11y testing", () => {
+  beforeEach(() => {
+    cy.injectAxe();
+  });
+
+  afterEach(() => {
+    cy.task("generateReport");
+  });
+
+  it("tests default banner", () => {
+    mount(<IcClassificationBanner></IcClassificationBanner>);
+
+    cy.compareSnapshot("default", DEFAULT_TEST_THRESHOLD + 0.02);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests official classification banner", () => {
+    mount(
+      <IcClassificationBanner classification="official"></IcClassificationBanner>
+    );
+
+    cy.compareSnapshot("official", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests official-sensitive classification banner", () => {
+    mount(
+      <IcClassificationBanner classification="official-sensitive"></IcClassificationBanner>
+    );
+
+    cy.compareSnapshot("official-sensitive", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests secret classification banner", () => {
+    mount(
+      <IcClassificationBanner classification="secret"></IcClassificationBanner>
+    );
+
+    cy.compareSnapshot("secret", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests top-secret classification banner", () => {
+    mount(
+      <IcClassificationBanner classification="top-secret"></IcClassificationBanner>
+    );
+
+    cy.compareSnapshot("top-secret", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests classification banner with country prop", () => {
+    mount(
+      <IcClassificationBanner
+        classification="official"
+        country="uk"
+      ></IcClassificationBanner>
+    );
+
+    cy.compareSnapshot("country official", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests classification banner with up-to prop", () => {
+    mount(
+      <IcClassificationBanner
+        classification="official"
+        upTo={true}
+      ></IcClassificationBanner>
+    );
+
+    cy.compareSnapshot("up to official", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+
+  it("tests classification banner with inline prop", () => {
+    mount(
+      <div>
+        <div
+          style={{
+            height: "10vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Container to show inline banner
+        </div>
+        <IcClassificationBanner
+          classification="official"
+          inline={true}
+        ></IcClassificationBanner>
+      </div>
+    );
+
+    cy.wait(500).compareSnapshot(
+      "inline official",
+      DEFAULT_TEST_THRESHOLD + 0.03
+    );
+    cy.checkA11y(undefined, CYPRESS_AXE_OPTIONS);
+  });
+});
