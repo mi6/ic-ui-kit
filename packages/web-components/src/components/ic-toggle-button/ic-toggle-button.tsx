ed191e6be9d589e9efbab4ae006a67655626c61a
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-01-23 14:20:38 +0000

    feat(web-components): add toggle button component
    Add toggle button component with unit tests. Update button component to have top icon slot.
    
    . #264

---
+++ packages/web-components/src/components/ic-toggle-button/ic-toggle-button.tsx
@@ -0,0 +1,168 @@
+import {
+  Component,
+  Element,
+  Host,
+  Prop,
+  Event,
+  EventEmitter,
+  Listen,
+  h,
+} from "@stencil/core";
+import {
+  isSlotUsed,
+  onComponentRequiredPropUndefined,
+  removeDisabledFalse,
+} from "../../utils/helpers";
+import { IcSizes, IcThemeForeground } from "../../utils/types";
+
+/**
+ * @slot icon - Content will be displayed alongside the toggle button label.
+ * @slot badge - Badge component overlaying the top right of the toggle button.
+ */
+
+@Component({
+  tag: "ic-toggle-button",
+  styleUrl: "ic-toggle-button.css",
+  shadow: {
+    delegatesFocus: true,
+  },
+})
+export class ToggleButton {
+  @Element() el: HTMLIcToggleButtonElement;
+
+  /**
+   * The accessible label that will be applied to the toggle button. This is required for the icon variant of toggle buttons.
+   */
+  @Prop() accessibleLabel?: string;
+
+  /**
+   * The appearance of the toggle button.
+   */
+  @Prop() appearance?: IcThemeForeground = "default";
+
+  /**
+   * If `true`, the toggle button will be in disabled state.
+   */
+  @Prop() disabled?: boolean = false;
+
+  /**
+   * If `true`, the toggle button will fill the width of the container.
+   */
+  @Prop() fullWidth?: boolean = false;
+
+  /**
+   * The placement of the icon in relation to the toggle button label.
+   */
+  @Prop() iconPlacement?: "left" | "right" | "top";
+
+  /**
+   * The label to display in the toggle button. This is required for the default variant of toggle buttons.
+   */
+  @Prop() label?: string;
+
+  /**
+   * If `true`, the toggle button will be in loading state.
+   */
+  @Prop() loading?: boolean = false;
+
+  /**
+   * The size of the toggle button to be displayed.
+   */
+  @Prop() size?: IcSizes = "default";
+
+  /**
+   * If `true`, the toggle button will be in a checked state.
+   */
+  @Prop({ mutable: true, reflect: true }) toggleChecked: boolean = false;
+
+  /**
+   * The variant of the toggle button.
+   */
+  @Prop({ reflect: true }) variant: "default" | "icon" = "default";
+
+  /**
+   * Emitted when the user clicks a toggle button.
+   */
+  @Event() icToggleChecked: EventEmitter<{
+    checked: boolean;
+  }>;
+
+  componentWillLoad(): void {
+    removeDisabledFalse(this.disabled, this.el);
+  }
+
+  componentDidLoad(): void {
+    onComponentRequiredPropUndefined(
+      [
+        {
+          prop: this.variant === "icon" ? this.accessibleLabel : this.label,
+          propName: this.variant === "icon" ? "accessible-label" : "label",
+        },
+      ],
+      "Toggle button"
+    );
+  }
+
+  @Listen("click", { capture: true })
+  handleHostClick(e: Event): void {
+    if (this.disabled) {
+      e.stopImmediatePropagation();
+    } else if (!this.loading) {
+      this.toggleChecked = !this.toggleChecked;
+    }
+  }
+
+  private handleClick = (): void => {
+    !this.loading &&
+      !this.disabled &&
+      this.icToggleChecked.emit({
+        checked: this.toggleChecked,
+      });
+  };
+
+  render() {
+    return (
+      <Host
+        class={{
+          ["disabled"]: this.disabled,
+          ["checked"]: this.toggleChecked,
+          [`${this.appearance}`]: true,
+          ["icon"]: this.variant === "icon",
+          [`${this.size}`]: true,
+          ["loading"]: this.loading,
+        }}
+      >
+        <ic-button
+          role="checkbox"
+          aria-checked={this.toggleChecked ? "true" : "false"}
+          variant={this.variant === "icon" ? "icon" : "secondary"}
+          onClick={this.handleClick}
+          title={this.accessibleLabel}
+          aria-label={`${
+            this.accessibleLabel ? this.accessibleLabel : this.label
+          }, ${this.toggleChecked ? "ticked" : "unticked"}`}
+          disabled={this.disabled}
+          appearance={this.appearance}
+          size={this.size}
+          fullWidth={this.fullWidth}
+          loading={this.loading}
+          aria-disabled={`${this.disabled}`}
+        >
+          {this.variant !== "icon" && this.label}
+          <slot />
+          {isSlotUsed(this.el, "icon") && (
+            <slot
+              name="icon"
+              slot={`${
+                this.iconPlacement ? `${this.iconPlacement}-icon` : "icon"
+              }`}
+            ></slot>
+          )}
+          {isSlotUsed(this.el, "badge") && (
+            <slot name="badge" slot="badge"></slot>
+          )}
+        </ic-button>
+      </Host>
+    );
+  }
+}
---
+++ packages/web-components/src/components/ic-toggle-button/test/basic/ic-toggle-button.spec.ts
@@ -0,0 +1,143 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { ToggleButton } from "../../ic-toggle-button";
+
+describe("ic-toggle-button component", () => {
+  it("should render", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle"></ic-toggle-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render icon variant", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button variant="icon" accessible-label="Refresh the page">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render slotted icon", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle">
+          <svg
+            slot="icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render slotted icon with icon placement", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle" icon-placement="right">
+          <svg
+            slot="icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render slotted badge", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle">
+          <ic-badge text-label="1" slot="badge" variant="success"></ic-badge>
+        </ic-toggle-button>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should update toggleChecked value on click", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle" id='test-button' onclick='alert('test')'></ic-toggle-button>`,
+    });
+
+    expect(page.rootInstance.toggleChecked).toBeFalsy();
+
+    const element = document.getElementById("test-button");
+
+    element.click();
+
+    await page.waitForChanges();
+
+    expect(page.rootInstance.toggleChecked).toBeTruthy();
+  });
+
+  it("should emit icToggleChecked on click", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle" id='test-button' onclick='alert('test')'></ic-toggle-button>`,
+    });
+
+    const eventSpy = jest.fn();
+
+    page.win.addEventListener("icToggleChecked", eventSpy);
+
+    page.rootInstance.handleClick();
+
+    await page.waitForChanges();
+
+    expect(eventSpy).toHaveBeenCalled();
+  });
+
+  it("should stop immediate propagation of a click event when disabled", async () => {
+    const page = await newSpecPage({
+      components: [ToggleButton],
+      html: `<ic-toggle-button label="Toggle" id='test-button' disabled=true onclick='alert('test')'></ic-toggle-button>`,
+    });
+
+    jest.spyOn(window, "alert").mockImplementation();
+
+    const element = document.getElementById("test-button");
+
+    element.click();
+
+    await page.waitForChanges();
+
+    expect(window.alert).not.toHaveBeenCalled;
+  });
+});
