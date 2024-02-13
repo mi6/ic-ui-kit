19226176a679729d4a573b9fa01da1d122d8a853
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-02-13 12:56:23 +0000

    test(react): add cypress tests for chip
    Add cypress tests for chip - a11y and visual regression
    
    . #1422

---
+++ packages/react/src/component-tests/IcChip/IcChip.cy.tsx
@@ -0,0 +1,151 @@
+/* eslint-disable react/jsx-no-bind */
+/// <reference types="Cypress" />
+
+import React from "react";
+import { IcChip } from "../../components";
+import { mount } from "cypress/react";
+import { SlottedSVG } from "../../";
+
+const DEFAULT_TEST_THRESHOLD = 0.03;
+
+describe("IcChip visual and a11y testing", () => {
+  beforeEach(() => {
+    cy.injectAxe();
+  });
+
+  afterEach(() => {
+    cy.task("generateReport");
+  });
+
+  it("renders static", () => {
+    mount(
+      <div style={{ padding: "8px" }}>
+        <IcChip label="Default" />
+      </div>
+    );
+
+    cy.compareSnapshot("static", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11yWithWait();
+  });
+
+  it("renders outlined", () => {
+    mount(
+      <div style={{ padding: "8px" }}>
+        <IcChip label="Default" variant="outlined" />
+      </div>
+    );
+
+    cy.compareSnapshot("outlined", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11yWithWait();
+  });
+
+  it("renders with icon", () => {
+    mount(
+      <div style={{ padding: "8px" }}>
+        <IcChip label="Default">
+          <SlottedSVG
+            slot="icon"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 3C11.66 3 13 4.34 13 6C13 7.66 11.66 9 10 9C8.34 9 7 7.66 7 6C7 4.34 8.34 3 10 3ZM10 17.2C7.5 17.2 5.29 15.92 4 13.98C4.03 11.99 8 10.9 10 10.9C11.99 10.9 15.97 11.99 16 13.98C14.71 15.92 12.5 17.2 10 17.2Z"></path>
+          </SlottedSVG>
+        </IcChip>
+      </div>
+    );
+
+    cy.compareSnapshot("with-icon", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11yWithWait();
+  });
+
+  it("renders sizes", () => {
+    mount(
+      <div style={{ padding: "8px" }}>
+        <IcChip label="Small" size="small" />
+        <IcChip label="Default" />
+        <IcChip label="Large" size="large" />
+      </div>
+    );
+
+    cy.compareSnapshot("sizes", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11yWithWait();
+  });
+
+  it("renders dismissible", () => {
+    mount(
+      <div style={{ padding: "8px" }}>
+        <IcChip id="small-chip" label="Small" size="small" dismissible />
+        <IcChip label="Default" dismissible />
+        <IcChip label="Large" size="large" dismissible />
+      </div>
+    );
+
+    cy.compareSnapshot("dismissible", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11yWithWait();
+
+    cy.findShadowEl("ic-chip#small-chip", "button").focus();
+
+    cy.compareSnapshot("dismissible-focus", DEFAULT_TEST_THRESHOLD + 0.01);
+    cy.checkA11yWithWait();
+  });
+
+  it("renders disabled", () => {
+    mount(
+      <div style={{ padding: "8px" }}>
+        <IcChip label="Default" disabled />
+        <IcChip label="Default" variant="outlined" disabled />
+      </div>
+    );
+
+    cy.compareSnapshot("disabled", DEFAULT_TEST_THRESHOLD);
+  });
+
+  it("renders with long label", () => {
+    mount(
+      <div style={{ padding: "8px", maxWidth: "150px" }}>
+        <IcChip
+          label="Really long name for a chip that should wrap"
+          dismissible
+        >
+          <SlottedSVG
+            slot="icon"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 3C11.66 3 13 4.34 13 6C13 7.66 11.66 9 10 9C8.34 9 7 7.66 7 6C7 4.34 8.34 3 10 3ZM10 17.2C7.5 17.2 5.29 15.92 4 13.98C4.03 11.99 8 10.9 10 10.9C11.99 10.9 15.97 11.99 16 13.98C14.71 15.92 12.5 17.2 10 17.2Z"></path>
+          </SlottedSVG>
+        </IcChip>
+      </div>
+    );
+
+    cy.compareSnapshot("long-label", DEFAULT_TEST_THRESHOLD + 0.02);
+    cy.checkA11yWithWait();
+  });
+
+  it("renders with white background for outlined variant", () => {
+    mount(
+      <div
+        style={{
+          backgroundColor: "#2c2f34",
+          padding: "8px",
+          width: "fit-content",
+        }}
+      >
+        <IcChip
+          label="Default"
+          variant="outlined"
+          transparentBackground={false}
+        />
+      </div>
+    );
+
+    cy.compareSnapshot("white-background", DEFAULT_TEST_THRESHOLD);
+    cy.checkA11yWithWait();
+  });
+});
