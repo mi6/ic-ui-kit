5b29e4b215e7091183790969ead977970022ab18
jd239 <116016939+jd239@users.noreply.github.com>
2024-02-20 22:19:13 +0000

    test(canary-react): added Cypress tests for Date Input
    - Added Cypress test for Date Input
    - Added new Cypress regression tests for Date Input generated via CI
    
    re #1364

--- packages/canary-react/cypress.config.ts
+++ packages/canary-react/cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 import { config } from '../react/cypress.config'
 
 export default defineConfig({
+  env: {
+    ci: process.env.CYPRESS_CI ?? false
+  },
   component: {
     ...config.component,
     supportFile: "../react/cypress/support/index.ts",
