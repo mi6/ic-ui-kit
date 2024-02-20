b362a357a6882ffb0fcd6cdbf05f93c12454c09c
jd239 <116016939+jd239@users.noreply.github.com>
2024-02-20 22:19:13 +0000

    test(canary-react): added Cypress tests for Date Input
    - Added Cypress test for Date Input
    - Added new Cypress regression tests for Date Input generated via CI
    
    re #1364

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -9,4 +9,5 @@ import { defineCustomElements } from '@ukic/canary-web-components/loader';
 
 defineCustomElements();
 export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
+export const IcDateInput = /*@__PURE__*/createReactComponent<JSX.IcDateInput, HTMLIcDateInputElement>('ic-date-input');
 export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
