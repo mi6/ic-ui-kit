58e52d94e3b60a24329b23c075b405db9e460522
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-03-06 12:45:08 +0000

    perf(canary-react): add import for icds.css to Storybook config

--- packages/canary-react/.storybook/preview.js
+++ packages/canary-react/.storybook/preview.js
@@ -1,6 +1,7 @@
 import "@ukic/web-components/dist/core/normalize.css";
 import "@ukic/fonts/dist/fonts.css";
 import "@ukic/web-components/dist/core/core.css";
+import "../../canary-web-components/src/global/icds.css";
 
 export const parameters = {
     controls: { 
