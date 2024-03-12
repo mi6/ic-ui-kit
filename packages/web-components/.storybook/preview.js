874b82d6ee1416a561fafd8c979c0ff08c73ddd4
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2024-03-12 10:56:09 +0000

    revert(web-components): removed storybook dark color-scheme
    since we do not support dark mode yet, dark color-scheme has been removed to prevent our components
    from breaking

--- packages/web-components/.storybook/preview.js
+++ packages/web-components/.storybook/preview.js
@@ -1,7 +1,6 @@
 import "../src/global/normalize.css"
 import "@ukic/fonts/dist/fonts.css";
 import "../src/global/icds.css";
-import "./storybook.css";
 
 import { defineCustomElements } from '../dist/esm/loader';
 
