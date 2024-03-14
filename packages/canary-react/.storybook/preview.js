709cd16f36b1d84a6e9cf30b574a4841553d9805
jd239 <116016939+jd239@users.noreply.github.com>
2024-03-14 14:20:52 +0000

    refactor(canary-react): regenerated components.ts file
    regenerated components.ts file

--- packages/canary-react/.storybook/preview.js
+++ packages/canary-react/.storybook/preview.js
@@ -1,7 +1,6 @@
-import "@ukic/web-components/dist/core/normalize.css";
+import "../dist/core/normalize.css";
 import "@ukic/fonts/dist/fonts.css";
-import "@ukic/web-components/dist/core/core.css";
-import "../../canary-web-components/src/global/icds.css";
+import "../dist/core/core.css";
 
 export const parameters = {
     controls: { 
--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -9,7 +9,7 @@ import { defineCustomElements } from '@ukic/canary-web-components/loader';
 
 defineCustomElements();
 export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
+export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
 export const IcMenuWithMulti = /*@__PURE__*/createReactComponent<JSX.IcMenuWithMulti, HTMLIcMenuWithMultiElement>('ic-menu-with-multi');
 export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
 export const IcSelectWithMulti = /*@__PURE__*/createReactComponent<JSX.IcSelectWithMulti, HTMLIcSelectWithMultiElement>('ic-select-with-multi');
-export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
