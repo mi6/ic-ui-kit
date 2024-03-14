6cf3970be2e445e97e748e6e9bd752b861381d53
jd239 <116016939+jd239@users.noreply.github.com>
2024-03-14 14:20:52 +0000

    refactor(canary-react): regenerated components.ts file
    regenerated components.ts file

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -11,7 +11,7 @@ defineCustomElements();
 export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
 export const IcDateInput = /*@__PURE__*/createReactComponent<JSX.IcDateInput, HTMLIcDateInputElement>('ic-date-input');
 export const IcDatePicker = /*@__PURE__*/createReactComponent<JSX.IcDatePicker, HTMLIcDatePickerElement>('ic-date-picker');
+export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
 export const IcMenuWithMulti = /*@__PURE__*/createReactComponent<JSX.IcMenuWithMulti, HTMLIcMenuWithMultiElement>('ic-menu-with-multi');
 export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
 export const IcSelectWithMulti = /*@__PURE__*/createReactComponent<JSX.IcSelectWithMulti, HTMLIcSelectWithMultiElement>('ic-select-with-multi');
-export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
