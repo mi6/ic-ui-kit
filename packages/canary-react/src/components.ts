96201e8982ef490942f44da8ac1ef4e2884eb0cc
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-03-19 11:53:15 +0000

    test(canary-react): add data table cypress tests and prettier fixes

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -1,15 +1,30 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
+import { createReactComponent } from "./react-component-lib";
 
-import type { JSX } from '@ukic/canary-web-components';
+import type { JSX } from "@ukic/canary-web-components";
 
-import { defineCustomElements } from '@ukic/canary-web-components/loader';
+import { defineCustomElements } from "@ukic/canary-web-components/loader";
 
 defineCustomElements();
-export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
-export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
-export const IcMenuWithMulti = /*@__PURE__*/createReactComponent<JSX.IcMenuWithMulti, HTMLIcMenuWithMultiElement>('ic-menu-with-multi');
-export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
-export const IcSelectWithMulti = /*@__PURE__*/createReactComponent<JSX.IcSelectWithMulti, HTMLIcSelectWithMultiElement>('ic-select-with-multi');
+export const IcDataTable = /*@__PURE__*/ createReactComponent<
+  JSX.IcDataTable,
+  HTMLIcDataTableElement
+>("ic-data-table");
+export const IcDataTableTitleBar = /*@__PURE__*/ createReactComponent<
+  JSX.IcDataTableTitleBar,
+  HTMLIcDataTableTitleBarElement
+>("ic-data-table-title-bar");
+export const IcMenuWithMulti = /*@__PURE__*/ createReactComponent<
+  JSX.IcMenuWithMulti,
+  HTMLIcMenuWithMultiElement
+>("ic-menu-with-multi");
+export const IcPaginationBar = /*@__PURE__*/ createReactComponent<
+  JSX.IcPaginationBar,
+  HTMLIcPaginationBarElement
+>("ic-pagination-bar");
+export const IcSelectWithMulti = /*@__PURE__*/ createReactComponent<
+  JSX.IcSelectWithMulti,
+  HTMLIcSelectWithMultiElement
+>("ic-select-with-multi");
