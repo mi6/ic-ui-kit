60be49decaf6952fbb122a7b7d3e31704b1b6465
ad9242 <116102637+ad9242@users.noreply.github.com>
2024-04-15 12:46:07 +0000

    fix(canary-web-components): fixes required prop change not updating label
    fixes issue where changing the required prop did not add\remove asterisk

--- packages/canary-web-components/src/components/ic-date-input/test/basic/ic-date-input.spec.tsx
+++ packages/canary-web-components/src/components/ic-date-input/test/basic/ic-date-input.spec.tsx
@@ -32,6 +32,11 @@ describe("ic-date-input component", () => {
     });
 
     expect(page.root).toMatchSnapshot();
+
+    page.root.required = false;
+    await page.waitForChanges();
+
+    expect(page.root).toMatchSnapshot("required-prop-false");
   });
 
   it("should render as disabled", async () => {
