e7d7dd96e8ca0b1cdbf3326726087c3993d96483
ad9242 <116102637+ad9242@users.noreply.github.com>
2024-06-18 10:27:40 +0000

    test(web-components): adds coverage check for utils files
    adds checks for 80% coverage for utils code

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -57,6 +57,12 @@ export const config: Config = {
         functions: 80,
         lines: 80,
         statements: 80,
+      },
+      './src/utils/*.ts': {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
       }
     },
     setupFilesAfterEnv: [
