a655eb3a5d958c175cfc58506cba83756b9886e9
mi6-286 <165915357+MI6-286@users.noreply.github.com>
2024-06-28 09:56:53 +0000

    test(react): adding cypress visual regression tests for IcSwitch
    adding cypress visual regression tests for IcSwitch

--- packages/react/src/component-tests/IcSwitch/IcSwitchTestData.tsx
+++ packages/react/src/component-tests/IcSwitch/IcSwitchTestData.tsx
@@ -39,3 +39,82 @@ export const Uncontrolled = () => {
   };
   return <IcSwitch ref={switchEl} label="Test" onIcChange={handleChange} />;
 };
+
+export const Default = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch label="Label" />
+    </div>
+  );
+};
+
+export const State = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch showState label="Label" />
+    </div>
+  );
+};
+
+export const Checked = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch checked showState label="Label" />
+    </div>
+  );
+};
+
+export const Disabled = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch disabled showState label="Label" />
+      <IcSwitch disabled checked showState label="Label" />
+    </div>
+  );
+};
+
+export const Small = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch size="small" showState label="Label" />
+    </div>
+  );
+};
+
+export const RightAdornment = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch label="Right adornment">
+        <svg
+          slot="right-adornment"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8 8C10.21 8 12 6.21 12 4C12 1.79 10.21 0 8 0C5.79 0 4 1.79 4 4C4 6.21 5.79 8 8 8ZM8 10C5.33 10 0 11.34 0 14V16H16V14C16 11.34 10.67 10 8 10Z"
+            fill="black"
+          />
+        </svg>
+      </IcSwitch>
+    </div>
+  );
+};
+
+export const HelperText = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch label="Label" helperText="This is some helper text" />
+    </div>
+  );
+};
+
+export const HiddenLabel = () => {
+  return (
+    <div style={{ padding: "10px" }}>
+      <IcSwitch label="Label" hideLabel />
+    </div>
+  );
+};
