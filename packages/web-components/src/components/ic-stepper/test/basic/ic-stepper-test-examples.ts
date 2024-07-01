bda480ba4e813fbfb42f39e105053b8ca0aed0f0
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-07-01 10:32:20 +0100

    feat(web-components): improve component prop names
    Improve component prop names for accordion group, footer link group, badge, empty state, input label, pagination, search bar, select, step and toggle button
    
    BREAKING CHANGE: Accordion group and footer link group “group-title” renamed to “label”, badge “text-label” renamed to “label”, empty state “body-max-lines” renamed to “max-lines”, input label “error” renamed to “status” with error as an option, pagination “boundary-count” and “adjacent-count” renamed to “boundary-page-count” and “adjacent-page-count”, search bar and select “disable-filter” renamed to “disable-auto-filtering”, search bar “hint-text” renamed to “assistive-hint-text”, step props had the word “step” removed from the start e.g “step-status” renamed to “status”, toggle button “toggle-checked” renamed to “checked”.
    
    . #662

--- packages/web-components/src/components/ic-stepper/test/basic/ic-stepper-test-examples.ts
+++ packages/web-components/src/components/ic-stepper/test/basic/ic-stepper-test-examples.ts
@@ -1,76 +1,76 @@
 export const defaultStepper = `
     <ic-stepper>
-        <ic-step step-title="First"></ic-step>
+        <ic-step heading="First"></ic-step>
         <ic-step
-        step-title="Second With a Very Long Title"
-        step-subtitle="Optional Subtitle"
-        step-type="current"
+        heading="Second With a Very Long Title"
+        subheading="Optional Subtitle"
+        type="current"
         ></ic-step>
-        <ic-step step-title="Third" step-type="disabled"></ic-step>
+        <ic-step heading="Third" type="disabled"></ic-step>
         <ic-step
-        step-title="Fourth"
-        step-subtitle="Optional Subtitle"
-        step-type="completed"
+        heading="Fourth"
+        subheading="Optional Subtitle"
+        type="completed"
         ></ic-step>
     </ic-stepper>`;
 
 export const customConnectorWidthStepper = `
     <ic-stepper aligned="left" connector-width="150">
-        <ic-step step-title="First"></ic-step>
+        <ic-step heading="First"></ic-step>
         <ic-step
-        step-title="Second With a Very Long Title"
-        step-subtitle="Optional Subtitle"
-        step-type="current"
+        heading="Second With a Very Long Title"
+        subheading="Optional Subtitle"
+        type="current"
         ></ic-step>
-        <ic-step step-title="Third" step-type="disabled"></ic-step>
+        <ic-step heading="Third" type="disabled"></ic-step>
         <ic-step
-        step-title="Fourth"
-        step-subtitle="Optional Subtitle"
-        step-type="completed"
+        heading="Fourth"
+        subheading="Optional Subtitle"
+        type="completed"
         ></ic-step>
     </ic-stepper>`;
 
 export const invalidConnectorWidthStepper = `
     <ic-stepper aligned="left" connector-width="96">
-        <ic-step step-title="First"></ic-step>
+        <ic-step heading="First"></ic-step>
         <ic-step
-        step-title="Second With a Very Long Title"
-        step-subtitle="Optional Subtitle"
-        step-type="current"
+        heading="Second With a Very Long Title"
+        subheading="Optional Subtitle"
+        type="current"
         ></ic-step>
-        <ic-step step-title="Third" step-type="disabled"></ic-step>
+        <ic-step heading="Third" type="disabled"></ic-step>
         <ic-step
-        step-title="Fourth"
-        step-subtitle="Optional Subtitle"
-        step-type="completed"
+        heading="Fourth"
+        subheading="Optional Subtitle"
+        type="completed"
         ></ic-step>
     </ic-stepper>`;
 
 export const compactStepper = `
     <ic-stepper variant="compact" id="custom-compact-stepper">
         <ic-step 
-        step-title="First"
+        heading="First"
         ></ic-step>
         <ic-step
-          step-title="Second With a Very Long Title"
-          step-subtitle="Optional subtitle that is long and should wrap"
+          heading="Second With a Very Long Title"
+          subheading="Optional subtitle that is long and should wrap"
           current
-          step-type="current"
+          type="current"
         ></ic-step>
         <ic-step
-          step-title="Third"
-          step-type="disabled"
+          heading="Third"
+          type="disabled"
         ></ic-step>
         <ic-step
-          step-title="Fourth title that is long and should wrap"
-          step-subtitle="Optional Subtitle"
-          step-type="completed"
+          heading="Fourth title that is long and should wrap"
+          subheading="Optional Subtitle"
+          type="completed"
         ></ic-step>
         <ic-step
-          step-title="Fifth and final step"
-          step-subtitle="Optional Subtitle"
+          heading="Fifth and final step"
+          subheading="Optional Subtitle"
           icon
-          step-status="optional"
-          step-type="completed"
+          status="optional"
+          type="completed"
         ></ic-step>
       </ic-stepper>`;
