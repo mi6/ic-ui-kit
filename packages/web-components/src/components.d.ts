c2f796fec3fd02388045318a7bc8df6b2fa19408
GCHQ-Developer-847 <111882109+GCHQ-Developer-847@users.noreply.github.com>
2024-07-02 10:44:27 +0100

    feat(web-components): update sideNavExpanded and topNavResized event names to have "ic" prefix
    Update sideNavExpanded and topNavResized event names to have "ic" prefix, to be consistent with
    other event names.
    
    BREAKING CHANGE: The events called "sideNavExpanded" and "topNavResized" are now called
    "icSideNavExpanded" and "icTopNavResized".
    
    .#1354

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -3062,7 +3062,7 @@ declare global {
         new (): HTMLIcSelectElement;
     };
     interface HTMLIcSideNavigationElementEventMap {
-        "sideNavExpanded": IcExpandedDetail;
+        "icSideNavExpanded": IcExpandedDetail;
     }
     interface HTMLIcSideNavigationElement extends Components.IcSideNavigation, HTMLStencilElement {
         addEventListener<K extends keyof HTMLIcSideNavigationElementEventMap>(type: K, listener: (this: HTMLIcSideNavigationElement, ev: IcSideNavigationCustomEvent<HTMLIcSideNavigationElementEventMap[K]>) => any, options?: boolean | AddEventListenerOptions): void;
@@ -3291,7 +3291,7 @@ declare global {
     interface HTMLIcTopNavigationElementEventMap {
         "icNavigationMenuClosed": void;
         "icNavigationMenuOpened": void;
-        "topNavResized": { size: number };
+        "icTopNavResized": { size: number };
     }
     interface HTMLIcTopNavigationElement extends Components.IcTopNavigation, HTMLStencilElement {
         addEventListener<K extends keyof HTMLIcTopNavigationElementEventMap>(type: K, listener: (this: HTMLIcTopNavigationElement, ev: IcTopNavigationCustomEvent<HTMLIcTopNavigationElementEventMap[K]>) => any, options?: boolean | AddEventListenerOptions): void;
@@ -5236,7 +5236,7 @@ declare namespace LocalJSX {
         /**
           * Emitted when the side navigation is collapsed and expanded.
          */
-        "onSideNavExpanded"?: (event: IcSideNavigationCustomEvent<IcExpandedDetail>) => void;
+        "onIcSideNavExpanded"?: (event: IcSideNavigationCustomEvent<IcExpandedDetail>) => void;
         /**
           * The short title of the app to be displayed at small screen sizes in place of the app title.
          */
@@ -5819,7 +5819,7 @@ declare namespace LocalJSX {
         /**
           * Emitted when the top navigation is resized.
          */
-        "onTopNavResized"?: (event: IcTopNavigationCustomEvent<{ size: number }>) => void;
+        "onIcTopNavResized"?: (event: IcTopNavigationCustomEvent<{ size: number }>) => void;
         /**
           * The short title of the app to be displayed at small screen sizes in place of the app title.
          */
