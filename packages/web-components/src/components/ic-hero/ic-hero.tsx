a148265231485dfdd4f4f1fa472718b64208b9a8
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-07-09 08:39:09 +0100

    refactor(web-components): update components with class names on host to be more specific
    Update components with class names on host to be more specific by including the component name in
    the class name
    
    BREAKING CHANGE: Class names that are on the host for most component have been updated to include
    the component name in the class name e.g. disabled updated to ic-select-disabled. Components updated
    are: accordion, accordion group, badge, breadcrumb, breadcrumb group, button, checkbox, checkbox
    group, classification banner, data entity, data row, dialog, empty state, footer, hero, horizontal
    scroll, input component container, input label, input validation, link, loading indicator, menu,
    navigation group, page header, popover menu, radio group, radio option, search bar, select, side
    nav, skeleton, step, stepper, tab, tab group, text field, toast, toggle button, toggle button group,
    and typography
    
    . #1156

--- packages/web-components/src/components/ic-hero/ic-hero.tsx
+++ packages/web-components/src/components/ic-hero/ic-hero.tsx
@@ -180,10 +180,10 @@ export class Hero {
     return (
       <Host
         class={{
-          [IcThemeForegroundEnum.Dark]:
+          [`ic-hero-${IcThemeForegroundEnum.Dark}`]:
             foregroundColor === IcThemeForegroundEnum.Dark,
           ["has-background-image"]: backgroundImage !== undefined,
-          ["small"]: small || size === "small",
+          ["ic-hero-small"]: small || size === "small",
           ["secondary-heading"]: !!secondaryHeading,
         }}
         style={style}
