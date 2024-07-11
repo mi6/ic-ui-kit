cc3ee7c8da110de599c5d476b73dbd38dd2f42db
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2024-07-11 10:39:16 +0000

    fix(web-components): set heading levels on Hero component
    Set heading prop to h2 and secondaryHeading to h3 semantic elements in Hero component
    
    BREAKING CHANGE: Heading heirarchy of ic-hero has changed, which might affect accessibility
    compliance when integrated. Removed deprecated 'small' prop from ic-hero
    
    re #1687

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -813,11 +813,11 @@ export namespace Components {
          */
         "disableBackgroundParallax"?: boolean;
         /**
-          * The heading of the hero.
+          * The heading of the hero. An <h2> level heading.
          */
         "heading": string;
         /**
-          * The optional secondary heading, replaced by slotted right content.
+          * The optional secondary heading, an <h3> level heading. Replaced by slotted right content.
          */
         "secondaryHeading"?: string;
         /**
@@ -828,10 +828,6 @@ export namespace Components {
           * The size of the hero component.
          */
         "size"?: IcSizesNoLarge;
-        /**
-          * @deprecated This prop should not be used anymore. Set prop `size` to "small" instead.
-         */
-        "small"?: boolean;
         /**
           * The description for the hero.
          */
@@ -4142,11 +4138,11 @@ declare namespace LocalJSX {
          */
         "disableBackgroundParallax"?: boolean;
         /**
-          * The heading of the hero.
+          * The heading of the hero. An <h2> level heading.
          */
         "heading"?: string;
         /**
-          * The optional secondary heading, replaced by slotted right content.
+          * The optional secondary heading, an <h3> level heading. Replaced by slotted right content.
          */
         "secondaryHeading"?: string;
         /**
@@ -4157,10 +4153,6 @@ declare namespace LocalJSX {
           * The size of the hero component.
          */
         "size"?: IcSizesNoLarge;
-        /**
-          * @deprecated This prop should not be used anymore. Set prop `size` to "small" instead.
-         */
-        "small"?: boolean;
         /**
           * The description for the hero.
          */
