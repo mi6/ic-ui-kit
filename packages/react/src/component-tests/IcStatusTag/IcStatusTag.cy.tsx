561055826ca3b6ba50712675fc43e5b62502eecb
ad9242 <116102637+ad9242@users.noreply.github.com>
2024-07-12 08:59:16 +0000

    fix(react): cypress test update after tooltip change
    updates a couple of baseline images after tooltip change

--- packages/react/src/component-tests/IcStatusTag/IcStatusTag.cy.tsx
+++ packages/react/src/component-tests/IcStatusTag/IcStatusTag.cy.tsx
@@ -95,7 +95,7 @@ describe("IcStatusTag visual regression and a11y tests", () => {
     cy.checkA11yWithWait();
     cy.compareSnapshot({
       name: "large",
-      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD + 0.021),
+      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD + 0.033),
     });
   });
 });
