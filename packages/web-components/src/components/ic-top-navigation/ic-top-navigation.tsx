19ce5284863f9e911bf16fc20ab0d9c2e175eec1
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-07-31 08:21:37 +0100

    feat(web-components): remove deprecated props/slots/events/methods
    Remove deprecated props/slots/events/methods and remove references to deprecated props in
    code/stories/tests
    
    BREAKING CHANGE: Deprecated props/slots/events/methods have been removed entirely.
    
    . #230

--- packages/web-components/src/components/ic-top-navigation/ic-top-navigation.tsx
+++ packages/web-components/src/components/ic-top-navigation/ic-top-navigation.tsx
@@ -469,7 +469,7 @@ export class TopNavigation {
                                 viewBox="0 0 24 24"
                                 width="24px"
                                 fill="#ffffff"
-                                slot="icon"
+                                slot="left-icon"
                               >
                                 <path d="M0 0h24v24H0V0z" fill="none" />
                                 <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
