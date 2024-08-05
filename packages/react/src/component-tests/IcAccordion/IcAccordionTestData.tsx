fdeae26687c4fa0493a8aaa63d85c3a9862f1ac1
mi6-286 <165915357+MI6-286@users.noreply.github.com>
2024-08-05 14:36:29 +0000

    test(react): update test data files to use v3 prop names and fix failing tests
    update IcAccordionTestData and IcBadgeTestData inline with v3 prop name changes and update failing Cypress tests
    
    BREAKING CHANGE: Accordion group 'groupTitle' renamed to 'label' and badge 'textLabel' renamed to
    'label'

--- packages/react/src/component-tests/IcAccordion/IcAccordionTestData.tsx
+++ packages/react/src/component-tests/IcAccordion/IcAccordionTestData.tsx
@@ -57,7 +57,7 @@ export const SlottedHeadingAccordion = (): ReactElement => (
 export const WithIcon = (): ReactElement => {
   return (
     <IcAccordion heading="Accordion 1">
-      <SlottedSVG
+      <svg
         slot="icon"
         width="20"
         height="1em"
@@ -66,7 +66,7 @@ export const WithIcon = (): ReactElement => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
-      </SlottedSVG>
+      </svg>
       Text
     </IcAccordion>
   );
@@ -101,13 +101,13 @@ export const DifferentSizes = (): ReactElement => {
 export const DifferentSizesGroup = (): ReactElement => {
   return (
     <>
-      <IcAccordionGroup size="small" groupTitle="Small">
+      <IcAccordionGroup size="small" label="Small">
         <TwoAccordions />
       </IcAccordionGroup>
-      <IcAccordionGroup groupTitle="Default">
+      <IcAccordionGroup label="Default">
         <TwoAccordions />
       </IcAccordionGroup>
-      <IcAccordionGroup size="large" groupTitle="Large">
+      <IcAccordionGroup size="large" label="Large">
         <TwoAccordions />
       </IcAccordionGroup>
     </>
