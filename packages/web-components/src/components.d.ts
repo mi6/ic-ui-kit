08c3f017c044695594f8e823fc1e89c8ed0bf164
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-08-07 14:45:16 +0100

    refactor(web-components): form attributes removed from checkbox, radio option and select
    Form attributes removed from checkbox, radio option and select. Update to components.d.ts and readme
    files to reflect changes
    
    BREAKING CHANGE: Props formaction, formenctype, formmethod, formnovalidate and formtarget removed
    from checkbox, radio option and select
    
    . #2000

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -431,26 +431,6 @@ export namespace Components {
          */
         "form"?: string;
         /**
-          * The URL that processes the information submitted by the checkbox. It overrides the action attribute of the checkbox's form owner. Does nothing if there is no form owner.
-         */
-        "formaction"?: string;
-        /**
-          * The way the submitted form data is encoded.
-         */
-        "formenctype"?: string;
-        /**
-          * The HTTP method used to submit the form.
-         */
-        "formmethod"?: string;
-        /**
-          * If `true`, the form will not be validated when submitted.
-         */
-        "formnovalidate"?: boolean;
-        /**
-          * The place to display the response from submitting the form. It overrides the target attribute of the checkbox's form owner.
-         */
-        "formtarget"?: string;
-        /**
           * The group label for the checkbox.
          */
         "groupLabel": string;
@@ -1378,26 +1358,6 @@ export namespace Components {
          */
         "form"?: string;
         /**
-          * The URL that processes the information submitted by the radio. It overrides the action attribute of the radio's form owner. Does nothing if there is no form owner.
-         */
-        "formaction"?: string;
-        /**
-          * The way the submitted form data is encoded.
-         */
-        "formenctype"?: string;
-        /**
-          * The HTTP method used to submit the form.
-         */
-        "formmethod"?: string;
-        /**
-          * If `true`, the form will not be validated when submitted.
-         */
-        "formnovalidate"?: boolean;
-        /**
-          * The place to display the response from submitting the form. It overrides the target attribute of the radio's form owner.
-         */
-        "formtarget"?: string;
-        /**
           * The group label for the radio option.
          */
         "groupLabel": string;
@@ -1580,26 +1540,6 @@ export namespace Components {
           * The <form> element to associate the select with.
          */
         "form"?: string;
-        /**
-          * The URL that processes the information submitted by the select. It overrides the action attribute of the select's form owner. Does nothing if there is no form owner. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formaction"?: string;
-        /**
-          * The way the submitted form data is encoded. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formenctype"?: string;
-        /**
-          * The HTTP method used to submit the form. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formmethod"?: string;
-        /**
-          * If `true`, the form will not be validated when submitted. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formnovalidate"?: boolean;
-        /**
-          * The place to display the response from submitting the form. It overrides the target attribute of the select's form owner. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formtarget"?: string;
         /**
           * If `true`, the select element will fill the width of the container. This prop should only be used with searchable select and will only be applied if searchable is true.
          */
@@ -3620,26 +3560,6 @@ declare namespace LocalJSX {
          */
         "form"?: string;
         /**
-          * The URL that processes the information submitted by the checkbox. It overrides the action attribute of the checkbox's form owner. Does nothing if there is no form owner.
-         */
-        "formaction"?: string;
-        /**
-          * The way the submitted form data is encoded.
-         */
-        "formenctype"?: string;
-        /**
-          * The HTTP method used to submit the form.
-         */
-        "formmethod"?: string;
-        /**
-          * If `true`, the form will not be validated when submitted.
-         */
-        "formnovalidate"?: boolean;
-        /**
-          * The place to display the response from submitting the form. It overrides the target attribute of the checkbox's form owner.
-         */
-        "formtarget"?: string;
-        /**
           * The group label for the checkbox.
          */
         "groupLabel"?: string;
@@ -4574,26 +4494,6 @@ declare namespace LocalJSX {
          */
         "form"?: string;
         /**
-          * The URL that processes the information submitted by the radio. It overrides the action attribute of the radio's form owner. Does nothing if there is no form owner.
-         */
-        "formaction"?: string;
-        /**
-          * The way the submitted form data is encoded.
-         */
-        "formenctype"?: string;
-        /**
-          * The HTTP method used to submit the form.
-         */
-        "formmethod"?: string;
-        /**
-          * If `true`, the form will not be validated when submitted.
-         */
-        "formnovalidate"?: boolean;
-        /**
-          * The place to display the response from submitting the form. It overrides the target attribute of the radio's form owner.
-         */
-        "formtarget"?: string;
-        /**
           * The group label for the radio option.
          */
         "groupLabel"?: string;
@@ -4813,26 +4713,6 @@ declare namespace LocalJSX {
           * The <form> element to associate the select with.
          */
         "form"?: string;
-        /**
-          * The URL that processes the information submitted by the select. It overrides the action attribute of the select's form owner. Does nothing if there is no form owner. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formaction"?: string;
-        /**
-          * The way the submitted form data is encoded. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formenctype"?: string;
-        /**
-          * The HTTP method used to submit the form. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formmethod"?: string;
-        /**
-          * If `true`, the form will not be validated when submitted. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formnovalidate"?: boolean;
-        /**
-          * The place to display the response from submitting the form. It overrides the target attribute of the select's form owner. This prop should only be used with searchable select and will only be applied if searchable is true.
-         */
-        "formtarget"?: string;
         /**
           * If `true`, the select element will fill the width of the container. This prop should only be used with searchable select and will only be applied if searchable is true.
          */
