a6e599a65e3a8a2d577c8dd0db4704526747258b
ad9242 <116102637+ad9242@users.noreply.github.com>
2024-08-12 08:16:16 +0000

    feat(react): adds theme playground for testing dark mode
    adds storybook playground for testing ic-theme component

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,6 +1,8 @@
 import "../dist/core/normalize.css";
 import "@ukic/fonts/dist/fonts.css";
 import "../dist/core/core.css";
+import "./storybook-overrides.css";
+
 import { withPerformance } from 'storybook-addon-performance';
 
 export const parameters = {
