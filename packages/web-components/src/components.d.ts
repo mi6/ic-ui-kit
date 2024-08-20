317c089110d61a401aa414cec64e5364a787f5ad
MI6-255 <90764648+MI6-255@users.noreply.github.com>
2024-08-20 14:53:14 +0100

    Revert "Dark mode prep"

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -1984,10 +1984,6 @@ export namespace Components {
           * The theme colour. Can be a hex value e.g. "#ff0000", RGB e.g. "rgb(255, 0, 0)", or RGBA e.g. "rgba(255, 0, 0, 1)".
          */
         "color"?: IcColor;
-        /**
-          * The theme mode. Can be "dark", "light", or "auto". "auto" will use the device or browser settings.
-         */
-        "mode": "dark" | "light" | "auto";
     }
     interface IcToast {
         /**
@@ -5227,10 +5223,6 @@ declare namespace LocalJSX {
           * The theme colour. Can be a hex value e.g. "#ff0000", RGB e.g. "rgb(255, 0, 0)", or RGBA e.g. "rgba(255, 0, 0, 1)".
          */
         "color"?: IcColor;
-        /**
-          * The theme mode. Can be "dark", "light", or "auto". "auto" will use the device or browser settings.
-         */
-        "mode"?: "dark" | "light" | "auto";
         "onThemeChange"?: (event: IcThemeCustomEvent<IcTheme>) => void;
     }
     interface IcToast {
