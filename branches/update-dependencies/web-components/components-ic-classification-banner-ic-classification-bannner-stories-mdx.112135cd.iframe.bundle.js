6da3bb78018b21c69e5bbecaed9583c47796e94a
GCHQ-Developer-112 <GCHQ-Developer-112@users.noreply.github.com>
2024-08-28 18:04:51 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@0f0184b3f34ae22a8596d8eab1bf27f03f913b22 🚀

---
+++ branches/update-dependencies/web-components/components-ic-classification-banner-ic-classification-bannner-stories-mdx.112135cd.iframe.bundle.js
@@ -0,0 +1,116 @@
+"use strict";(self.webpackChunk_ukic_web_components=self.webpackChunk_ukic_web_components||[]).push([[857],{"./src/components/ic-classification-banner/ic-classification-bannner.stories.mdx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{__namedExportsOrder:()=>__namedExportsOrder,additionalSelectors:()=>additionalSelectors,bottomOfPage:()=>bottomOfPage,classifications:()=>classifications,country:()=>country,default:()=>__WEBPACK_DEFAULT_EXPORT__,defaultArgs:()=>defaultArgs,playground:()=>playground,upTo:()=>upTo});__webpack_require__("./node_modules/react/index.js");var _home_runner_work_ic_ui_kit_ic_ui_kit_packages_web_components_node_modules_storybook_addon_docs_dist_shims_mdx_react_shim__WEBPACK_IMPORTED_MODULE_5__=__webpack_require__("./node_modules/@storybook/addon-docs/node_modules/@mdx-js/react/lib/index.js"),_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./node_modules/@storybook/addon-docs/dist/index.mjs"),lit_html__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("./node_modules/lit-html/lit-html.js"),_readme_md__WEBPACK_IMPORTED_MODULE_3__=__webpack_require__("./src/components/ic-classification-banner/readme.md"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__=__webpack_require__("./node_modules/react/jsx-runtime.js");const defaultArgs={additionalSelectors:"",classification:"official",country:"uk",inline:!0,upTo:!1};function _createMdxContent(props){const _components=Object.assign({h3:"h3"},(0,_home_runner_work_ic_ui_kit_ic_ui_kit_packages_web_components_node_modules_storybook_addon_docs_dist_shims_mdx_react_shim__WEBPACK_IMPORTED_MODULE_5__.RP)(),props.components);return(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.Fragment,{children:[(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.W8,{title:"Web Components/Classification banner",component:"ic-classification-banner"}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.VY,{markdown:_readme_md__WEBPACK_IMPORTED_MODULE_3__}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_components.h3,{id:"classifications",children:"Classifications"}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.Hl,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.gG,{name:"Classifications",parameters:{loki:{skip:!0}},children:lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner inline="true"></ic-classification-banner>
+      <ic-classification-banner
+        classification="official"
+        inline="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="official-sensitive"
+        inline="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="secret"
+        inline="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="top-secret"
+        inline="true"
+      ></ic-classification-banner>`})}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_components.h3,{id:"up-to-classifications",children:"Up to classifications"}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.Hl,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.gG,{name:"Up to",parameters:{loki:{skip:!0}},children:lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner inline="true"></ic-classification-banner>
+      <ic-classification-banner
+        classification="official"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="official-sensitive"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="secret"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="top-secret"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>`})}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_components.h3,{id:"bottom-of-page",children:"Bottom of page"}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.Hl,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.gG,{name:"Bottom of page",parameters:{loki:{skip:!0}},children:lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+      classification="official"
+    ></ic-classification-banner>`})}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_components.h3,{id:"country",children:"Country"}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.Hl,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.gG,{name:"Country",parameters:{loki:{skip:!0}},children:lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+        classification="official"
+        inline="true"
+        country="us"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="official"
+        inline="true"
+        country=""
+      ></ic-classification-banner>`})}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_components.h3,{id:"additional-selectors",children:"Additional selectors"}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.Hl,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.gG,{name:"Additional selectors",parameters:{loki:{skip:!0}},children:lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+      classification="official"
+      inline="true"
+      additional-selectors="ukic"
+    />`})}),"\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_components.h3,{id:"playground",children:"Playground"}),"\n","\n",(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.Hl,{children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_storybook_addon_docs__WEBPACK_IMPORTED_MODULE_1__.gG,{args:defaultArgs,parameters:{loki:{skip:!0}},argTypes:{classification:{options:["default","official","secret","official-sensitive","top-secret"],control:{type:"select"}}},name:"Playground",children:args=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+        classification=${args.classification}
+        inline=${args.inline}
+        up-to=${args.upTo}
+        country=${args.country}
+        additional-selectors=${args.additionalSelectors}
+      ></ic-classification-banner>`})})]})}const classifications=()=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner inline="true"></ic-classification-banner>
+      <ic-classification-banner
+        classification="official"
+        inline="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="official-sensitive"
+        inline="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="secret"
+        inline="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="top-secret"
+        inline="true"
+      ></ic-classification-banner>`;classifications.storyName="Classifications",classifications.parameters={storySource:{source:'html`<ic-classification-banner inline="true"></ic-classification-banner>\n      <ic-classification-banner\n        classification="official"\n        inline="true"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="official-sensitive"\n        inline="true"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="secret"\n        inline="true"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="top-secret"\n        inline="true"\n      ></ic-classification-banner>`'},loki:{skip:!0}};const upTo=()=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner inline="true"></ic-classification-banner>
+      <ic-classification-banner
+        classification="official"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="official-sensitive"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="secret"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="top-secret"
+        inline="true"
+        up-to="true"
+      ></ic-classification-banner>`;upTo.storyName="Up to",upTo.parameters={storySource:{source:'html`<ic-classification-banner inline="true"></ic-classification-banner>\n      <ic-classification-banner\n        classification="official"\n        inline="true"\n        up-to="true"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="official-sensitive"\n        inline="true"\n        up-to="true"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="secret"\n        inline="true"\n        up-to="true"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="top-secret"\n        inline="true"\n        up-to="true"\n      ></ic-classification-banner>`'},loki:{skip:!0}};const bottomOfPage=()=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+      classification="official"
+    ></ic-classification-banner>`;bottomOfPage.storyName="Bottom of page",bottomOfPage.parameters={storySource:{source:'html`<ic-classification-banner\n      classification="official"\n    ></ic-classification-banner>`'},loki:{skip:!0}};const country=()=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+        classification="official"
+        inline="true"
+        country="us"
+      ></ic-classification-banner>
+      <ic-classification-banner
+        classification="official"
+        inline="true"
+        country=""
+      ></ic-classification-banner>`;country.storyName="Country",country.parameters={storySource:{source:'html`<ic-classification-banner\n        classification="official"\n        inline="true"\n        country="us"\n      ></ic-classification-banner>\n      <ic-classification-banner\n        classification="official"\n        inline="true"\n        country=""\n      ></ic-classification-banner>`'},loki:{skip:!0}};const additionalSelectors=()=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+      classification="official"
+      inline="true"
+      additional-selectors="ukic"
+    />`;additionalSelectors.storyName="Additional selectors",additionalSelectors.parameters={storySource:{source:'html`<ic-classification-banner\n      classification="official"\n      inline="true"\n      additional-selectors="ukic"\n    />`'},loki:{skip:!0}};const playground=args=>lit_html__WEBPACK_IMPORTED_MODULE_2__.qy`<ic-classification-banner
+        classification=${args.classification}
+        inline=${args.inline}
+        up-to=${args.upTo}
+        country=${args.country}
+        additional-selectors=${args.additionalSelectors}
+      ></ic-classification-banner>`;playground.storyName="Playground",playground.argTypes={classification:{options:["default","official","secret","official-sensitive","top-secret"],control:{type:"select"}}},playground.args=defaultArgs,playground.parameters={storySource:{source:"args => html`<ic-classification-banner\n        classification=${args.classification}\n        inline=${args.inline}\n        up-to=${args.upTo}\n        country=${args.country}\n        additional-selectors=${args.additionalSelectors}\n      ></ic-classification-banner>`"},loki:{skip:!0}};const componentMeta={title:"Web Components/Classification banner",tags:["stories-mdx"],includeStories:["classifications","upTo","bottomOfPage","country","additionalSelectors","playground"]};componentMeta.parameters=componentMeta.parameters||{},componentMeta.parameters.docs={...componentMeta.parameters.docs||{},page:function MDXContent(props={}){const{wrapper:MDXLayout}=Object.assign({},(0,_home_runner_work_ic_ui_kit_ic_ui_kit_packages_web_components_node_modules_storybook_addon_docs_dist_shims_mdx_react_shim__WEBPACK_IMPORTED_MODULE_5__.RP)(),props.components);return MDXLayout?(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(MDXLayout,{...props,children:(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx)(_createMdxContent,{...props})}):_createMdxContent(props)}};const __WEBPACK_DEFAULT_EXPORT__=componentMeta,__namedExportsOrder=["defaultArgs","classifications","upTo","bottomOfPage","country","additionalSelectors","playground"]}}]);
+//# sourceMappingURL=components-ic-classification-banner-ic-classification-bannner-stories-mdx.112135cd.iframe.bundle.js.map
