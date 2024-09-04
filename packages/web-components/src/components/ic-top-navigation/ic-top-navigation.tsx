e5a83449215f174c7fbeb7d2bef14881a172a742
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-09-04 11:57:40 +0100

    fix(web-components): update search icon in top nav so that it displays in safari
    Update search icon in top nav so that it displays in safari
    
    . #2374

--- packages/web-components/src/components/ic-top-navigation/ic-top-navigation.tsx
+++ packages/web-components/src/components/ic-top-navigation/ic-top-navigation.tsx
@@ -33,6 +33,7 @@ import {
   isPropDefined,
 } from "../../utils/helpers";
 import { IcSearchBarBlurEventDetail } from "../ic-search-bar/ic-search-bar.types";
+import searchIcon from "../../assets/top-nav-search.svg";
 
 /**
  * @slot app-icon - Content will be rendered to left of app title. Anything that is slotted here will be hidden from screen readers.
@@ -418,17 +419,13 @@ export class TopNavigation {
                           aria-label={mobileSearchButtonTitle}
                           appearance={foregroundColor}
                           onClick={searchButtonClickHandler}
+                          innerHTML={
+                            !isSlotUsed(this.el, "toggle-icon")
+                              ? searchIcon
+                              : null
+                          }
                         >
-                          <slot name="toggle-icon">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 24 24"
-                              fill="#ffffff"
-                            >
-                              <path d="M0 0h24v24H0V0z" fill="none" />
-                              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-                            </svg>
-                          </slot>
+                          <slot name="toggle-icon"></slot>
                         </ic-button>
                       )}
 
