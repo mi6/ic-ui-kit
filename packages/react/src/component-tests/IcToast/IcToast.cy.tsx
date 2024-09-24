404594f104e278388e0694884095a436935d8463
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2024-09-24 09:43:39 +0100

    feat(react): added test for new IcToastRegion componentDidLoad

--- packages/react/src/component-tests/IcToast/IcToast.cy.tsx
+++ packages/react/src/component-tests/IcToast/IcToast.cy.tsx
@@ -139,6 +139,11 @@ describe("IcToast end-to-end tests", () => {
       .find(DISMISS_BUTTON_SELECTOR)
       .should("exist");
   });
+  
+  it("should render a toast on page load if openToast is set early", () => {
+    mount(<AutoLoadToast />);
+    cy.get(IC_TOAST_SELECTOR).should(NOT_HAVE_CLASS, "hidden");
+  });
 });
 
 describe("IcToast visual regression and a11y tests", () => {
