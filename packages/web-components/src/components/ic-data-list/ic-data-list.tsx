2a68c41f67b6bdba8b878d9a619e666c52cde152
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2024-09-25 13:21:18 +0100

    feat(web-components): update data list and data row to have theme prop
    Update data list and data row to have theme prop to utilise light and dark mode
    
    Working towards #1214

--- packages/web-components/src/components/ic-data-list/ic-data-list.tsx
+++ packages/web-components/src/components/ic-data-list/ic-data-list.tsx
@@ -1,5 +1,5 @@
 import { Component, Element, Host, h, Prop } from "@stencil/core";
-import { IcSizesNoLarge } from "../../utils/types";
+import { IcSizesNoLarge, IcThemeMode } from "../../utils/types";
 
 /**
  * @slot heading - Content will be placed at the top of the data list.
@@ -22,8 +22,13 @@ export class DataList {
    */
   @Prop() size?: IcSizesNoLarge = "medium";
 
+  /**
+   * Sets the theme color to the dark or light theme color. "inherit" will set the color based on the system settings or ic-theme component.
+   */
+  @Prop() theme?: IcThemeMode = "inherit";
+
   render() {
-    const { el, heading, size } = this;
+    const { el, heading, size, theme } = this;
 
     if (size === "small") {
       Array.from(el.children).forEach((child) =>
@@ -32,7 +37,12 @@ export class DataList {
     }
 
     return (
-      <Host class={{ "ic-data-list-small": size === "small" }}>
+      <Host
+        class={{
+          "ic-data-list-small": size === "small",
+          [`ic-theme-${theme}`]: theme !== "inherit",
+        }}
+      >
         <div class="heading" id="data-list-heading">
           <slot name="heading">
             <ic-typography variant="h3">{heading}</ic-typography>
