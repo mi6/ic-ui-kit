edd9312686def02086764015ebe498ece7887453
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-10-09 16:30:22 +0100

    feat(web-components): add additional functionality to existing ic-divider component
    Add props to allow ic-divider to be more customisable, add stories & prop playground, add tests,
    update side navigation test following change to ic-divider
    
    BREAKING CHANGE: Change default styling and props on ic-divider
    
    . #1331

---
+++ packages/web-components/src/components/ic-divider/ic-divider.types.tsx
@@ -0,0 +1,8 @@
+export type IcDividerStyles = "solid" | "dashed";
+export type IcDividerWeights = "thin" | "medium" | "thick" | "very-thick";
+export type IcDividerLabelPlacement =
+  | "left"
+  | "center"
+  | "right"
+  | "top"
+  | "bottom";
