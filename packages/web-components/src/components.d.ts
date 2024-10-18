8f4b68c2b0b739efde2e87835bf7e1d173fa7aa5
ad9242 <116102637+ad9242@users.noreply.github.com>
2024-10-18 12:02:47 +0000

    fix(web-components): change to previous fix with hiding icon
    tweak to css applied based on attribute

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -139,7 +139,7 @@ export namespace Components {
          */
         "message"?: string;
         /**
-          * If `true`, the default icon for the neutral variant will be appear on the left of the alert.
+          * If `true`, the default icon for the neutral variant will appear on the left of the alert.
          */
         "showDefaultIcon": boolean;
         /**
@@ -3472,7 +3472,7 @@ declare namespace LocalJSX {
          */
         "onIcDismiss"?: (event: IcAlertCustomEvent<void>) => void;
         /**
-          * If `true`, the default icon for the neutral variant will be appear on the left of the alert.
+          * If `true`, the default icon for the neutral variant will appear on the left of the alert.
          */
         "showDefaultIcon"?: boolean;
         /**
