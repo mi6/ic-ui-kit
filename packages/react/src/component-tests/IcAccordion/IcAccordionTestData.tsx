684326d035f9fb655bede8ade4608bd4cfe3c471
GCHQ-Developer-847 <111882109+GCHQ-Developer-847@users.noreply.github.com>
2024-11-01 16:24:11 +0000

    feat(react): add theme prop to accordion component
    Replace "appearance" prop with "theme", add / update tests.
    
    BREAKING CHANGE: The "appearance" prop has been replaced with a prop called "theme" and the values
    have been swapped (i.e. the "dark" theme has white text, rather than "light").

--- packages/react/src/component-tests/IcAccordion/IcAccordionTestData.tsx
+++ packages/react/src/component-tests/IcAccordion/IcAccordionTestData.tsx
@@ -1,6 +1,19 @@
 import React, { ReactElement } from "react";
 import { IcAccordion, IcAccordionGroup, IcTypography } from "../../components";
 
+const Icon = (): ReactElement => (
+  <svg
+    slot="icon"
+    width="20"
+    height="1em"
+    viewBox="0 0 512 512"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+  </svg>
+);
+
 export const SimpleAccordion = (): ReactElement => (
   <IcAccordion heading="Accordion 1">Text</IcAccordion>
 );
@@ -56,16 +69,7 @@ export const SlottedHeadingAccordion = (): ReactElement => (
 export const WithIcon = (): ReactElement => {
   return (
     <IcAccordion heading="Accordion 1">
-      <svg
-        slot="icon"
-        width="20"
-        height="1em"
-        viewBox="0 0 512 512"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
-      </svg>
+      <Icon />
       Text
     </IcAccordion>
   );
@@ -110,5 +114,57 @@ export const DifferentSizesGroup = (): ReactElement => {
         <TwoAccordions />
       </IcAccordionGroup>
     </>
+  );
+};
+
+export const DarkTheme = (): ReactElement => {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <IcAccordion heading="Accordion 1" theme="dark">
+        Text
+      </IcAccordion>
+    </div>
+  );
+};
+
+export const DarkThemeGroup = (): ReactElement => {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <IcAccordionGroup theme="dark" label="Dark theme">
+        <IcAccordion heading="Accordion 1" />
+        <IcAccordion heading="Accordion 2" disabled />
+        <IcAccordion heading="Accordion 3" message="Text" expanded />
+      </IcAccordionGroup>
+    </div>
+  );
+};
+
+export const DarkThemeGroupSlottedContent = (): ReactElement => {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <IcAccordionGroup theme="dark">
+        <IcTypography variant="h4" slot="label">
+          Slotted group title
+        </IcTypography>
+        <IcAccordion>
+          <Icon />
+          <IcTypography variant="subtitle-large" slot="heading">
+            Slotted heading
+          </IcTypography>
+        </IcAccordion>
+        <IcAccordion disabled>
+          <Icon />
+          <IcTypography variant="subtitle-large" slot="heading">
+            Slotted heading
+          </IcTypography>
+        </IcAccordion>
+        <IcAccordion expanded>
+          <IcTypography variant="subtitle-large" slot="heading">
+            Slotted heading
+          </IcTypography>
+          <IcTypography variant="body">Text</IcTypography>
+        </IcAccordion>
+      </IcAccordionGroup>
+    </div>
   );
 };
