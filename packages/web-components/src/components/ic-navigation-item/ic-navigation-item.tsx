db44b23828d962ae0e87b3e447a4a848ba2b0245
lz405 <179817187+lz405@users.noreply.github.com>
2024-11-14 12:16:30 +0000

    feat(web-components): fix navigation items not inheriting expanded state
    fix navigation items not inheriting the expanded status of the parent side navigation element on
    load
    
    fix #2610

--- packages/web-components/src/components/ic-navigation-item/ic-navigation-item.tsx
+++ packages/web-components/src/components/ic-navigation-item/ic-navigation-item.tsx
@@ -144,6 +144,10 @@ export class NavigationItem {
     this.parentEl = parent;
     this.deviceSize = getCurrentDeviceSize();
 
+    this.sideNavExpanded =
+      this.parentEl?.classList.contains("sm-expanded") ||
+      this.parentEl?.classList.contains("xs-menu-open");
+
     if (this.navigationType === "side") {
       this.parentEl.addEventListener(
         "sideNavExpanded",
