da067a44b17410187e240cc4ab646bcdc7c67445
ad9242 <116102637+ad9242@users.noreply.github.com>
2024-12-02 16:04:49 +0000

    feat(react): adds tests & playground for theme prop
    adds tests and storybook playground for back-to-top theme prop
    
    BREAKING CHANGE: adds theme prop

--- packages/react/src/component-tests/IcBackToTop/IcBackToTopTestData.tsx
+++ packages/react/src/component-tests/IcBackToTop/IcBackToTopTestData.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { IcBackToTop } from "../../components";
 import { IcBackToTopVariants } from "@ukic/web-components";
 
-export const BackToTop = (props: { variant?: IcBackToTopVariants }) => {
-  const { variant } = props;
+export const BackToTop = (props: { variant?: IcBackToTopVariants; theme }) => {
+  const { variant, theme } = props;
   return (
     <div style={{ height: "120vh" }}>
       <div id="topEl" style={{ position: "absolute", top: "0", width: "100%" }}>
         Top of screen
       </div>
-      <IcBackToTop target="topEl" variant={variant} />
+      <IcBackToTop target="topEl" variant={variant} theme={theme} />
     </div>
   );
 };
