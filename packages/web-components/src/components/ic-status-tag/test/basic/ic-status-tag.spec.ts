e6dd0977f88c23b36d785028912727d82823d173
GCHQ-Developer-112 <113986285+GCHQ-Developer-112@users.noreply.github.com>
2024-12-06 12:12:31 +0000

    feat(web-components): add `uppercase` prop to ic-status-tag
    Add `uppercase` prop to ic-status-tag, update tests and stories following this change

--- packages/web-components/src/components/ic-status-tag/test/basic/ic-status-tag.spec.ts
+++ packages/web-components/src/components/ic-status-tag/test/basic/ic-status-tag.spec.ts
@@ -8,38 +8,26 @@ describe("ic-status-tag component renders label", () => {
       html: `<ic-status-tag label="Neutral"></ic-status-tag>`,
     });
 
-    expect(page.root).toEqualHtml(`
-    <ic-status-tag aria-label="Status" label="Neutral">
-    <mock:shadow-root>
-      <strong class="filled-neutral medium tag">
-        <ic-typography variant="label-uppercase">
-          <span>
-            Neutral
-          </span>
-        </ic-typography>
-      </strong>
-      </mock:shadow-root>
-    </ic-status-tag>
-    `);
-  }),
-    it("should have the role of 'status' if announced is passed as true", async () => {
-      const page = await newSpecPage({
-        components: [StatusTag],
-        html: `<ic-status-tag label="Neutral" announced="true"></ic-status-tag>`,
-      });
+    expect(page.root).toMatchSnapshot(`renders label`);
+  });
 
-      expect(page.root).toEqualHtml(`
-    <ic-status-tag announced="true" aria-label="Status" label="Neutral" role="status">
-    <mock:shadow-root>
-      <strong class="filled-neutral medium tag">
-        <ic-typography variant="label-uppercase">
-          <span>
-            Neutral
-          </span>
-        </ic-typography>
-      </strong>
-      </mock:shadow-root>
-    </ic-status-tag>
-    `);
+  it("should have the role of 'status' if announced is passed as true", async () => {
+    const page = await newSpecPage({
+      components: [StatusTag],
+      html: `<ic-status-tag label="Neutral" announced="true"></ic-status-tag>`,
     });
+
+    expect(page.root).toMatchSnapshot(`role of status if announced is true`);
+  });
+
+  it("should render a lowercase label when `uppercase` is set to false", async () => {
+    const page = await newSpecPage({
+      components: [StatusTag],
+      html: `<ic-status-tag label="Neutral" uppercase="false"></ic-status-tag>`,
+    });
+
+    expect(page.root).toMatchSnapshot(
+      `lowercase label when uppercase is false`
+    );
+  });
 });
