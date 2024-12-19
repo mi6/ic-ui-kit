d313066c8dd198d206797d12bcdbc14e2fa3801f
cd3859 <185913911+cd3859@users.noreply.github.com>
2024-12-19 18:00:29 +0000

    feat(codemod): codemod for v2 to v3
    codemod to help convert files to new v3 props and component names
    
    2064

---
+++ packages/codemod/codemod.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+import fs from "fs";
+import yargs from "yargs";
+import { searchDirectory } from "./sections/directory-search.js";
+import { compareComponent } from "./sections/component-changes.js";
+import { simpleTestComparison } from "./sections/simple-test-comparison.js";
+import htmlData from "./data/changes.js";
+import reactData from "./data/reactChanges.js";
+
+/**
+ *
+ * @param {*} filePath
+ * @returns list of files in the directory as strings
+ *
+ */
+function readFileLinesToArray(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    return data;
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return [];
+  }
+}
+
+/**
+ *
+ * @param {*} path
+ * @param {*} type
+ *
+ * main function for code mod, will convert v2 file format to v3 file format
+ */
+const main = async (path, test = false) => {
+  const files = await searchDirectory(path);
+  files.forEach((file) => {
+    const linesArray = readFileLinesToArray(file);
+    const isReact = file.includes(".tsx") || file.includes(".jsx");
+    if (test && (file.includes(".spec.") || file.includes(".test."))) {
+      const changedFile = simpleTestComparison(linesArray, [
+        ...htmlData,
+        ...reactData,
+      ]);
+      if (changedFile !== linesArray) {
+        fs.writeFile(file, changedFile, (err) => {
+          if (err) return console.log(err);
+          console.log(`${file.split("\\").slice(-1)} modified successfully`);
+        });
+      }
+    }
+    if (file.includes(`.cy.`)) {
+      const changedFile = compareComponent(linesArray, htmlData);
+      if (changedFile !== linesArray) {
+        fs.writeFile(file, changedFile, (err) => {
+          if (err) return console.log(err);
+          console.log(`${file.split("\\").slice(-1)} modified successfully`);
+        });
+      }
+    } else {
+      const changedComponentFile = compareComponent(
+        linesArray,
+        isReact ? reactData : htmlData
+      );
+      if (changedComponentFile !== linesArray) {
+        fs.writeFile(file, String(changedComponentFile), (err) => {
+          if (err) return console.log(err);
+          console.log(`${file.split("\\").slice(-1)} modified successfully`);
+        });
+      }
+    }
+  });
+};
+
+const cli = yargs(process.argv.slice(2));
+cli
+  .usage("Usage: node ./$0 [options]")
+  .example("node ./$0 --dir ./src", "execute codemod in src directory")
+  .option("dir", {
+    alias: "d",
+    describe: "Directory to migrate",
+    nargs: 1,
+    demandOption: "Directory to migrate required",
+    type: "string",
+  })
+  .option("test", {
+    alias: "t",
+    describe: "Run codemod on directory provided, if false, skip migration",
+    type: "boolean",
+    default: false,
+  })
+  .help("h")
+  .alias("h", "help")
+  .alias("v", "version")
+  .epilog("Created by ICDS ♥")
+  .parse();
+
+const { dir, test } = cli.argv;
+
+if (!dir) {
+  console.error("Please provide a directory path to the codemod");
+  process.exit(1);
+}
+
+await main(dir, test);
