7db1566298164c180facb6f22fa26c08885a6f76
BluePandaCard <194483540+BluePandaCard@users.noreply.github.com>
2025-01-17 09:05:15 +0000

    fix(react): add Cypress visual regression tests for ag grid wrapper
    Add Cypress visual regression tests for ag grid wrapper light and dark theme
    
    3031

---
+++ packages/react/src/component-tests/AgGridWrapper/AgGridWrapper.cy.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/jsx-no-bind */
+/// <reference types="Cypress" />
+
+import { mount } from "cypress/react";
+import React from "react";
+import { AGGridLight, AGGridDark } from "./AgGridWrapperTestData";
+import { setThresholdBasedOnEnv } from "../../../cypress/utils/helpers";
+
+const DEFAULT_TEST_THRESHOLD = 0;
+
+describe("Visual regression and a11y tests", () => {
+  beforeEach(() => {
+    cy.injectAxe();
+    cy.viewport(1024, 768);
+  });
+
+  afterEach(() => {
+    cy.task("generateReport");
+  });
+
+  it("should render", () => {
+    mount(<AGGridLight />);
+
+    cy.get("span.ag-header-expand-icon-collapsed").eq(1).click();
+
+    cy.checkA11yWithWait();
+    cy.compareSnapshot({
+      name: "ag-grid-light",
+      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD),
+    });
+  });
+
+  it("should render dark", () => {
+    mount(<AGGridDark />);
+
+    cy.get("span.ag-header-expand-icon-collapsed").eq(1).click();
+
+    cy.checkA11yWithWait();
+    cy.compareSnapshot({
+      name: "ag-grid-dark",
+      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD),
+    });
+  });
+});
