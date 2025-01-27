0b60f2cdfd194a56a94e378ac5df98892ed454d2
GCHQ-Developer-741 <117368893+GCHQ-Developer-741@users.noreply.github.com>
2025-01-27 14:17:31 +0000

    feat(web-components): added ic-skip-link component
    added an ic-skip-link component, including relevant props, styling and stories. Included spec tests,
    and added some new color tokens to global css files

---
+++ packages/web-components/src/components/ic-skip-link/test/ic-skip-link.spec.ts
@@ -0,0 +1,50 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { Link } from "../../ic-link/ic-link";
+import { SkipLink } from "../ic-skip-link";
+
+describe("ic-skip-link component", () => {
+  it("should render a skip link by default with the correct text", async () => {
+    const page = await newSpecPage({
+      components: [SkipLink, Link],
+      html: `<ic-skip-link target="#page-content"></ic-skip-link>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render a skip link with a custom label", async () => {
+    const page = await newSpecPage({
+      components: [SkipLink, Link],
+      html: `<ic-skip-link target="#page-content" label="Custom skip label"></ic-skip-link>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render a skip link without a background when `transparentBackground` is true", async () => {
+    const page = await newSpecPage({
+      components: [SkipLink, Link],
+      html: `<ic-skip-link target="#page-content" transparent-background="true"></ic-skip-link>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should pass down the theme and monochrome prop to the ic-link component", async () => {
+    const page = await newSpecPage({
+      components: [SkipLink, Link],
+      html: `<ic-skip-link target="#page-content" theme="dark" monochrome="true"></ic-skip-link>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should render with the `inline` prop", async () => {
+    const page = await newSpecPage({
+      components: [SkipLink, Link],
+      html: `<ic-skip-link target="#page-content" inline="true"></ic-skip-link>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
+});
