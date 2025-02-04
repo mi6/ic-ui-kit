be6ed013ef527671a049b1f679d2778ead93e66d
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2025-02-04 14:06:29 +0000

    chore(web-components): update to Storybook v8
    Update to Storybook v8 - .stories.mdx replaced with new format of .stories.js and .mdx

---
+++ packages/web-components/src/components/ic-radio-group/ic-radio-group.stories.js
@@ -0,0 +1,443 @@
+import { html } from "lit-html";
+
+const defaultArgs = {
+  disabled: false,
+  helperText: "Some helper text",
+  hideLabel: false,
+  label: "This is a label",
+  name: "1",
+  orientation: "vertical",
+  required: false,
+  size: "medium",
+  validationStatus: "",
+  validationText: "",
+  additionalFieldDisplay: "static",
+  radioDisabled: false,
+  dynamicText: "This selection requires additional answers",
+  radioLabel: "Radio 1",
+  radioName: "radio1",
+  value: "valueName1",
+  selected: false,
+  additionalFieldSlot: true,
+  theme: "inherit",
+};
+
+export default {
+  title: "Web Components/Radio button",
+  component: "Radio",
+};
+
+export const Default = {
+  render: (args) => html`
+    <ic-radio-group label="This is a label" name="1">
+      <ic-radio-option
+        value="valueName1"
+        label="Unselected / Default"
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Selected / Default"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName3"
+        label="Unselected / Disabled"
+        disabled
+      ></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Default",
+};
+
+export const Horizontal = {
+  render: (args) => html`
+    <ic-radio-group
+      orientation="horizontal"
+      label="This should be horizontal"
+      name="1"
+    >
+      <ic-radio-option
+        value="valueName1"
+        label="Selected / Default"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Unselected / Default"
+      ></ic-radio-option>
+    </ic-radio-group>
+    <br />
+    <p>Horizontal orientation is overridden with >2 radio options</p>
+    <br />
+    <ic-radio-group
+      orientation="horizontal"
+      label="This should be vertical"
+      name="2"
+    >
+      <ic-radio-option
+        value="valueNameA"
+        label="First"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option value="valueNameB" label="Second"></ic-radio-option>
+      <ic-radio-option value="valueNameC" label="Third"></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Horizontal",
+};
+
+export const Required = {
+  render: (args) => html`
+    <ic-radio-group label="This is a label" required name="1">
+      <ic-radio-option
+        value="valueName1"
+        label="Unselected / Default"
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Selected / Default"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName3"
+        label="Unselected / Disabled"
+        disabled
+      ></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Required",
+};
+
+export const Disabled = {
+  render: (args) => html`
+    <ic-radio-group label="This is a label" name="1">
+      <ic-radio-option
+        value="valueName1"
+        label="Unselected / Disabled 1"
+        disabled
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Unselected / Disabled 2"
+        disabled
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName3"
+        label="Unselected / Disabled 3"
+        disabled
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName4"
+        label="Selected / Disabled"
+        disabled
+        selected
+      ></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Disabled",
+};
+
+export const ConditionalDynamic = {
+  render: (args) => html`
+    <script>
+      function handleIcChange(ev) {
+        console.log(ev.detail.selectedOption);
+      }
+      function handleClick() {
+        alert("Button clicked!");
+      }
+      document
+        .querySelector("ic-button")
+        .addEventListener("click", handleClick);
+      document
+        .querySelector("ic-radio-group")
+        .addEventListener("icChange", handleIcChange);
+      document
+        .querySelector("ic-button")
+        .addEventListener("click", () => alert("Button"));
+    </script>
+    <ic-radio-group label="Conditional dynamic" name="1">
+      <ic-radio-option
+        additional-field-display="dynamic"
+        value="valueName1"
+        label="Option with Radio as conditional slotted additional field"
+        selected
+      >
+        <ic-radio-group
+          slot="additional-field"
+          helperText="Child Group Helper Text"
+          label="Children"
+          name="radio-group-2"
+        >
+          <ic-radio-option
+            key="child-option-1"
+            label="child-option-1"
+            value="child-option-1"
+          ></ic-radio-option>
+          <ic-radio-option
+            key="child-option-2"
+            label="child-option-2"
+            value="child-option-2"
+          ></ic-radio-option>
+        </ic-radio-group>
+      </ic-radio-option>
+      <ic-radio-option
+        additional-field-display="dynamic"
+        value="valueName2"
+        label="Option with Text-field as conditional slotted additional field"
+      >
+        <ic-text-field
+          validation-status="error"
+          validation-text="Now it has really gone to (coffee) pot"
+          slot="additional-field"
+          placeholder="Placeholder"
+          label="What's your favourite type of coffee?"
+        >
+        </ic-text-field>
+      </ic-radio-option>
+      <ic-radio-option
+        additional-field-display="dynamic"
+        value="valueName3"
+        label="option3 - clickable component"
+      >
+        <ic-button slot="additional-field"> Click me! </ic-button>
+      </ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Conditional dynamic",
+};
+
+export const ConditionalStatic = {
+  render: (args) => html`
+    <ic-radio-group label=" Conditional static" name="1">
+      <ic-radio-option value="valueName1" label="option1">
+        <ic-text-field
+          slot="additional-field"
+          label="What's your favourite type of coffee?"
+        ></ic-text-field>
+      </ic-radio-option>
+      <ic-radio-option value="valueName2" label="option2">
+        <ic-radio-group
+          slot="additional-field"
+          helperText="Child Group Helper Text"
+          label="Children"
+          name="radio-group-2"
+        >
+          <ic-radio-option
+            key="child-option-1"
+            label="child-option-1"
+            value="child-option-1"
+          ></ic-radio-option>
+          <ic-radio-option
+            key="child-option-2"
+            label="child-option-2"
+            value="child-option-2"
+          ></ic-radio-option>
+        </ic-radio-group>
+      </ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Conditional static",
+};
+
+export const HelperText = {
+  render: (args) => html`
+    <ic-radio-group label="This is a label" name="1" helper-text="Helper text">
+      <ic-radio-option
+        value="valueName1"
+        label="Unselected / Default"
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Selected / Default"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName3"
+        label="Unselected / Disabled"
+        disabled
+      ></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Helper text",
+};
+
+export const Small = {
+  render: (args) => html`
+    <ic-radio-group label="This is a label" name="1" size="small">
+      <ic-radio-option
+        value="valueName1"
+        label="Unselected / Default"
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Selected / Default"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName3"
+        label="Unselected / Disabled"
+        disabled
+      ></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Small",
+};
+
+export const Validation = {
+  render: (args) => html`
+    <ic-radio-group
+      label="This is a label"
+      name="1"
+      validation-status="error"
+      validation-text="Error message"
+    >
+      <ic-radio-option
+        value="valueName1"
+        label="Unselected / Default"
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName2"
+        label="Selected / Default"
+        selected
+      ></ic-radio-option>
+      <ic-radio-option
+        value="valueName3"
+        label="Unselected / Disabled"
+        disabled
+      ></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  name: "Validation",
+};
+
+export const Form = {
+  render: (args) => html`
+    <form>
+      <ic-radio-group label="This is a label" name="1">
+        <ic-radio-option
+          value="valueName1"
+          label="Unselected / Default"
+        ></ic-radio-option>
+        <ic-radio-option
+          value="valueName2"
+          label="Selected / Default"
+        ></ic-radio-option>
+        <ic-radio-option
+          value="valueName3"
+          label="Unselected / Disabled"
+          disabled
+        ></ic-radio-option>
+      </ic-radio-group>
+      <input type="submit" value="Submit" />
+      <input type="reset" value="Reset" />
+    </form>
+    <script>
+      document.querySelector("form").addEventListener("submit", (ev) => {
+        ev.preventDefault();
+        console.log(document.querySelector('input[name="1"]').value);
+      });
+    </script>
+  `,
+
+  name: "Form",
+};
+
+const inlineRadioSelector = "inline-radio";
+
+export const Playground = {
+  render: (args) => html`
+    <ic-radio-group
+      label=${args.label}
+      name=${args.name}
+      orientation=${args.orientation}
+      required=${args.required}
+      size=${args.size}
+      validation-status=${args.validationStatus}
+      validation-text=${args.validationText}
+      helper-text=${args.helperText}
+      disabled=${args.disabled}
+      hide-label=${args.hideLabel}
+      theme=${args.theme}
+    >
+      <ic-radio-option
+        additional-field-display=${args.additionalFieldDisplay}
+        value=${args.value}
+        label=${args.radioLabel}
+        name=${args.radioName}
+        selected=${args.selected}
+        disabled=${args.radioDisabled}
+        dynamic-text=${args.dynamicText}
+      >
+        <ic-text-field
+          slot=${args.additionalFieldSlot}
+          placeholder="Placeholder"
+          label="What's your favourite type of coffee?"
+        ></ic-text-field>
+      </ic-radio-option>
+      <ic-radio-option value="valueName2" label="Radio 2"></ic-radio-option>
+    </ic-radio-group>
+  `,
+
+  args: defaultArgs,
+
+  argTypes: {
+    orientation: {
+      options: ["vertical", "horizontal"],
+
+      control: {
+        type: inlineRadioSelector,
+      },
+    },
+
+    size: {
+      options: ["medium", "small"],
+
+      control: {
+        type: inlineRadioSelector,
+      },
+    },
+
+    validationStatus: {
+      options: ["error", "warning", "success", ""],
+
+      control: {
+        type: "radio",
+      },
+    },
+
+    additionalFieldDisplay: {
+      options: ["static", "dynamic"],
+
+      control: {
+        type: inlineRadioSelector,
+      },
+    },
+
+    additionalFieldSlot: {
+      mapping: {
+        true: "additional-field",
+        false: "",
+      },
+    },
+
+    theme: {
+      options: ["inherit", "light", "dark"],
+
+      control: {
+        type: inlineRadioSelector,
+      },
+    },
+  },
+
+  name: "Playground",
+};
---
+++ packages/web-components/src/components/ic-toggle-button/ic-toggle-button.stories.js
@@ -0,0 +1,427 @@
+import { html } from "lit-html";
+
+export default {
+  title: "Web Components/Toggle button",
+  component: "ic-toggle-button",
+};
+
+export const Default = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle"></ic-toggle-button>
+      <script>
+        var toggle = document.querySelector("ic-toggle-button");
+        toggle.addEventListener("icToggleChecked", function (event) {
+          console.log(event.detail.checked);
+        });
+      </script>`,
+
+  name: "Default",
+};
+
+export const Checked = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle" checked="true"></ic-toggle-button>`,
+  name: "Checked",
+};
+
+export const Disabled = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle" disabled="true"></ic-toggle-button>
+      <ic-toggle-button
+        label="Toggle"
+        disabled="true"
+        checked="true"
+      ></ic-toggle-button>`,
+
+  name: "Disabled",
+};
+
+export const WithIcon = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle">
+      <svg
+        slot="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+        />
+      </svg>
+    </ic-toggle-button>`,
+
+  name: "With icon",
+};
+
+export const Sizes = {
+  render: (args) =>
+    html`<div style="padding: 6px">
+        <ic-toggle-button label="Toggle" size="small"></ic-toggle-button>
+      </div>
+      <div style="padding: 6px">
+        <ic-toggle-button label="Toggle"></ic-toggle-button>
+      </div>
+      <div style="padding: 6px">
+        <ic-toggle-button label="Toggle" size="large"></ic-toggle-button>
+      </div>`,
+
+  name: "Sizes",
+};
+
+export const WithBadge = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle">
+        <ic-badge label="1" slot="badge" variant="success"></ic-badge>
+      </ic-toggle-button>
+      <ic-toggle-button label="Toggle">
+        <ic-badge slot="badge" variant="success" type="dot"></ic-badge>
+      </ic-toggle-button>`,
+
+  name: "With badge",
+};
+
+export const Monochrome = {
+  render: (args) =>
+    html`<ic-toggle-button
+      label="Toggle"
+      monochrome="true"
+    ></ic-toggle-button>`,
+
+  name: "Monochrome",
+};
+
+export const FullWidth = {
+  render: (args) =>
+    html`<ic-toggle-button
+      label="Toggle"
+      full-width="true"
+    ></ic-toggle-button>`,
+
+  name: "Full width",
+};
+
+export const Loading = {
+  render: (args) =>
+    html`<div style="padding: 6px">
+        <ic-toggle-button label="Toggle" loading="true"></ic-toggle-button>
+        <ic-toggle-button
+          label="Toggle"
+          loading="true"
+          checked="true"
+        ></ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          loading="true"
+          accessible-label="Refresh the page"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          loading="true"
+          checked="true"
+          accessible-label="Refresh the page"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+      </div>
+      <div style="padding:6px 10px; width:fit-content">
+        <ic-toggle-button
+          label="Toggle"
+          loading="true"
+          monochrome="true"
+        ></ic-toggle-button>
+        <ic-toggle-button
+          label="Toggle"
+          loading="true"
+          checked="true"
+          monochrome="true"
+        ></ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          loading="true"
+          accessible-label="Refresh the page"
+          monochrome="true"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          loading="true"
+          checked="true"
+          accessible-label="Refresh the page"
+          monochrome="true"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+      </div>`,
+
+  name: "Loading",
+};
+
+export const IconOnly = {
+  render: (args) =>
+    html`<div style="padding:6px 10px; width:fit-content">
+        <ic-toggle-button
+          variant="icon"
+          size="small"
+          accessible-label="Refresh the page"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+        <ic-toggle-button variant="icon" accessible-label="Refresh the page">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          size="large"
+          accessible-label="Refresh the page"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          disabled="true"
+          accessible-label="Refresh the page"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+      </div>
+      <div style="padding:6px 10px; width:fit-content">
+        <ic-toggle-button
+          variant="icon"
+          accessible-label="Refresh the page"
+          monochrome="true"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+        <ic-toggle-button
+          variant="icon"
+          disabled="true"
+          accessible-label="Refresh the page"
+          monochrome="true"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+            />
+          </svg>
+        </ic-toggle-button>
+      </div>`,
+
+  name: "Icon only",
+};
+
+export const IconRight = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle" icon-placement="right">
+      <svg
+        slot="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+        />
+      </svg>
+    </ic-toggle-button>`,
+
+  name: "Icon right",
+};
+
+export const IconTop = {
+  render: (args) =>
+    html`<ic-toggle-button label="Toggle" icon-placement="top">
+      <svg
+        slot="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path
+          d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+        />
+      </svg>
+    </ic-toggle-button>`,
+
+  name: "Icon top",
+};
+
+export const TooltipPlacement = {
+  render: (args) =>
+    html`<div style="padding:100px 10px; width:fit-content">
+      <ic-toggle-button
+        variant="icon"
+        tooltip-placement="right"
+        accessible-label="Refresh the page"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 0 24 24"
+          width="24px"
+          fill="#000000"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+          />
+        </svg>
+      </ic-toggle-button>
+      <ic-toggle-button
+        variant="icon"
+        tooltip-placement="top"
+        accessible-label="Refresh the page"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 0 24 24"
+          width="24px"
+          fill="#000000"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+          />
+        </svg>
+      </ic-toggle-button>
+      <ic-toggle-button
+        variant="icon"
+        tooltip-placement="left"
+        accessible-label="Refresh the page"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 0 24 24"
+          width="24px"
+          fill="#000000"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path
+            d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+          />
+        </svg>
+      </ic-toggle-button>
+    </div>`,
+
+  name: "Tooltip placement",
+};
