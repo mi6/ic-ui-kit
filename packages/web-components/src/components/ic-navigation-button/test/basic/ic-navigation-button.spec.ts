80fc2e4e876d06224f60495115f76d9332759339
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2025-03-03 13:38:53 +0000

    chore(web-components): upgrade the minimum stencil version to 4.26.0
    Upgrade the minimum stencil version to 4.26.0. Update test snapshots to replace mock:shadow-root
    with template shadowrootmode="open"
    
    . #3052

--- packages/web-components/src/components/ic-navigation-button/test/basic/ic-navigation-button.spec.ts
+++ packages/web-components/src/components/ic-navigation-button/test/basic/ic-navigation-button.spec.ts
@@ -25,19 +25,8 @@ describe("ic-navigation-button", () => {
       </svg>
     </ic-navigation-button>`,
     });
-    expect(page.root).toEqualHtml(`
-      <ic-navigation-button label="button1" onclick="alert('test')">
-        <mock:shadow-root>
-          <ic-button appearance="light" aria-label="button1" monochrome="" size="large" theme="light" variant="icon">
-            <slot name="icon" slot="left-icon"></slot>
-          </ic-button>
-        </mock:shadow-root>
-        <svg fill="#000000" height="24px" slot="icon" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 0h24v24H0V0z" fill="none"></path>
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z"></path>
-        </svg>
-      </ic-navigation-button>
-    `);
+
+    expect(page.root).toMatchSnapshot();
   });
 
   it("should render differently if mode is menu", async () => {
