639048cf3025f32ffda1dd323f16b735da4fbe75
gd2910 <122624946+gd2910@users.noreply.github.com>
2025-03-06 16:32:35 +0000

    test(react): storybook update and cypress tests for IcBackToTop
    visual regression images added for IcbackToTop position attribute
    
    2154

--- packages/react/src/component-tests/IcBackToTop/IcBackToTopTestData.tsx
+++ packages/react/src/component-tests/IcBackToTop/IcBackToTopTestData.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { IcBackToTop } from "../../components";
-import { IcBackToTopVariants } from "@ukic/web-components";
+import {
+  IcBackToTopPositions,
+  IcBackToTopVariants,
+  IcThemeMode,
+} from "@ukic/web-components";
 
-export const BackToTop = (props: { variant?: IcBackToTopVariants; theme }) => {
-  const { variant, theme } = props;
+export const BackToTop = (props: {
+  variant?: IcBackToTopVariants;
+  theme: IcThemeMode;
+  position?: IcBackToTopPositions;
+}) => {
+  const { variant, theme, position } = props;
+
   return (
     <div style={{ height: "120vh" }}>
       <div id="topEl" style={{ position: "absolute", top: "0", width: "100%" }}>
         Top of screen
       </div>
-      <IcBackToTop target="topEl" variant={variant} theme={theme} />
+      <IcBackToTop
+        target="topEl"
+        variant={variant}
+        theme={theme}
+        position={position == undefined ? "right" : position}
+      />
     </div>
   );
 };
