d0edf99a2da0f0ca5a5ed2d240fd341b2dc141ba
srt544 <180160108+srt544@users.noreply.github.com>
2025-06-30 14:56:12 +0000

    fix(react): determinate Loading indicator for both circular and linear show progress of 0%

--- packages/react/src/component-tests/IcLoadingIndicator/IcLoadingIndicator.cy.tsx
+++ packages/react/src/component-tests/IcLoadingIndicator/IcLoadingIndicator.cy.tsx
@@ -18,6 +18,8 @@ import {
   DarkModeColours,
   LightMonochrome,
   LoadingIndicatorInDialog,
+  DeterminateLinearLoadingIndicatorZeroProgress,
+  DeterminateCircularLoadingIndicatorZeroProgress,
 } from "./IcLoadingIndicatorTestData";
 
 const ICON_CLIP = { x: 0, y: 0, width: 50, height: 50 };
@@ -111,6 +113,48 @@ describe("IcLoadingIndicator end-to-end, visual regression and a11y tests", () =
     cy.compareSnapshot({
       name: "/determinate-linears-with-3-sizes",
       testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD + 0.035),
+      cypressScreenshotOptions: {
+        clip: MULTI_CLIP,
+      },
+    });
+  });
+
+  it("should render determinate linear loading indicators with zero progress", () => {
+    mount(
+      <DeterminateLinearLoadingIndicatorZeroProgress
+        progress={0}
+        max={defaultArgs.max}
+        min={defaultArgs.min}
+      />
+    );
+
+    cy.checkHydrated(LOADING_INDICATOR_SELECTOR);
+
+    cy.checkA11yWithWait();
+    cy.compareSnapshot({
+      name: "/determinate-linear-with-0-progress",
+      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD + 0.018),
+      cypressScreenshotOptions: {
+        clip: MULTI_CLIP,
+      },
+    });
+  });
+
+  it("should render determinate circular loading indicators with zero progress", () => {
+    mount(
+      <DeterminateCircularLoadingIndicatorZeroProgress
+        progress={0}
+        max={defaultArgs.max}
+        min={defaultArgs.min}
+      />
+    );
+
+    cy.checkHydrated(LOADING_INDICATOR_SELECTOR);
+
+    cy.checkA11yWithWait();
+    cy.compareSnapshot({
+      name: "/determinate-circular-with-0-progress",
+      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD + 0.018),
       cypressScreenshotOptions: {
         clip: MULTI_CLIP,
       },
