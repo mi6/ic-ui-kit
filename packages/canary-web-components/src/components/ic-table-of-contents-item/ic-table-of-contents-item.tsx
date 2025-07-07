3dfb0f565983bd2b1df0dae34a1c4c4257411791
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2025-07-07 10:41:17 +0000

    feat(canary-web-components): add initial Table of Contents work
    Table of Contents component created, storybooks created
    
    ref #752

---
+++ packages/canary-web-components/src/components/ic-table-of-contents-item/ic-table-of-contents-item.tsx
@@ -0,0 +1,96 @@
+import { FunctionalComponent, h } from "@stencil/core";
+
+interface IcTableOfContentsItemElement {
+  label: string;
+  target: string;
+  variant: string;
+  selected: number;
+  position: number;
+  updateSelected: (position: number) => void;
+  setItemLevelOffset: (variant: string) => string;
+  size?: string;
+  truncate?: boolean;
+  monochrome?: boolean;
+}
+
+interface TOCItemProps {
+  variant: string;
+  label: string;
+  target: string;
+  selected: number;
+  position: number;
+  updateSelected: (position: number) => void;
+  size?: string;
+  truncate?: boolean;
+  monochrome?: boolean;
+}
+
+const ItemHook = () => {
+  // adds offset to the item based on its variant
+  // e.g. h2 = 4 + (8*(2-2)) = 4px
+  // h3 = 4 + (8*(3-2)) = 12px
+  const setItemLevelOffset = (variant: string): string => {
+    return `${4 + 8 * (parseInt(variant.split("")[1]) - 2)}px`;
+  };
+
+  return { setItemLevelOffset };
+};
+
+const ItemComponent: FunctionalComponent<IcTableOfContentsItemElement> = (
+  itemProps
+) => {
+  const {
+    label,
+    target,
+    selected,
+    truncate,
+    variant,
+    position,
+    size,
+    monochrome,
+    updateSelected,
+    setItemLevelOffset,
+  } = itemProps;
+
+  return (
+    <li
+      class={`table-of-contents-item  ${
+        monochrome && "table-of-contents-item-monochrome"
+      }`}
+    >
+      <a
+        id={`${target}-table-of-contents-item-content`}
+        onClick={() => updateSelected(position)}
+        class={`${selected && "table-of-contents-item-content-selected"}
+        ${
+          size === "large"
+            ? `table-of-contents-item-content-large`
+            : `table-of-contents-item-content-default`
+        }
+        table-of-contents-item-content
+       `}
+        href={`#${target}`}
+      >
+        <ic-typography
+          id={`${target}-table-of-contents-item-label`}
+          bold={selected === position}
+          class={`${truncate && "truncated-item-text"}  ${
+            size && `table-of-contents-item-content-${size}`
+          }
+          table-of-contents-item-label
+          `}
+          style={{
+            paddingLeft: setItemLevelOffset(variant),
+          }}
+        >
+          {label}
+        </ic-typography>
+      </a>
+    </li>
+  );
+};
+
+export const TableOfContentsItem = (props: TOCItemProps) => {
+  const innerProps = ItemHook();
+  return <ItemComponent {...props} {...innerProps} />;
+};
