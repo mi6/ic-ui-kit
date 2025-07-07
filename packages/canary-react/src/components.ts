690cc564cf511bd9c6e4281553d619e816673e56
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2025-07-07 10:42:10 +0000

    test(canary-react): add initial table-of-contents tests and stories for react
    Add foundation for cypress tests and storybooks in canary-react for table-of-contents
    
    ref #752

--- packages/canary-react/src/components.ts
+++ packages/canary-react/src/components.ts
@@ -8,11 +8,13 @@ import type { JSX } from '@ukic/canary-web-components';
 import { defineCustomElements } from '@ukic/canary-web-components/loader';
 
 defineCustomElements();
+export const IcActionChip = /*@__PURE__*/createReactComponent<JSX.IcActionChip, HTMLIcActionChipElement>('ic-action-chip');
 export const IcCardHorizontal = /*@__PURE__*/createReactComponent<JSX.IcCardHorizontal, HTMLIcCardHorizontalElement>('ic-card-horizontal');
 export const IcDataTable = /*@__PURE__*/createReactComponent<JSX.IcDataTable, HTMLIcDataTableElement>('ic-data-table');
 export const IcDataTableTitleBar = /*@__PURE__*/createReactComponent<JSX.IcDataTableTitleBar, HTMLIcDataTableTitleBarElement>('ic-data-table-title-bar');
 export const IcDateInput = /*@__PURE__*/createReactComponent<JSX.IcDateInput, HTMLIcDateInputElement>('ic-date-input');
 export const IcDatePicker = /*@__PURE__*/createReactComponent<JSX.IcDatePicker, HTMLIcDatePickerElement>('ic-date-picker');
 export const IcPaginationBar = /*@__PURE__*/createReactComponent<JSX.IcPaginationBar, HTMLIcPaginationBarElement>('ic-pagination-bar');
+export const IcTableOfContents = /*@__PURE__*/createReactComponent<JSX.IcTableOfContents, HTMLIcTableOfContentsElement>('ic-table-of-contents');
 export const IcTreeItem = /*@__PURE__*/createReactComponent<JSX.IcTreeItem, HTMLIcTreeItemElement>('ic-tree-item');
 export const IcTreeView = /*@__PURE__*/createReactComponent<JSX.IcTreeView, HTMLIcTreeViewElement>('ic-tree-view');
