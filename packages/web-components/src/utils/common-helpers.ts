d9a2db0fdfddd7c12d431214df71e233bcee6c5e
ad9242 <116102637+ad9242@users.noreply.github.com>
2025-09-17 06:40:25 +0000

    revert(web-components): revert changes from PR #3922

--- packages/web-components/src/utils/common-helpers.ts
+++
@@ -1,8 +0,0 @@
-import purifier from "dompurify";
-import { IC_ALL_COMPONENTS } from "./constants";
-
-export const sanitizeHTMLString = (html: string): string => {
-  return purifier.sanitize(html, {
-    ADD_TAGS: IC_ALL_COMPONENTS,
-  }) as unknown as string;
-};
