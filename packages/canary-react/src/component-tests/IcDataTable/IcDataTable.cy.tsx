f20814d28da72db539f3f1111be85bc3add08648
ad9242 <116102637+ad9242@users.noreply.github.com>
2025-09-24 09:38:51 +0000

    fix(canary-react): adds tests for fixes to data-table and tree-view
    adds tests for fixes to data-table and tree-view when adding custom elements

--- packages/canary-react/src/component-tests/IcDataTable/IcDataTable.cy.tsx
+++ packages/canary-react/src/component-tests/IcDataTable/IcDataTable.cy.tsx
@@ -13,6 +13,7 @@ import {
   IcSectionContainer,
   IcTypography,
   IcLink,
+  IcSelect,
 } from "@ukic/react";
 import { IcPaginationBarOptions } from "@ukic/canary-web-components/src/utils/types";
 import {
@@ -72,6 +73,7 @@ import {
   NOT_BE_VISIBLE,
   NOT_EXIST,
   NOT_HAVE_CSS,
+  NOT_HAVE_BEEN_CALLED,
 } from "@ukic/react/src/component-tests/utils/constants";
 
 import { setThresholdBasedOnEnv } from "@ukic/react/cypress/utils/helpers";
@@ -226,6 +228,41 @@ export const ExternalSortColumnDataTable = (): ReactElement => {
         sortOrders: ["ascending", "descending"],
       }}
     />
+  );
+};
+
+export const PaginationDataTable = (): ReactElement => {
+  return (
+    <IcDataTable
+      caption="Data tables"
+      columns={COLS_ELEMENTS_SINGLE_ACTION}
+      data={DATA_ELEMENTS_PAGINATION}
+      showPagination
+    >
+      {DATA_ELEMENTS_PAGINATION.map((_, index) => (
+        <>
+          {index === 4 || index === 9 ? (
+            <IcSelect
+              label="Select label"
+              hideLabel
+              placeholder="Placeholder goes here"
+              slot={`actions-${index}`}
+              key={`actions-${index}`}
+            ></IcSelect>
+          ) : (
+            <IcButton
+              onClick={() => {
+                console.log(`${index + 1}`);
+              }}
+              slot={`actions-${index}`}
+              key={`actions-${index}`}
+            >
+              {index + 1}
+            </IcButton>
+          )}
+        </>
+      ))}
+    </IcDataTable>
   );
 };
 
@@ -675,6 +712,24 @@ describe("IcDataTables", () => {
       },
     });
     cy.checkA11yWithWait();
+  });
+
+  it("should test code in column definition object", () => {
+    const clonedCols = JSON.parse(JSON.stringify(ICON_COLS));
+    clonedCols[1].icon.icon = `<img src=x onerror=alert(12345)//>`;
+
+    cy.spy(window, "alert").as("spyAlert");
+
+    mount(
+      <IcDataTable
+        columns={clonedCols}
+        data={ICON_DATA}
+        caption="Data Tables"
+      />
+    );
+
+    cy.checkHydrated(DATA_TABLE_SELECTOR);
+    cy.get("@spyAlert").should(NOT_HAVE_BEEN_CALLED);
   });
 
   it("should render the empty state when data is null", () => {
@@ -1741,17 +1796,10 @@ describe("IcDataTables with IcPaginationBar", () => {
   });
 
   it("should render a select menu above the pagination bar", () => {
-    mount(
-      <IcDataTable
-        caption="Data tables"
-        columns={COLS_ELEMENTS_SINGLE_ACTION}
-        data={DATA_ELEMENTS_PAGINATION}
-        showPagination
-      />
-    );
+    mount(<PaginationDataTable />);
 
     cy.checkHydrated(DATA_TABLE_SELECTOR);
-    cy.findShadowEl(DATA_TABLE_SELECTOR, ".data-type-element").eq(9).click();
+    cy.get("ic-select").eq(1).click();
 
     cy.checkA11yWithWait();
     cy.compareSnapshot({
@@ -4460,6 +4508,23 @@ describe("IcDataTable table with descriptions", () => {
         capture: "viewport",
       },
     });
+  });
+
+  it("should test code in description object", () => {
+    const clonedData = JSON.parse(
+      JSON.stringify(LONG_DATA_ELEMENTS_WITH_DESCRIPTIONS)
+    );
+    clonedData[0].firstName.icon = `<img src=x onerror=alert(12345)//>`;
+    clonedData[0].firstName.description.icon = `<img src=x onerror=alert(12345)//>`;
+
+    cy.spy(window, "alert").as("spyAlert");
+
+    mount(
+      <IcDataTable columns={COLS} data={clonedData} caption="Data Tables" />
+    );
+
+    cy.checkHydrated(DATA_TABLE_SELECTOR);
+    cy.get("@spyAlert").should(NOT_HAVE_BEEN_CALLED);
   });
 
   it("should truncate long text without truncating descriptions - using show/hide pattern when global row height is set", () => {
