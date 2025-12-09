ff41d9c663f1722a77bfe8d2f2fdc9329f0530b5
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2025-12-09 14:46:02 +0000

    chore(web-components): remove unused args from stories
    Remove unused args from stories - helps tidy up lint warnings in the CI

--- packages/web-components/src/components/ic-button/ic-button.stories.js
+++ packages/web-components/src/components/ic-button/ic-button.stories.js
@@ -2436,7 +2436,7 @@ export const Tooltips = {
 };
 
 export const PositioningStrategy = {
-  render: (args) =>
+  render: () =>
     html` <script>
         var switchEl = document.querySelector("ic-switch");
         var buttonEl = document.querySelector("ic-button");
@@ -2677,7 +2677,7 @@ export const AriaDescribedby = {
 };
 
 export const FileUploadInAForm = {
-  render: (args) => html`
+  render: () => html`
     <form method="POST" action="someurl" enctype="multipart/form-data">
       <span id="selected-file">No File Selected</span>
       <div style="padding: 6px">
@@ -2719,7 +2719,7 @@ export const FileUploadInAForm = {
 };
 
 export const FileUpload = {
-  render: (args) => html`
+  render: () => html`
     <span id="selected-file">No File Selected</span>
     <div style="padding: 6px">
       <ic-button
