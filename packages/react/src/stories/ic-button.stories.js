3563a11acba3fe642250b3b2201a934a67829ed5
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2025-12-30 13:40:25 +0000

    chore(react): update button stories to have icon only playground
    Update button stories to have icon only playground
    
    . #3871

--- packages/react/src/stories/ic-button.stories.js
+++ packages/react/src/stories/ic-button.stories.js
@@ -1665,6 +1665,86 @@ export const PlaygroundWithIcon = {
   name: "Playground with icon",
 };
 
+export const PlaygroundIconOnly = {
+  render: (args) => (
+    <IcButton
+      disableTooltip={args.disableTooltip}
+      disabled={args.disabled}
+      fullWidth={args.fullWidth}
+      href={args.href}
+      loading={args.loading}
+      monochrome={args.monochrome}
+      size={args.size}
+      theme={args.theme}
+      tooltipPlacement={args.tooltipPlacement}
+      transparentBackground={args.transparentBackground}
+      variant={args.variant}
+      aria-label="refresh"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 0 24 24"
+        width="24px"
+        fill="#000000"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+      </svg>
+    </IcButton>
+  ),
+
+  args: {
+    disableTooltip: false,
+    disabled: false,
+    fullWidth: false,
+    href: "",
+    loading: false,
+    monochrome: false,
+    size: "medium",
+    theme: "inherit",
+    tooltipPlacement: "bottom",
+    transparentBackground: false,
+    variant: "icon-primary",
+  },
+
+  argTypes: {
+    variant: {
+      options: ["icon-primary", "icon-secondary", "icon-tertiary", "icon-destructive"],
+
+      control: {
+        type: "select",
+      },
+    },
+
+    size: {
+      options: ["medium", "large", "small"],
+
+      control: {
+        type: "inline-radio",
+      },
+    },
+
+    theme: {
+      options: ["inherit", "dark", "light"],
+
+      control: {
+        type: "inline-radio",
+      },
+    },
+
+    tooltipPlacement: {
+      options: ["top", "right", "bottom", "left"],
+
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
+
+  name: "Playground - icon only",
+};
+
 export const FileUpload_ = {
   render: () => <FileUpload />,
   name: "File Upload",
