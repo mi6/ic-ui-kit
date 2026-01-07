8c92889af31c4a10135334fc367aa870eb702962
jd3267 <228690498+jd3267@users.noreply.github.com>
2026-01-07 12:19:39 +0000

    feat(react): react version update in line with web-components
    Threshold update to bring react version in sync with web components

--- packages/react/src/component-tests/IcAlert/IcAlert.cy.tsx
+++ packages/react/src/component-tests/IcAlert/IcAlert.cy.tsx
@@ -239,7 +239,7 @@ describe("IcAlert visual regression and a11y tests", () => {
     cy.checkA11yWithWait();
     cy.compareSnapshot({
       name: "/variants",
-      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD),
+      testThreshold: setThresholdBasedOnEnv(DEFAULT_TEST_THRESHOLD + 0.062),
     });
   });
 
