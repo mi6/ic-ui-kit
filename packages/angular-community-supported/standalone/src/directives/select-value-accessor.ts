1c9e211f96713a2c77a77623fb9c6fa068ca41b9
EC2 Default User <221165268+GCHQDev01001010@users.noreply.github.com>
2026-01-22 12:40:33 +0000

    feat(angular-community-supported): adds package structure for publishing the angular native wrappers
    Provides the underlying folders structure, npm config, scripts and such for publishing the Angular
    package. Also includes the 3.18.0 output from the stencil generator.
    
    closes #3933

---
+++ packages/angular-community-supported/standalone/src/directives/select-value-accessor.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, forwardRef } from "@angular/core";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
+
+import { ValueAccessor } from "./value-accessor";
+
+@Directive({
+  /* tslint:disable-next-line:directive-selector */
+  selector: "ic-select",
+  host: {
+    "(icChange)": 'handleChangeEvent($event.target?.["value"])',
+  },
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectValueAccessor),
+      multi: true,
+    },
+  ],
+})
+export class SelectValueAccessor extends ValueAccessor {
+  constructor(el: ElementRef) {
+    super(el);
+  }
+}
