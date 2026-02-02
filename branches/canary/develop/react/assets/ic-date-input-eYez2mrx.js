25ed216436d59dc850ada1391fcdc150dfee56aa
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2026-02-02 11:09:35 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@3ff5ad1e9b15ddcc59314ac9b0046237fad5a06c 🚀

---
+++ branches/canary/develop/react/assets/ic-date-input-eYez2mrx.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-4gip-BOf.js";import{useMDXComponents as i}from"./index-jOos6cxN.js";import{I as m}from"./ic-date-input.stories-BElImsoj.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-B5VYDsPw.js";import"./index-BGZpz9zn.js";import"./components-hMJuZ_5j.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
