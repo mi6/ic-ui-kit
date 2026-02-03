1b1677fa9cde83df372ad3cbd82b77a1157823df
ad3872 <ad3872@users.noreply.github.com>
2026-02-03 16:52:20 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@d9dd0e78862e8bd0dcb091f4f746282990ccfbe5 🚀

---
+++ branches/3873-ic-time-input---ic-change-event-detail-includes-current-date/canary-web-components/assets/ic-table-of-contents-CHTGCyT3.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-CYSGlvpW.js";import{useMDXComponents as i}from"./index-Dk7SVjLx.js";import{I as m}from"./ic-table-of-contents.stories-D0pYEseu.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BNJL3x4j.js";import"./lit-html-DDAUtwXN.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
