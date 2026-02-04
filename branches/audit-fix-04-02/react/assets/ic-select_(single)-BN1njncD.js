480e2abe45fe34eef32c457ac536d601fcfaf4c8
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-02-04 12:12:43 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@98c5e7f53e6e8aacb9fe52b85e27ad84d16bacf8 🚀

---
+++ branches/audit-fix-04-02/react/assets/ic-select_(single)-BN1njncD.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as i}from"./blocks-DVZUEMUM.js";import{useMDXComponents as m}from"./index-DlBHeH1L.js";import{I as a}from"./ic-select_(single).stories-X7XOsip_.js";import{r as p}from"./readme-CMyk4jTu.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-CHj_GJ12.js";import"./index-Fss0ArKs.js";import"./slottedSVG-DZgSkS84.js";function e(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:a}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(i,{})]})}function S(r={}){const{wrapper:o}={...m(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e()}export{S as default};
