89757c091797a86f4292dd3992c071510f658c3d
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-02-04 12:12:58 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@98c5e7f53e6e8aacb9fe52b85e27ad84d16bacf8 🚀

---
+++ branches/audit-fix-04-02/canary-web-components/assets/ic-card-horizontal-DO8Kt6e-.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-CU7stiHj.js";import{useMDXComponents as i}from"./index-wqCn4KX9.js";import{I as m}from"./ic-card-horizontal.stories-Cmcw2OJH.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Dn5hLHgB.js";import"./lit-html-DDAUtwXN.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
