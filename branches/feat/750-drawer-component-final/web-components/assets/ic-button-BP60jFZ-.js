3bb314b7a3ce5efdbb4794598154bda350091a19
GCHQ-Developer-847 <GCHQ-Developer-847@users.noreply.github.com>
2026-02-06 21:59:18 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@f899dcdceb9ae4f0e4981c44f4ef51b04f207580 🚀

---
+++ branches/feat/750-drawer-component-final/web-components/assets/ic-button-BP60jFZ-.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as m}from"./blocks-C9foOLs9.js";import{useMDXComponents as a}from"./index-EGnjyBsi.js";import{I as i}from"./ic-button.stories-DEPeOLLa.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B4cpH-Fh.js";import"./lit-html-CWQq0DyO.js";const p=""+new URL("readme-BqBlNuOJ.md",import.meta.url).href;function e(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:i}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(m,{})]})}function l(r={}){const{wrapper:o}={...a(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e()}export{l as default};
