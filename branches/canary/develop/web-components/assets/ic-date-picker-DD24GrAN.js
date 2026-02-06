6d7f9d4c55f5c71444be0a2ad7a7654c52a972ff
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2026-02-06 12:45:25 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@8400064d4b3e6668ca0b4fd9c7c6b836e9083c5d 🚀

--- branches/canary/develop/web-components/assets/ic-date-picker-DD24GrAN.js
+++
@@ -1,2 +0,0 @@
-import{j as t,M as n,S as s}from"./blocks-CU7stiHj.js";import{useMDXComponents as i}from"./index-wqCn4KX9.js";import{I as m}from"./ic-date-picker.stories-Cx7EDnaL.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Dn5hLHgB.js";import"./lit-html-DDAUtwXN.js";function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:m}),`
-`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e()}export{M as default};
