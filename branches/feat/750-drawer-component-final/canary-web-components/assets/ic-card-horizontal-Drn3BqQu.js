9eb55f2876c0b9a2ae82b8f196e66a1ad068098b
GCHQ-Developer-847 <GCHQ-Developer-847@users.noreply.github.com>
2026-02-06 21:59:47 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@f899dcdceb9ae4f0e4981c44f4ef51b04f207580 🚀

---
+++ branches/feat/750-drawer-component-final/canary-web-components/assets/ic-card-horizontal-Drn3BqQu.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-CpeYFsST.js";import{useMDXComponents as i}from"./index-Cl-1b263.js";import{I as m}from"./ic-card-horizontal.stories-Ci9_ryM1.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BzXX7fDv.js";import"./lit-html-DDAUtwXN.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
