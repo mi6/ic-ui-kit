0610895cdf50c126104e3d29e8ab33a7d874cfe4
GCHQ-Developer-847 <111882109+GCHQ-Developer-847@users.noreply.github.com>
2026-02-10 15:05:12 +0000

    fix(canary-web-components): update canary-web-components Vite config to fix hot reloading

--- packages/canary-web-components/.storybook/main.mjs
+++ packages/canary-web-components/.storybook/main.mjs
@@ -1,27 +1,35 @@
 export default {
-  "core": {
-    disableTelemetry: true
+  core: {
+    disableTelemetry: true,
   },
 
-  "stories": [
+  stories: [
     "../src/*.mdx",
     "../src/**/**/*.mdx",
-    "../src/**/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
   ],
 
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
   ],
 
-  "framework": {
+  framework: {
     name: "@storybook/web-components-vite",
-    options: {}
+    options: {},
   },
-  async viteFinal(config) {
-    config.assetsInclude = ['**/*.md']
+
+  async viteFinal(config, { configType }) {
+    config.assetsInclude = ["**/*.md"];
+
+    if (configType === "DEVELOPMENT") {
+      // Set outDir to something other than 'dist' to avoid conflict with Stencil's 'dist' directory
+      config.build = { ...(config.build || {}), outDir: "dist-vite" };
+    }
+
     return config;
   },
-  docs: {}
-}
+
+  docs: {},
+};
