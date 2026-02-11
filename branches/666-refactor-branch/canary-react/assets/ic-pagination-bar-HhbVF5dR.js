9de727af3aea9bd465f8203b01a39824d231dff5
lz405 <lz405@users.noreply.github.com>
2026-02-11 14:29:48 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@05b051061385c34e5e3658a0167bbeb4ea65f741 🚀

---
+++ branches/666-refactor-branch/canary-react/assets/ic-pagination-bar-HhbVF5dR.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-CGgrltZ3.js";import{useMDXComponents as i}from"./index-DniYleWq.js";import{I as a}from"./ic-pagination-bar.stories-CT47vPzl.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-p1ooj4se.js";import"./index-DTtcAqIm.js";import"./components-6ZmllG_-.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
