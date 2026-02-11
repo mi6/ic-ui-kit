f4dca6f0d260122e5b5b779f6d11259bf3a734ef
EC2 Default User <221165268+GCHQDev01001010@users.noreply.github.com>
2026-02-11 12:55:45 +0000

    chore(angular-community-supported): addrsses PR comment & updates ng wrappers to use ICDS v 3.19e

--- packages/angular-community-supported/standalone/src/directives/radio-value-accessor.ts
+++ packages/angular-community-supported/standalone/src/directives/radio-value-accessor.ts
@@ -1,22 +1,22 @@
-import { Directive, ElementRef, forwardRef } from "@angular/core";
-import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { Directive, ElementRef, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
-import { ValueAccessor } from "./value-accessor";
+import { ValueAccessor } from './value-accessor';
 
 @Directive({
   /* tslint:disable-next-line:directive-selector */
-  selector: "ic-radio-group, ic-radio-option",
+  selector: 'ic-radio-group, ic-radio-option',
   host: {
-    "(icChange)": 'handleChangeEvent($event.target?.["value"])',
-    "(icCheck)": 'handleChangeEvent($event.target?.["value"])',
+    '(icChange)': 'handleChangeEvent($event.target?.["value"])',
+    '(icCheck)': 'handleChangeEvent($event.target?.["value"])'
   },
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => RadioValueAccessor),
-      multi: true,
-    },
-  ],
+      multi: true
+    }
+  ]
 })
 export class RadioValueAccessor extends ValueAccessor {
   constructor(el: ElementRef) {
--- packages/angular-community-supported/standalone/src/directives/text-value-accessor.ts
+++ packages/angular-community-supported/standalone/src/directives/text-value-accessor.ts
@@ -1,22 +1,21 @@
-import { Directive, ElementRef, forwardRef } from "@angular/core";
-import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { Directive, ElementRef, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
-import { ValueAccessor } from "./value-accessor";
+import { ValueAccessor } from './value-accessor';
 
 @Directive({
   /* tslint:disable-next-line:directive-selector */
-  selector:
-    "ic-text-field, ic-search-bar, ic-switch, ic-date-input, ic-date-picker",
+  selector: 'ic-text-field, ic-search-bar, ic-switch, ic-date-input, ic-date-picker',
   host: {
-    "(icChange)": 'handleChangeEvent($event.target?.["value"])',
+    '(icChange)': 'handleChangeEvent($event.target?.["value"])'
   },
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => TextValueAccessor),
-      multi: true,
-    },
-  ],
+      multi: true
+    }
+  ]
 })
 export class TextValueAccessor extends ValueAccessor {
   constructor(el: ElementRef) {
