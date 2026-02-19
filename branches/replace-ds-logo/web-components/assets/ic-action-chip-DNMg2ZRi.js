7793bfc965e7c90695fefdc2189959fa71eb1269
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-02-19 13:14:50 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@a67ee0385e7579045ab65a64fe42a14a1ff64988 🚀

---
+++ branches/replace-ds-logo/web-components/assets/ic-action-chip-DNMg2ZRi.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as m}from"./blocks-6rRl-zrq.js";import{useMDXComponents as i}from"./index-DET2L6kD.js";import{I as a}from"./ic-action-chip.stories-C-HChC8R.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-rCGIVrRY.js";import"./lit-html-CWQq0DyO.js";const p=""+new URL("readme-BVB_4rE8.md",import.meta.url).href;function e(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:a}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(m,{})]})}function h(r={}){const{wrapper:o}={...i(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e()}export{h as default};
