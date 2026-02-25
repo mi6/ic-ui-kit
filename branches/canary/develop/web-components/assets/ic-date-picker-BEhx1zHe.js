d9e47f1e748a88515fa08b100d78b06fb3e795b1
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2026-02-25 09:31:03 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@884b69dcf7828a9c6dbdd0af7f982480d071293a 🚀

---
+++ branches/canary/develop/web-components/assets/ic-date-picker-BEhx1zHe.js
@@ -0,0 +1,2 @@
+import{j as t,M as n,S as s}from"./blocks-mHNWm8W1.js";import{useMDXComponents as i}from"./index-c2WPzhnl.js";import{I as m}from"./ic-date-picker.stories-Cx7EDnaL.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BgIFkKLg.js";import"./lit-html-DDAUtwXN.js";function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e()}export{M as default};
---
+++ branches/canary/develop/web-components/assets/usage-with-vue-DFMDY8f1.js
@@ -0,0 +1,70 @@
+import{j as n,M as o}from"./blocks-mHNWm8W1.js";import{useMDXComponents as i}from"./index-c2WPzhnl.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BgIFkKLg.js";function t(s){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...i(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(o,{title:"Usage with Vue"}),`
+`,n.jsx(e.h1,{id:"installing-in-vue-projects",children:"Installing in Vue projects"}),`
+`,n.jsx(e.p,{children:"These instructions were used with Vue version 3.2.31."}),`
+`,n.jsx(e.h2,{id:"installing",children:"Installing"}),`
+`,n.jsx(e.h3,{id:"step-one",children:"Step one"}),`
+`,n.jsxs(e.p,{children:["Install the packages with ",n.jsx(e.code,{children:"npm"})," or ",n.jsx(e.code,{children:"yarn"}),"."]}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-shell",children:`npm install @ukic/canary-web-components @ukic/fonts
+
+yarn add @ukic/canary-web-components @ukic/fonts
+`})}),`
+`,n.jsx(e.p,{children:"Add options into your relevant config"}),`
+`,n.jsx(e.h3,{id:"-step-two",children:"### Step two"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-js",children:`// Only works if using in-browser compilation.
+app.config.compilerOptions.isCustomElement = (tag) => tag.includes("-");
+`})}),`
+`,n.jsx(e.h3,{id:"vite-config",children:"Vite config"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-js",children:`// vite.config.js
+import vue from "@vitejs/plugin-vue";
+
+export default {
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      },
+    }),
+  ],
+};
+`})}),`
+`,n.jsx(e.h3,{id:"vue-cli-config",children:"Vue CLI config"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-js",children:`// vue.config.js
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => ({
+        ...options,
+        compilerOptions: {
+          // treat any tag that starts with ic- as custom elements
+          isCustomElement: (tag) => tag.startsWith("ic-"),
+        },
+      }));
+  },
+};
+`})}),`
+`,n.jsxs(e.p,{children:["Import ",n.jsx(e.code,{children:"defineCustomElements"})," in your ",n.jsx(e.code,{children:"main.js"})," file and call it at the bottom of the file:"]}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-js",children:`// main.js
+import { defineCustomElements } from "@ukic/canary-web-components/loader";
+
+// ...
+
+defineCustomElements();
+`})}),`
+`,n.jsx(e.p,{children:"Declare and use components in your file, e.g.:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-html",children:`<ic-data-table ...></ic-data-table>
+`})}),`
+`,n.jsx(e.h2,{id:"importing-css-styles-and-fonts",children:"Importing CSS styles and fonts"}),`
+`,n.jsx(e.p,{children:"Import the ICDS CSS files into your app's top level CSS file:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`/* In your top level CSS file */
+/* Depending on your setup, a path to node_modules may be needed instead */
+@import "@ukic/fonts/dist/fonts.css";
+@import "@ukic/canary-web-components/dist/core/core.css";
+`})}),`
+`,n.jsx(e.p,{children:"Import the normalised styles that are used within the ICDS components if you would like to apply them to the rest of your project and any slotted elements:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`@import "@ukic/canary-web-components/dist/core/normalize.css";
+`})})]})}function p(s={}){const{wrapper:e}={...i(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(t,{...s})}):t(s)}export{p as default};
