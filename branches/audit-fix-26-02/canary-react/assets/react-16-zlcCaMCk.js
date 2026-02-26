0d89e06be29dc0d87096df9edfe71e2781d5e397
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-02-26 08:32:03 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@2389bc10b31205d27e485ec16dc9fe0142b05b5d 🚀

---
+++ branches/audit-fix-26-02/canary-react/assets/react-16-zlcCaMCk.js
@@ -0,0 +1,1 @@
+import{r}from"./index-D4EO1_WE.js";import"./iframe-DGkVIHT0.js";import"./preload-helper-PPVm8Dsz.js";var u=async(e,n)=>new Promise(o=>{r.render(e,n,()=>o(null))}),a=e=>{r.unmountComponentAtNode(e)};export{u as renderElement,a as unmountElement};
