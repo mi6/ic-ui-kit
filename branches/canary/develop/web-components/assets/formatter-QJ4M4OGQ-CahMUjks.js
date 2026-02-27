5a2eb9414a5a9f1e81a9044ca2b8f7dab2bd0fdc
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2026-02-27 11:56:29 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@a95b1f9dfe6ad7c6c725f2f546ca5b41c87ebdd7 🚀

--- branches/canary/develop/web-components/assets/formatter-QJ4M4OGQ-CahMUjks.js
+++
@@ -1,1 +0,0 @@
-import{d as m,b as e}from"./blocks-mHNWm8W1.js";import{b as o}from"./iframe-BgIFkKLg.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
