d85c75bf7436c1b7181af0620bd37fe2a4ef86ed
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2026-03-03 08:39:57 +0000

    feat(canary-web-components): remove ic-pagination-bar from canary components
    Remove ic-pagination-bar from canary components and move to core components

--- packages/canary-web-components/src/components/ic-pagination-bar/ic-pagination-bar.types.ts
+++
@@ -1,9 +0,0 @@
-export interface IcPageChangeEventDetail {
-  value: number;
-  fromItemsPerPage?: boolean;
-}
-
-export interface IcItemsPerPageChangeEventDetail {
-  value: number;
-  isUserAction?: boolean;
-}
