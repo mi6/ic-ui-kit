5b4b5999c67931e9f9db81fe09f9bf7c5dc6b3b9
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-03-03 13:29:20 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@87ef088d7b8dcbbe532caaf1489667cc6faf8155 🚀

---
+++ branches/4116-move-pagination-bar-out-of-canary/canary-web-components/assets/ic-table-of-contents-fGtb-XcM.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-3auJ7qBF.js";import{useMDXComponents as i}from"./index-BCkRKSua.js";import{I as m}from"./ic-table-of-contents.stories-D0pYEseu.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BUu1FUpe.js";import"./lit-html-DDAUtwXN.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
