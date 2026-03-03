bd8272e6cca2e1d54a837751653d15f32229d19d
GCHQDeveloper253 <GCHQDeveloper253@users.noreply.github.com>
2026-03-03 12:42:33 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@76a9d63956de203e02c26573b5f21c0321856e3f 🚀

---
+++ branches/2937-ic-chip-padding/web-components/assets/ic-card-vertical.entry-fdhw85sW.js
@@ -0,0 +1,722 @@
+import{r as w,h as n,H as E,g as C}from"./iframe-D1soBsLZ.js";import{r as h,i as t,o as z,a as I,j as S,I as r}from"./helpers-0f85537f-hEp0fLo6.js";import{C as F}from"./chevron-icon-589e3b46-BQ4i-iCo.js";import"./preload-helper-PPVm8Dsz.js";const R=`/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+
+/* Document
+   ========================================================================== */
+
+/**
+ * 1. Correct the line height in all browsers.
+ * 2. Prevent adjustments of font size after orientation changes in iOS.
+ */
+
+html {
+  line-height: 1.15; /* 1 */
+  -webkit-text-size-adjust: 100%; /* 2 */
+}
+
+/* Sections
+     ========================================================================== */
+
+/**
+   * Remove the margin in all browsers.
+   */
+
+body {
+  margin: 0;
+}
+
+/**
+   * Render the \`main\` element consistently in IE.
+   */
+
+main {
+  display: block;
+}
+
+/**
+   * Correct the font size and margin on \`h1\` elements within \`section\` and
+   * \`article\` contexts in Chrome, Firefox, and Safari.
+   */
+
+h1 {
+  font-size: 2em;
+  margin: 0.67em 0;
+}
+
+/* Grouping content
+     ========================================================================== */
+
+/**
+   * 1. Add the correct box sizing in Firefox.
+   * 2. Show the overflow in Edge and IE.
+   */
+
+hr {
+  box-sizing: content-box; /* 1 */
+  height: 0; /* 1 */
+  overflow: visible; /* 2 */
+}
+
+/**
+   * 1. Correct the inheritance and scaling of font size in all browsers.
+   * 2. Correct the odd \`em\` font sizing in all browsers.
+   */
+
+pre {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+
+/* Text-level semantics
+     ========================================================================== */
+
+/**
+   * Remove the gray background on active links in IE 10.
+   */
+
+a {
+  background-color: transparent;
+}
+
+/**
+   * 1. Remove the bottom border in Chrome 57-
+   * 2. Add the correct text decoration in Chrome, Edge, IE, Opera, and Safari.
+   */
+
+abbr[title] {
+  border-bottom: none; /* 1 */
+  text-decoration: underline; /* 2 */
+  -webkit-text-decoration: underline dotted;
+          text-decoration: underline dotted; /* 2 */
+}
+
+/**
+   * Add the correct font weight in Chrome, Edge, and Safari.
+   */
+
+b,
+strong {
+  font-weight: bolder;
+}
+
+/**
+   * 1. Correct the inheritance and scaling of font size in all browsers.
+   * 2. Correct the odd \`em\` font sizing in all browsers.
+   */
+
+code,
+kbd,
+samp {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+
+/**
+   * Add the correct font size in all browsers.
+   */
+
+small {
+  font-size: 80%;
+}
+
+/**
+   * Prevent \`sub\` and \`sup\` elements from affecting the line height in
+   * all browsers.
+   */
+
+sub,
+sup {
+  font-size: 75%;
+  line-height: 0;
+  position: relative;
+  vertical-align: baseline;
+}
+
+sub {
+  bottom: -0.25em;
+}
+
+sup {
+  top: -0.5em;
+}
+
+/* Embedded content
+     ========================================================================== */
+
+/**
+   * Remove the border on images inside links in IE 10.
+   */
+
+img {
+  border-style: none;
+}
+
+/* Forms
+     ========================================================================== */
+
+/**
+   * 1. Change the font styles in all browsers.
+   * 2. Remove the margin in Firefox and Safari.
+   */
+
+button,
+input,
+optgroup,
+select,
+textarea {
+  font-family: inherit; /* 1 */
+  font-size: 100%; /* 1 */
+  line-height: 1.15; /* 1 */
+  margin: 0; /* 2 */
+}
+
+/**
+   * Show the overflow in IE.
+   * 1. Show the overflow in Edge.
+   */
+
+button,
+input {
+  /* 1 */
+  overflow: visible;
+}
+
+/**
+   * Remove the inheritance of text transform in Edge, Firefox, and IE.
+   * 1. Remove the inheritance of text transform in Firefox.
+   */
+
+button,
+select {
+  /* 1 */
+  text-transform: none;
+}
+
+/**
+   * Correct the inability to style clickable types in iOS and Safari.
+   */
+
+button,
+[type="button"],
+[type="reset"],
+[type="submit"] {
+  -webkit-appearance: button;
+}
+
+/**
+   * Remove the inner border and padding in Firefox.
+   */
+
+button::-moz-focus-inner,
+[type="button"]::-moz-focus-inner,
+[type="reset"]::-moz-focus-inner,
+[type="submit"]::-moz-focus-inner {
+  border-style: none;
+  padding: 0;
+}
+
+/**
+   * Restore the focus styles unset by the previous rule.
+   */
+
+button:-moz-focusring,
+[type="button"]:-moz-focusring,
+[type="reset"]:-moz-focusring,
+[type="submit"]:-moz-focusring {
+  outline: 1px dotted ButtonText;
+}
+
+/**
+   * Correct the padding in Firefox.
+   */
+
+fieldset {
+  padding: 0.35em 0.75em 0.625em;
+}
+
+/**
+   * 1. Correct the text wrapping in Edge and IE.
+   * 2. Correct the color inheritance from \`fieldset\` elements in IE.
+   * 3. Remove the padding so developers are not caught out when they zero out
+   *    \`fieldset\` elements in all browsers.
+   */
+
+legend {
+  box-sizing: border-box; /* 1 */
+  color: inherit; /* 2 */
+  display: table; /* 1 */
+  max-width: 100%; /* 1 */
+  padding: 0; /* 3 */
+  white-space: normal; /* 1 */
+}
+
+/**
+   * Add the correct vertical alignment in Chrome, Firefox, and Opera.
+   */
+
+progress {
+  vertical-align: baseline;
+}
+
+/**
+   * Remove the default vertical scrollbar in IE 10+.
+   */
+
+textarea {
+  overflow: auto;
+}
+
+/**
+   * 1. Add the correct box sizing in IE 10.
+   * 2. Remove the padding in IE 10.
+   */
+
+[type="checkbox"],
+[type="radio"] {
+  box-sizing: border-box; /* 1 */
+  padding: 0; /* 2 */
+}
+
+/**
+   * Correct the cursor style of increment and decrement buttons in Chrome.
+   */
+
+[type="number"]::-webkit-inner-spin-button,
+[type="number"]::-webkit-outer-spin-button {
+  height: auto;
+}
+
+/**
+   * 1. Correct the odd appearance in Chrome and Safari.
+   * 2. Correct the outline style in Safari.
+   */
+
+[type="search"] {
+  -webkit-appearance: textfield; /* 1 */
+  outline-offset: -2px; /* 2 */
+}
+
+/**
+   * Remove the inner padding in Chrome and Safari on macOS.
+   */
+
+[type="search"]::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+
+/**
+   * 1. Correct the inability to style clickable types in iOS and Safari.
+   * 2. Change font properties to \`inherit\` in Safari.
+   */
+
+::-webkit-file-upload-button {
+  -webkit-appearance: button; /* 1 */
+  font: inherit; /* 2 */
+}
+
+/* Interactive
+     ========================================================================== */
+
+/*
+   * Add the correct display in Edge, IE 10+, and Firefox.
+   */
+
+details {
+  display: block;
+}
+
+/*
+   * Add the correct display in all browsers.
+   */
+
+summary {
+  display: list-item;
+}
+
+/* Misc
+     ========================================================================== */
+
+/**
+   * Add the correct display in IE 10+.
+   */
+
+template {
+  display: none;
+}
+
+/**
+   * Add the correct display in IE 10.
+   */
+
+[hidden] {
+  display: none;
+}
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font-style: inherit;
+  vertical-align: baseline;
+}
+
+
+:host {
+  display: flex;
+}
+
+a {
+  text-decoration: none !important;
+  color: var(--ic-card-text-primary) !important;
+}
+
+a:visited {
+  color: var(--ic-card-link-action-dark) !important;
+}
+
+button {
+  border: none;
+  background-color: transparent;
+  outline: var(--ic-hc-focus-outline);
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  border: var(--ic-space-1px) solid var(--ic-card-border-primary);
+  border-radius: var(--ic-border-radius);
+  box-sizing: border-box;
+  padding: var(--ic-space-md);
+  text-align: left;
+  color: var(--ic-card-text-primary);
+  transition: var(--ic-easing-transition-fast);
+  position: relative;
+  width: inherit;
+  min-width: -moz-fit-content;
+  min-width: fit-content;
+  height: -moz-fit-content;
+  height: fit-content;
+  min-height: 100%;
+
+  ::slotted(svg) {
+    fill: var(--ic-card-icon);
+  }
+
+  &.monochrome {
+    border: var(--ic-border-width) solid var(--ic-card-border-monochrome);
+  }
+
+  &.fullwidth {
+    width: 100%;
+  }
+
+  &.clickable {
+    &:hover {
+      background-color: var(--ic-card-background-hover);
+      border: var(--ic-space-1px) solid var(--ic-card-hover-border-color);
+      cursor: pointer;
+    }
+
+    &.focussed,
+    &:focus {
+      background-color: var(--ic-card-background-hover);
+      box-shadow: var(--ic-border-focus);
+      outline: var(--ic-hc-focus-outline);
+      border: var(--ic-space-1px) solid var(--ic-card-pressed-border-color);
+    }
+
+    &:active {
+      background-color: var(--ic-card-background-pressed);
+      box-shadow: var(--ic-border-focus);
+
+      .card-title {
+        text-decoration: none;
+      }
+    }
+
+    .card-title {
+      --ic-typography-color: var(--ic-card-clickable-text);
+
+      color: var(--ic-card-clickable-text);
+      text-decoration: underline;
+      text-decoration-thickness: var(--ic-space-1px);
+      margin-bottom: var(--ic-space-xxs);
+    }
+
+    .interaction-button,
+    .icon {
+      margin-bottom: var(--ic-space-xxs);
+    }
+  }
+
+  &.disabled {
+    border: var(--ic-space-1px) dashed var(--ic-card-disabled-border-color);
+
+    .card-message,
+    .subheading,
+    .card-title {
+      --ic-typography-color: var(--ic-card-disabled-text);
+    }
+
+    .card-title {
+      text-decoration: underline;
+      text-decoration-thickness: var(--ic-space-1px);
+      text-decoration-color: var(--ic-card-disabled-text);
+      color: var(--ic-card-disabled-text);
+    }
+
+    ::slotted(svg) {
+      fill: var(--ic-card-icon-disabled);
+    }
+  }
+}
+
+.card-header {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  display: flex;
+  align-items: center;
+  padding-right: var(--ic-space-xs);
+}
+
+.card.clickable:hover .card-title,
+.card.clickable:focus .card-title,
+.card.clickable.focussed .card-title {
+  display: inline-block;
+  border-bottom: 0.25rem solid !important;
+  margin-bottom: 0 !important;
+  text-decoration: none;
+}
+
+@supports (text-underline-offset: 25%) {
+  .card.clickable:hover .card-title,
+  .card.clickable:focus .card-title,
+  .card.clickable.focussed .card-title {
+    text-decoration-line: underline;
+    text-decoration-thickness: 25%;
+    text-underline-offset: 25%;
+    border-bottom: 0 !important;
+    margin-bottom: 0.25rem !important;
+  }
+}
+
+.subheading {
+  margin-top: var(--ic-space-xxs);
+}
+
+.adornment {
+  margin-top: var(--ic-space-xxs);
+}
+
+.card-message {
+  margin-top: var(--ic-space-md);
+  align-items: left;
+}
+
+.interaction-button {
+  margin-left: auto;
+}
+
+.image-top {
+  margin-bottom: var(--ic-space-md);
+  display: flex;
+  justify-content: center;
+}
+
+.image-mid {
+  margin-top: var(--ic-space-md);
+  display: flex;
+  justify-content: center;
+}
+
+.interaction-area {
+  display: flex;
+  flex-grow: 1;
+  gap: var(--ic-space-md);
+  margin-top: var(--ic-space-md);
+  align-items: flex-end;
+}
+
+.interaction-controls {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: var(--ic-space-sm);
+}
+
+.toggle-button {
+  width: 2.5rem;
+  height: 2.5rem;
+  padding: var(--ic-space-xs);
+  margin: var(--ic-space-1px) 0;
+  min-width: 0;
+  cursor: pointer;
+  transition: var(--ic-easing-transition-fast);
+  border-radius: var(--ic-border-radius);
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  border: none;
+  box-sizing: border-box;
+  white-space: nowrap;
+  vertical-align: middle;
+
+  &:hover {
+    background-color: var(--ic-card-background-hover);
+  }
+
+  &:focus {
+    box-shadow: var(--ic-border-focus);
+  }
+
+  &:active:not(:focus) {
+    background-color: var(--ic-card-background-pressed);
+  }
+
+  svg {
+    pointer-events: none;
+    width: 100% !important;
+    height: 100% !important;
+    fill: currentcolor !important;
+  }
+}
+
+#ic-tooltip-expand-button {
+  margin-left: auto;
+  position: relative;
+}
+
+.toggle-button-closed svg {
+  transform: rotate(90deg);
+}
+
+.toggle-button-expanded svg {
+  transform: rotate(-90deg);
+}
+
+.expanded-content {
+  margin-top: var(--ic-space-md);
+}
+
+/** High Contrast **/
+@media (forced-colors: active) {
+  .card {
+    ::slotted(svg) {
+      fill: currentcolor;
+    }
+
+    &.disabled {
+      border-color: GrayText !important;
+
+      ::slotted(svg) {
+        fill: GrayText !important;
+      }
+
+      .card-message,
+      .subheading,
+      .card-title {
+        color: GrayText;
+
+        --ic-typography-color: GrayText;
+      }
+    }
+  }
+
+  .toggle-button:focus,
+  .toggle-button:hover {
+    outline-color: Highlight;
+  }
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .card,
+  .toggle-button {
+    transition: none;
+  }
+}
+`,T=R,A=class{constructor(e){w(this,e),this.hostMutationObserver=null,this.areaExpanded=!1,this.isFocussed=!1,this.monochrome=!1,this.parentEl=null,this.parentIsAnchorTag=!1,this.clickable=!1,this.disabled=!1,this.expandable=!1,this.fullWidth=!1,this.hreflang="",this.message="",this.theme="inherit",this.parentFocussed=()=>{this.isFocussed=!0},this.parentBlurred=()=>{this.isFocussed=!1},this.toggleExpanded=()=>{this.areaExpanded=!this.areaExpanded}}watchDisabledHandler(){h(this.disabled,this.el)}disconnectedCallback(){var e;this.parentEl&&this.parentIsAnchorTag&&(this.parentEl.removeEventListener("focus",this.parentFocussed),this.parentEl.removeEventListener("blur",this.parentBlurred)),(e=this.hostMutationObserver)===null||e===void 0||e.disconnect()}componentWillLoad(){var e;this.parentEl=this.el.parentElement,((e=this.parentEl)===null||e===void 0?void 0:e.tagName)==="A"&&(this.clickable=!0,this.parentIsAnchorTag=!0,this.parentEl.classList.add("ic-card-wrapper-link"),this.parentEl.addEventListener("focus",this.parentFocussed),this.parentEl.addEventListener("blur",this.parentBlurred)),h(this.disabled,this.el)}componentDidLoad(){!t(this.el,"heading")&&z([{prop:this.heading,propName:"heading"}],"Card"),this.updateTheme(),this.hostMutationObserver=new MutationObserver(e=>I(e,["message","adornment","expanded-content","image-top","image-mid","icon","interaction-button","badge","interaction-controls"],this)),this.hostMutationObserver.observe(this.el,{childList:!0})}handleHostClick(e){this.disabled&&e.stopImmediatePropagation()}brandChangeHandler(e){this.updateTheme(e.detail.mode)}async setFocus(){var e,a;const i=this.el.shadowRoot;i&&(i.querySelector("a")?(e=i.querySelector("a"))===null||e===void 0||e.focus():i.querySelector("button")&&((a=i.querySelector("button"))===null||a===void 0||a.focus()))}updateTheme(e=null){const a=S(this.el,e);a!==r.Default&&(this.monochrome=!0,this.theme=a===r.Light?r.Dark:r.Light)}render(){const{clickable:e,disabled:a,expandable:i,fullWidth:f,heading:m,isFocussed:p,message:o,monochrome:g,href:d,hreflang:u,parentIsAnchorTag:s,referrerpolicy:v,rel:x,subheading:c,target:y,theme:l}=this,b=s||!e?"div":d===void 0?"button":"a",k=b=="a"&&{href:d,hrefLang:u,referrerPolicy:v,rel:x,target:y};return n(E,{key:"46770bac0c91ca2ff14b5b23b14d47e9ea9aa517",class:{[`ic-theme-${l}`]:l!=="inherit"}},n(b,Object.assign({key:"f9c57247f148b34efe2c3e7daaea5972c0055d08",class:{card:!0,clickable:!!e&&!a,disabled:!!a,fullwidth:!!f,focussed:p,monochrome:g},tabindex:e&&!s?0:void 0,"aria-disabled":a?"true":null,disabled:a},k),t(this.el,"image-top")&&n("div",{key:"68fd16d99929db18ce1d15d85df109fc0c35da55",class:"image-top"},n("slot",{key:"ae5268f759fcb5de6e234c98c9b36fca541d6a21",name:"image-top"})),n("div",{key:"fe1f84f925b429c5b9b655255d0d6d2b88a6dd6a",class:"card-header"},t(this.el,"icon")&&n("div",{key:"4e30702242894da1f3ccd9e1e358d1c3fbd71a2e",class:"icon"},n("slot",{key:"dfadc70813f5154653dd786d60f720c2d5fd2e35",name:"icon"})),n("div",{key:"4acdb8d08b23a8439969975ebb50f714ac7a730b",class:"card-title"},n("slot",{key:"39426e1483af5888db4de69f4f4a033360ad1aa2",name:"heading"},n("ic-typography",{key:"779ca8a43a40a224496168487d17c1a98bef414e",variant:"h4"},n("p",{key:"5814c1302336b5af5287b5089318ead442894439"},m)))),t(this.el,"interaction-button")&&n("div",{key:"e50238ba2fd6b157e1fce79fe80f509d87293843",class:"interaction-button"},n("slot",{key:"c1c350b36e8f65b805b5d31342ee34d652294ab0",name:"interaction-button"}))),(c||t(this.el,"subheading"))&&n("div",{key:"f07efe89a5206f1f71208065295b49b19212ab8a",class:"subheading"},n("slot",{key:"863e9984f2578003f83dc612c212a81f2077d536",name:"subheading"},n("ic-typography",{key:"494ca7b92bf8b74f1b8e8f3754d936f7e197fa01",variant:"subtitle-small"},c))),t(this.el,"adornment")&&n("div",{key:"692f8c815513ef39673d6fca2c6f3f6f32e485a7",class:"adornment"},n("slot",{key:"0af23493519cb12100484c3b00203fe5a48aa5c5",name:"adornment"})),t(this.el,"image-mid")&&n("div",{key:"fb024ab8f4a7278ea339abfe9365167120410aa5",class:"image-mid"},n("slot",{key:"aeba0ea8a10f062fcc52265f53101399b17ddd3b",name:"image-mid"})),(o||t(this.el,"message"))&&n("div",{key:"0651e40348c1e8afd2ccd06990b53aafc5231f2d",class:"card-message"},o&&n("ic-typography",{key:"d5df79541fd96f8bd267a856d9131f95d8f6fcd8",variant:"body"},o),t(this.el,"message")&&n("slot",{key:"c52651c591659c971cc568e390bf8559d5019b16",name:"message"})),(t(this.el,"interaction-controls")||i)&&n("div",{key:"aec46b8aad2eb2fc326ff8c3112d5e15a9cde438",class:"interaction-area"},n("div",{key:"2192aa2ea298e807dd5d9586de7d02db349170aa",class:"interaction-controls"},n("slot",{key:"88cb68760b56f8fd594e7a63e99c9d4785d365c1",name:"interaction-controls"})),i&&n("ic-tooltip",{key:"e74d63f15d9b6d928d9fc3873d3575d33fada88c",id:"ic-tooltip-expand-button",label:"Toggle expandable area",silent:!0},n("button",{key:"ea7d567651d43918de8c9c5fa39834f74a5a5d6d",class:{"toggle-button":!0,[`toggle-button-${this.areaExpanded?"expanded":"closed"}`]:!0},"aria-label":"Toggle expandable area","aria-expanded":`${this.areaExpanded}`,"aria-controls":this.areaExpanded?"expanded-content-area":null,onClick:this.toggleExpanded,innerHTML:F}))),t(this.el,"expanded-content")&&this.areaExpanded&&n("div",{key:"816ef1364b763c1f39bb69e9289993001bcf16dc",class:"expanded-content",id:"expanded-content-area"},n("slot",{key:"c6565b2c7fb145e9a7409626ff97fed2eca4386a",name:"expanded-content"})),t(this.el,"badge")&&n("slot",{key:"8762e11d3bdbfe493c8228a9bf5c43a003552516",name:"badge"})))}get el(){return C(this)}static get watchers(){return{disabled:["watchDisabledHandler"]}}};A.style=T;export{A as ic_card_vertical};
---
+++ branches/2937-ic-chip-padding/web-components/assets/readme-Cn1sgCla.md
@@ -0,0 +1,63 @@
+# ic-navigation-button
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property             | Attribute        | Description                                                                                                                             | Type                                                                                                                                                                                                  | Default     |
+| -------------------- | ---------------- | --------------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ----------- |
+| `download`           | `download`       | If `true`, the user can save the linked URL instead of navigating to it.                                                                | `boolean \| string`                                                                                                                                                                                   | `false`     |
+| `href`               | `href`           | The URL that the link points to. This will render the button as an "a" tag.                                                             | `string \| undefined`                                                                                                                                                                                 | `undefined` |
+| `hreflang`           | `hreflang`       | The human language of the linked URL.                                                                                                   | `string \| undefined`                                                                                                                                                                                 | `undefined` |
+| `label` _(required)_ | `label`          | The label info to display.                                                                                                              | `string`                                                                                                                                                                                              | `undefined` |
+| `referrerpolicy`     | `referrerpolicy` | How much of the referrer to send when following the link.                                                                               | `"" \| "no-referrer" \| "no-referrer-when-downgrade" \| "origin" \| "origin-when-cross-origin" \| "same-origin" \| "strict-origin" \| "strict-origin-when-cross-origin" \| "unsafe-url" \| undefined` | `undefined` |
+| `rel`                | `rel`            | The relationship of the linked URL as space-separated link types.                                                                       | `string \| undefined`                                                                                                                                                                                 | `undefined` |
+| `target`             | `target`         | The place to display the linked URL, as the name for a browsing context (a tab, window, or iframe).                                     | `string \| undefined`                                                                                                                                                                                 | `undefined` |
+| `theme`              | `theme`          | Sets the theme color to the dark or light theme color. "inherit" will set the color based on the system settings or ic-theme component. | `"dark" \| "inherit" \| "light"`                                                                                                                                                                      | `"inherit"` |
+
+
+## Methods
+
+### `setFocus() => Promise<void>`
+
+Sets focus on the native `button`.
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+
+## Slots
+
+| Slot      | Description                                             |
+| --------- | ------------------------------------------------------- |
+| `"badge"` | Badge component overlaying the top right of the button. |
+| `"icon"`  | Content will be placed to the left of the button label. |
+
+
+## Dependencies
+
+### Depends on
+
+- [ic-button](../ic-button)
+
+### Graph
+```mermaid
+graph TD;
+  ic-navigation-button --> ic-button
+  ic-button --> ic-typography
+  ic-button --> ic-loading-indicator
+  ic-button --> ic-tooltip
+  ic-loading-indicator --> ic-typography
+  ic-tooltip --> ic-typography
+  style ic-navigation-button fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+
