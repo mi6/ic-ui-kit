9fa7e08a4fb062608e0e31d89a07985aace8a2b4
lz405 <lz405@users.noreply.github.com>
2026-03-04 14:33:06 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@0c62c16f4c1b26479cfc4cc641d6dc33bdf15fa4 🚀

--- branches/666-refactor-branch/canary-web-components/assets/ic-table-of-contents-DNRJYcD6.js
+++
@@ -1,2 +0,0 @@
-import{j as t,M as e,S as s}from"./blocks-DXaeBB6t.js";import{useMDXComponents as i}from"./index-CCBymIu2.js";import{I as m}from"./ic-table-of-contents.stories-D0pYEseu.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DlpxCI_V.js";import"./lit-html-DDAUtwXN.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
-`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
