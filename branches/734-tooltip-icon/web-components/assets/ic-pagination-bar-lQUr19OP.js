1f24d908e14dd4e0d6382f51d6275ac68a24ef33
GCHQDeveloper416 <GCHQDeveloper416@users.noreply.github.com>
2026-03-12 11:46:23 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@c4a551eaad375538b21e23ba54e6ddcaaf8afee4 🚀

---
+++ branches/734-tooltip-icon/web-components/assets/ic-pagination-bar-lQUr19OP.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-D9oKeYzH.js";import{useMDXComponents as i}from"./index-DUa24Hhb.js";import{I as a}from"./ic-pagination-bar.stories-DKlLQxKw.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DkpUTMS4.js";import"./lit-html-CWQq0DyO.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
