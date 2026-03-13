06883546a617fad36f1b914caf6842813cf5a235
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2026-03-13 07:55:14 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@7683cf1ffb0e51e90cfa71b7affb0659926807e0 🚀

--- branches/develop/web-components/assets/formatter-QJ4M4OGQ-ifaULqW1.js
+++
@@ -1,1 +0,0 @@
-import{d as i,e as m}from"./blocks-BqHWECqG.js";import{b as o}from"./iframe-DJ46H7kM.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{s as formatter};
