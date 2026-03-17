f4bed8049d7a2da110800f782f04dad338a1afd9
MI6-711 <MI6-711@users.noreply.github.com>
2026-03-17 09:37:06 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@4642d93ac62192450c967b8dacb51634e9f9ac28 🚀

--- branches/4229-create-a-large-variant-of-radio-button/web-components/assets/ic-select_(multi)-DLdmttiH.js
+++
@@ -1,3 +0,0 @@
-import{j as t,M as n,a as s,S as i}from"./blocks-DTC8XKw_.js";import{useMDXComponents as m}from"./index-BtgKQKXw.js";import{r as a}from"./readme-DhSAGobi.js";import{I as p}from"./ic-select_(multi).stories-BISp0Swf.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CMMr3Tpc.js";import"./lit-html-CWQq0DyO.js";function e(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:p}),`
-`,t.jsx(s,{children:a}),`
-`,t.jsx(i,{})]})}function S(r={}){const{wrapper:o}={...m(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e()}export{S as default};
