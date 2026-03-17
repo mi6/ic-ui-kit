72a104a0397c58eca6c9043b097a7eeb8029298a
MI6-711 <MI6-711@users.noreply.github.com>
2026-03-17 09:37:36 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@4642d93ac62192450c967b8dacb51634e9f9ac28 🚀

---
+++ branches/4229-create-a-large-variant-of-radio-button/canary-web-components/assets/formatter-QJ4M4OGQ-Ck26wys_.js
@@ -0,0 +1,1 @@
+import{d as m,b as e}from"./blocks-EplFPz12.js";import{b as o}from"./iframe-DgBt66dS.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
