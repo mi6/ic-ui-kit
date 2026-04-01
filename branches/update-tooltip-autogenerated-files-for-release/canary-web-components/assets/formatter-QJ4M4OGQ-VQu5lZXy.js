946fa95f524165abcaf7dd5fb944c46030328c74
lz405 <lz405@users.noreply.github.com>
2026-04-01 12:45:58 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@13e93ea65738cf66785031bac683ea3a9dba1dba 🚀

---
+++ branches/update-tooltip-autogenerated-files-for-release/canary-web-components/assets/formatter-QJ4M4OGQ-VQu5lZXy.js
@@ -0,0 +1,1 @@
+import{d as m,b as e}from"./blocks-ZBBo9b3u.js";import{b as o}from"./iframe-Du3IWQQJ.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
