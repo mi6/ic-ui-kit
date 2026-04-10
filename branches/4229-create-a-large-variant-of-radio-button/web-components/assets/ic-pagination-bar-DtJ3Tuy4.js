bdb7208edd94573a9a5e3261836520a6334aa3d9
GCHQ-Developer-112 <GCHQ-Developer-112@users.noreply.github.com>
2026-04-10 11:46:12 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@56ae3d7b323a0419ec224a10513afeab6e37a2b4 🚀

--- branches/4229-create-a-large-variant-of-radio-button/web-components/assets/ic-pagination-bar-DtJ3Tuy4.js
+++
@@ -1,2 +0,0 @@
-import{j as t,M as e,S as s}from"./blocks-BQXcXw7e.js";import{useMDXComponents as i}from"./index-BGSbVKZ0.js";import{I as a}from"./ic-pagination-bar.stories-DKlLQxKw.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-fRSHFzrt.js";import"./lit-html-CWQq0DyO.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
-`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
