8f13d90003ea11cd27364371d8388e21c53e9b48
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-04-13 10:47:57 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@ded6670fea48ee5816378c533f8e5b23be4da8d5 🚀

---
+++ branches/audit-fix-13/04/web-components/assets/ic-tooltip-Pss914nk.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as m}from"./blocks-BQ9PfdbC.js";import{useMDXComponents as a}from"./index-3BybqTqu.js";import{I as i}from"./ic-tooltip.stories-BeNh3Fi_.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CXereksS.js";import"./lit-html-CWQq0DyO.js";const p=""+new URL("readme-BkHz-L2o.md",import.meta.url).href;function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:i}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(m,{})]})}function l(o={}){const{wrapper:r}={...a(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e()}export{l as default};
