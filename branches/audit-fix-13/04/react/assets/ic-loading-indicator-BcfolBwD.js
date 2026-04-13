20d56d4e7afaee06dc630a795d368cade28bb0ad
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-04-13 13:51:50 +0000

    Branch: audit-fix-13/04 deleted in ic-ui-kit

--- branches/audit-fix-13/04/react/assets/ic-loading-indicator-BcfolBwD.js
+++
@@ -1,3 +0,0 @@
-import{j as t,M as e,a as s,S as a}from"./blocks-DXgm3A2_.js";import{useMDXComponents as m}from"./index-CGoCeeIW.js";import{I as i}from"./ic-loading-indicator.stories-DJ9Pz8us.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-BP3NWfRm.js";import"./index-DWnWy31L.js";const c=""+new URL("readme-BkNDwlnZ.md",import.meta.url).href;function o(r){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:i}),`
-`,t.jsx(s,{children:c}),`
-`,t.jsx(a,{})]})}function M(r={}){const{wrapper:n}={...m(),...r.components};return n?t.jsx(n,{...r,children:t.jsx(o,{...r})}):o()}export{M as default};
--- branches/audit-fix-13/04/react/assets/ic-toggle-button-group-ID-fvCzo.js
+++
@@ -1,3 +0,0 @@
-import{j as t,M as e,a as s,S as m}from"./blocks-DXgm3A2_.js";import{useMDXComponents as a}from"./index-CGoCeeIW.js";import{r as i}from"./readme-C_BXe4qm.js";import{I as p}from"./ic-toggle-button-group.stories-y95-Tk-C.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-BP3NWfRm.js";import"./index-DWnWy31L.js";import"./slottedSVG-CzaXPoiZ.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:p}),`
-`,t.jsx(s,{children:i}),`
-`,t.jsx(m,{})]})}function g(o={}){const{wrapper:r}={...a(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{g as default};
