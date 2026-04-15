60c8b18b182991656d25246a269f41983d5c2dfb
GCHQDeveloper416 <GCHQDeveloper416@users.noreply.github.com>
2026-04-15 13:47:05 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@4284a457a318d204e7b35079463be0612312100e 🚀

---
+++ branches/734-tooltip-icon/web-components/assets/ic-card-horizontal-CwAjTmQD.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-DiqWi7jd.js";import{useMDXComponents as i}from"./index-D-nZtLLd.js";import{I as m}from"./ic-card-horizontal.stories-D1_UPYl7.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DNMvx9RX.js";import"./lit-html-CWQq0DyO.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
