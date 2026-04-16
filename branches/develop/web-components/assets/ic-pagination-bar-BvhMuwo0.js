0287ac8dcc2dc0947efb52be1bb41e3ebfaa00b5
GCHQ-Developer-299 <111292477+GCHQ-Developer-299@users.noreply.github.com>
2026-04-16 14:59:42 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@f85dda8156315e4273da51669e58b68537ec8ee8 🚀

---
+++ branches/develop/web-components/assets/ic-pagination-bar-BvhMuwo0.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-CPa3puMk.js";import{useMDXComponents as i}from"./index-quuvMevd.js";import{I as a}from"./ic-pagination-bar.stories-CT26dN7W.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-lg5-impA.js";import"./lit-html-B6sBSybp.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
