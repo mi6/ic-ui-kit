1b17615567592c2ba8f622c835776dc0347ac890
GCHQ-Developer-847 <GCHQ-Developer-847@users.noreply.github.com>
2026-04-21 10:29:21 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@f911eac7dc51ec90ada08215a1efa372cf8d7eaf 🚀

---
+++ branches/feat/4191-upgrade-jest-to-v30/canary-react/assets/ic-date-picker-jFD2c6RQ.js
@@ -0,0 +1,2 @@
+import{j as t,M as n,S as s}from"./blocks-EeJYZtg9.js";import{useMDXComponents as i}from"./index-CQ6WFVBR.js";import{I as m}from"./ic-date-picker.stories-DDcSfsDr.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-D0Fl-YRZ.js";import"./index-MY8r2sw-.js";import"./components-BYY6RCWN.js";function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e()}export{M as default};
