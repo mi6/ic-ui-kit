b94e322498c27071948c8a1cde90233662c5cba7
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-04-24 10:22:12 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@67df8b361964328b30a4de9625ee3bf4d08b85db 🚀

---
+++ branches/4299-upgrade-node-in-workflows/web-components/assets/ic-button-BtoGpDDZ.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as m}from"./blocks-DBvaHmKc.js";import{useMDXComponents as a}from"./index-B47dYoVw.js";import{I as i}from"./ic-button.stories-DztGZptn.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DOstQ1Y1.js";import"./lit-html-B6sBSybp.js";const p=""+new URL("readme-CMnIj5XY.md",import.meta.url).href;function e(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:i}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(m,{})]})}function l(r={}){const{wrapper:o}={...a(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e()}export{l as default};
