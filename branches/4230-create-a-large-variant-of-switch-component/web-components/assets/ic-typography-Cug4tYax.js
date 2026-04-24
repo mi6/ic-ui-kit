20ca519494f7015c76a4302b96345225cb5a654d
GCHQ-Developer-112 <GCHQ-Developer-112@users.noreply.github.com>
2026-04-24 15:45:48 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@13b1c2814aa839f52095469b284100cc42336151 🚀

---
+++ branches/4230-create-a-large-variant-of-switch-component/web-components/assets/ic-typography-Cug4tYax.js
@@ -0,0 +1,3 @@
+import{j as r,M as n,a as s,S as m}from"./blocks-BP7qaChi.js";import{useMDXComponents as a}from"./index-CaV7zrAm.js";import{I as i}from"./ic-typography.stories-Ctoa-zN-.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CTpbBakO.js";import"./lit-html-B6sBSybp.js";const p=""+new URL("readme-CpIDx9oN.md",import.meta.url).href;function e(t){return r.jsxs(r.Fragment,{children:[r.jsx(n,{of:i}),`
+`,r.jsx(s,{children:p}),`
+`,r.jsx(m,{})]})}function h(t={}){const{wrapper:o}={...a(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(e,{...t})}):e()}export{h as default};
