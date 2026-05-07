502f097b2b2d76c89f562197e569d3504ee492f2
jd3267 <jd3267@users.noreply.github.com>
2026-05-07 14:57:50 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@9a727fe137b7a4ec8f85cc18b6a180f48e59e6e1 🚀

---
+++ branches/1536-make-disabled-options-on-ic-select-focusable/web-components/assets/ic-pagination-DJSIbsW6.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as a}from"./blocks-CXFlWEkx.js";import{useMDXComponents as i}from"./index-BskazxMw.js";import{I as m}from"./ic-pagination.stories-CAgP6_bu.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BRN2AVIz.js";import"./lit-html-B6sBSybp.js";const p=""+new URL("readme-CHlALite.md",import.meta.url).href;function o(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:m}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(a,{})]})}function l(r={}){const{wrapper:e}={...i(),...r.components};return e?t.jsx(e,{...r,children:t.jsx(o,{...r})}):o()}export{l as default};
