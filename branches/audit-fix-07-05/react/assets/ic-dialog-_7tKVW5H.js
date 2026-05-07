2427b3c58142a1ce1fabc8308d48f99dd629bba9
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-05-07 08:52:35 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@7df438173b4af64d186c6f6999fdac2b33a71f2d 🚀

---
+++ branches/audit-fix-07-05/react/assets/ic-dialog-_7tKVW5H.js
@@ -0,0 +1,3 @@
+import{j as t,M as n,a as s,S as m}from"./blocks-DFCQYxGu.js";import{useMDXComponents as a}from"./index-S7ujkgaZ.js";import{I as i}from"./ic-dialog.stories-ZluRq8uC.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-C08Q6Qc-.js";import"./index-CQsFW_qS.js";import"./slottedSVG-iit3tI_n.js";const p=""+new URL("readme-FS0cf6Ci.md",import.meta.url).href;function e(r){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:i}),`
+`,t.jsx(s,{children:p}),`
+`,t.jsx(m,{})]})}function l(r={}){const{wrapper:o}={...a(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e()}export{l as default};
