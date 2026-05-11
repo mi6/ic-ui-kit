b2aa7af90baf86ab013eeda59dc12a8f4e428938
GCHQ-Developer-112 <GCHQ-Developer-112@users.noreply.github.com>
2026-05-11 10:57:45 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@6a6096b142e9c6a87a0316e2016c9d86a46c3720 🚀

---
+++ branches/audit-fix-11-05/canary-react/assets/formatter-QJ4M4OGQ-VmNxY3FV.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-BtQxqgHu.js";import{i as o}from"./iframe-C3b6VaJv.js";import"./preload-helper-PPVm8Dsz.js";import"./index-ZfeW3ZYy.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
