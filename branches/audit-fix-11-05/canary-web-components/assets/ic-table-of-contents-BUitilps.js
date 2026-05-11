5c8d8f17f60211ea7814100377802a2e784d9263
GCHQ-Developer-112 <GCHQ-Developer-112@users.noreply.github.com>
2026-05-11 10:57:28 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@6a6096b142e9c6a87a0316e2016c9d86a46c3720 🚀

---
+++ branches/audit-fix-11-05/canary-web-components/assets/ic-table-of-contents-BUitilps.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-CwEjedzJ.js";import{useMDXComponents as i}from"./index-BQZ79Fw9.js";import{I as m}from"./ic-table-of-contents.stories-DyJLsHB1.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CTZ9hgJQ.js";import"./lit-html-B6sBSybp.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
+`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
