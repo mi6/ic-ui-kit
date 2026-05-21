46d008dd11ba409d973ccf6bad032c7e1a462d25
jd3267 <jd3267@users.noreply.github.com>
2026-05-21 13:37:49 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@e5686bf020ecc7ac7ec6889d1f40084a8ae15189 🚀

---
+++ branches/4484-audit-fix-210526/react/assets/ic-card-horizontal-_L57GvYF.js
@@ -0,0 +1,2 @@
+import{j as t,M as e,S as s}from"./blocks-FZ_uHu9D.js";import{useMDXComponents as i}from"./index-CrsfMrbC.js";import{I as a}from"./ic-card-horizontal.stories-Dk3iQGI6.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-C8YUw7Fe.js";import"./index-CYwpFK8f.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
+`,t.jsx(s,{})]})}function j(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{j as default};
