d6990361e2a3e0f35e0e012a8e18c643483b1d60
GCHQ-Developer-530 <111882034+GCHQ-Developer-530@users.noreply.github.com>
2026-06-08 09:05:07 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@9215c543bb2b598f67d5e4c43489c7886e4826af 🚀

--- branches/develop/web-components/assets/ic-card-horizontal-CihiJ8Mu.js
+++
@@ -1,2 +0,0 @@
-import{j as t,M as e,S as s}from"./blocks-Jyi3o9Uf.js";import{useMDXComponents as i}from"./index-ciIpw58e.js";import{I as m}from"./ic-card-horizontal.stories-DlN9YM0J.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-pgZV3zTU.js";import"./lit-html-B6sBSybp.js";function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:m}),`
-`,t.jsx(s,{})]})}function M(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n()}export{M as default};
