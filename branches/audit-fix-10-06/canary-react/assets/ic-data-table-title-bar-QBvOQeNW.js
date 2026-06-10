f944d54f563d5438effd6552347142ab00009e7c
GCHQ-Developer-530 <GCHQ-Developer-530@users.noreply.github.com>
2026-06-10 07:11:21 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@24e5f01096556e9e244d1ac795d2c806d2c9d745 🚀

---
+++ branches/audit-fix-10-06/canary-react/assets/ic-data-table-title-bar-QBvOQeNW.js
@@ -0,0 +1,2 @@
+import{j as t,M as n,S as s}from"./blocks-DZcw2x5r.js";import{useMDXComponents as i}from"./index-DjRM6u6S.js";import{I as m}from"./ic-data-table-title-bar.stories-BA8KG5ZX.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-BRwE2Yak.js";import"./index-O7bxg-80.js";import"./mdi-BzQ819As.js";import"./components-CUqGtqoO.js";import"./slottedSVG-Sa10w0h1.js";function e(o){return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:m}),`
+`,t.jsx(s,{})]})}function d(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e()}export{d as default};
