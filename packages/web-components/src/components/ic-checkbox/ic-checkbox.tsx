b06eb78d9d3532efbc153cc9cb9d3db34633dafb
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-25 00:22:37 +0100

    fix(web-components): #3879 mark checkbox checkmark decorative

--- packages/web-components/src/components/ic-checkbox/ic-checkbox.tsx
+++ packages/web-components/src/components/ic-checkbox/ic-checkbox.tsx
@@ -262,13 +262,14 @@ export class Checkbox {
           ) : (
             checked && (
               <svg
+                aria-hidden="true"
+                focusable="false"
                 class="checkmark"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
                 fill-rule="evenodd"
                 clip-rule="evenodd"
               >
-                <title>checkmark icon</title>
                 <path d="M21 6.285l-11.16 12.733-6.84-6.018 1.319-1.49 5.341 4.686 9.865-11.196 1.475 1.285z" />
               </svg>
             )
