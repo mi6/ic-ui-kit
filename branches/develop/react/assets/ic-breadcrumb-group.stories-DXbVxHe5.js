9419c20c9cfae201894ab73c2d4c75badea3b424
GCHQ-Developer-847 <111882109+GCHQ-Developer-847@users.noreply.github.com>
2026-09-03 14:14:36 +0000

    Deploying to gh-pages from @ mi6/ic-ui-kit@27800140a505ccbf406cfdba06eb13491bf257f1 🚀

---
+++ branches/develop/react/assets/ic-breadcrumb-group.stories-DXbVxHe5.js
@@ -0,0 +1,144 @@
+import{a as e,i as t}from"./preload-helper-xPQekRTU.js";import{H as n,O as r,c as i,in as a,l as o,lt as s,t as c,ut as l}from"./iframe-BOA-jNtR.js";import{n as u,t as d}from"./slottedSVG-KIv07TK4.js";import{a as f,c as p,i as m,n as h,o as g,r as _,t as v}from"./dist-BzsGiuNZ.js";var y=e({Back:()=>k,Collapsed:()=>j,Default:()=>D,Icon:()=>A,Playground:()=>N,ReactRouter:()=>M,SlottedLinks:()=>O,__namedExportsOrder:()=>P,default:()=>E}),b,x,S,C,w,T,E,D,O,k,A,j,M,N,P,F=t((()=>{a(),l(),u(),_(),b=c(),x=()=>(0,b.jsxs)(n,{children:[(0,b.jsx)(s,{variant:`h1`,children:`Hi there!`}),(0,b.jsx)(s,{children:`This example is demonstrating the breadcrumb and breadcrumb group components with React Router`})]}),S=()=>(0,b.jsxs)(n,{children:[(0,b.jsx)(s,{variant:`h1`,children:`Daily tippers`}),(0,b.jsx)(s,{children:`Lorem ipsum doloe sit amet, consectetur adipiscing`})]}),C=()=>(0,b.jsxs)(n,{children:[(0,b.jsx)(s,{variant:`h1`,children:`About`}),(0,b.jsx)(s,{children:`Lorem ipsum doloe sit amet, consectetur adipiscing`})]}),w=()=>{let e=p();return(0,b.jsxs)(o,{children:[(0,b.jsx)(i,{pageTitle:`Home`,current:e.pathname===`/`,children:(0,b.jsx)(r,{children:(0,b.jsx)(h,{to:`/`,slot:`router-item`,children:`Home`})})}),(0,b.jsx)(i,{pageTitle:`Daily Tippers`,current:e.pathname===`/daily-tippers`,children:(0,b.jsx)(r,{children:(0,b.jsx)(h,{to:`/daily-tippers`,slot:`router-item`,children:`Daily Tippers`})})}),(0,b.jsx)(i,{pageTitle:`About`,current:e.pathname===`/about`,children:(0,b.jsx)(r,{children:(0,b.jsx)(h,{to:`/about`,slot:`router-item`,children:`About`})})})]})},T={backBreadcrumbOnly:!1,collapsed:!1,current:!1,href:`/`,pageTitle:`Home`,iconSlot:!0,theme:`inherit`,monochrome:!1},E={title:`Breadcrumb`,component:o},D={render:()=>(0,b.jsx)(`div`,{children:(0,b.jsxs)(o,{children:[(0,b.jsx)(i,{pageTitle:`foo`,href:`/foo`}),(0,b.jsx)(i,{pageTitle:`bar`,href:`/bar`}),(0,b.jsx)(i,{pageTitle:`baz`,href:`/baz`,current:!0})]})}),name:`Default`},O={render:()=>(0,b.jsx)(`div`,{children:(0,b.jsxs)(o,{children:[(0,b.jsx)(i,{children:(0,b.jsx)(r,{children:(0,b.jsx)(`a`,{slot:`router-item`,href:`/foo`,children:`Foo`})})}),(0,b.jsx)(i,{children:(0,b.jsx)(r,{children:(0,b.jsx)(`a`,{slot:`router-item`,href:`/bar`,children:`Bar`})})}),(0,b.jsx)(i,{current:!0,children:(0,b.jsx)(r,{children:(0,b.jsx)(`a`,{slot:`router-item`,href:`/baz`,children:`Baz`})})})]})}),name:`Slotted links`},k={render:()=>(0,b.jsx)(`div`,{children:(0,b.jsxs)(o,{backBreadcrumbOnly:!0,children:[(0,b.jsx)(i,{pageTitle:`foo`,href:`/foo`}),(0,b.jsx)(i,{pageTitle:`bar`,href:`/bar`}),(0,b.jsx)(i,{pageTitle:`baz`,href:`/baz`,current:!0})]})}),name:`Back`},A={render:()=>(0,b.jsx)(`div`,{children:(0,b.jsxs)(o,{children:[(0,b.jsx)(i,{pageTitle:`foo`,href:`/foo`,children:(0,b.jsxs)(d,{slot:`icon`,height:`24px`,viewBox:`0 0 24 24`,width:`24px`,children:[(0,b.jsx)(`path`,{d:`M0 0h24v24H0V0z`,fill:`none`}),(0,b.jsx)(`path`,{d:`M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z`})]})}),(0,b.jsx)(i,{pageTitle:`bar`,href:`/bar`,children:(0,b.jsxs)(d,{slot:`icon`,height:`24px`,viewBox:`0 0 24 24`,width:`24px`,children:[(0,b.jsx)(`path`,{d:`M0 0h24v24H0V0z`,fill:`none`}),(0,b.jsx)(`path`,{d:`M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z`})]})}),(0,b.jsx)(i,{pageTitle:`baz`,href:`/baz`,current:!0,children:(0,b.jsxs)(d,{slot:`icon`,height:`24px`,viewBox:`0 0 24 24`,width:`24px`,children:[(0,b.jsx)(`path`,{d:`M0 0h24v24H0V0z`,fill:`none`}),(0,b.jsx)(`path`,{d:`M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z`})]})})]})}),name:`Icon`},j={render:()=>(0,b.jsx)(`div`,{children:(0,b.jsxs)(o,{collapsed:!0,children:[(0,b.jsx)(i,{pageTitle:`foo`,href:`/foo`}),(0,b.jsx)(i,{pageTitle:`bar`,href:`/bar`}),(0,b.jsx)(i,{pageTitle:`baz`,href:`/baz`}),(0,b.jsx)(i,{pageTitle:`qux`,href:`/qux`}),(0,b.jsx)(i,{pageTitle:`quxx`,href:`/quxx`,current:!0})]})}),name:`Collapsed`},M={render:()=>(0,b.jsxs)(`div`,{children:[(0,b.jsx)(w,{}),(0,b.jsxs)(g,{children:[(0,b.jsx)(f,{path:`/`,element:(0,b.jsx)(x,{})}),(0,b.jsx)(f,{path:`/daily-tippers`,element:(0,b.jsx)(S,{})}),(0,b.jsx)(f,{path:`/about`,element:(0,b.jsx)(C,{})})]}),(0,b.jsxs)(`div`,{style:{display:`flex`,gap:`10px`,marginTop:`16px`},children:[(0,b.jsx)(s,{children:`Click the links to switch pages and update breadcrumb states:`}),(0,b.jsx)(r,{children:(0,b.jsx)(v,{slot:`router-item`,to:`/`,children:`Home`})}),(0,b.jsx)(r,{children:(0,b.jsx)(v,{slot:`router-item`,to:`/daily-tippers`,children:`Daily Tippers`})}),(0,b.jsx)(r,{children:(0,b.jsx)(v,{slot:`router-item`,to:`/about`,children:`About`})})]})]}),name:`React Router`,decorators:[e=>(0,b.jsx)(m,{initialEntries:[`/`],children:(0,b.jsx)(e,{})})]},N={render:e=>(0,b.jsxs)(o,{backBreadcrumbOnly:e.backBreadcrumbOnly,collapsed:e.collapsed,theme:e.theme,monochrome:e.monochrome,children:[(0,b.jsx)(i,{current:e.current,pageTitle:e.pageTitle,href:e.href,children:(0,b.jsx)(`svg`,{slot:e.iconSlot,width:`24`,height:`24`,viewBox:`0 0 24 24`,fill:`none`,xmlns:`http://www.w3.org/2000/svg`,children:(0,b.jsx)(`path`,{d:`M12 6.19L17 10.69V18.5H15V12.5H9V18.5H7V10.69L12 6.19ZM12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L12 3.5Z`,fill:`currentColor`})})}),(0,b.jsx)(i,{pageTitle:`Breadcrumb 2`,href:`/breadcrumb-2`}),(0,b.jsx)(i,{pageTitle:`Breadcrumb 3`,href:`/breadcrumb-3`})]}),args:T,argTypes:{iconSlot:{mapping:{true:`icon`,false:``}},theme:{options:[`inherit`,`light`,`dark`],control:{type:`inline-radio`}}},name:`Playground`},D.parameters={...D.parameters,docs:{...D.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <IcBreadcrumbGroup>
+        <IcBreadcrumb pageTitle="foo" href="/foo" />
+        <IcBreadcrumb pageTitle="bar" href="/bar" />
+        <IcBreadcrumb pageTitle="baz" href="/baz" current />
+      </IcBreadcrumbGroup>
+    </div>,
+  name: "Default"
+}`,...D.parameters?.docs?.source}}},O.parameters={...O.parameters,docs:{...O.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <IcBreadcrumbGroup>
+        <IcBreadcrumb>
+          <IcLink>
+            <a slot="router-item" href="/foo">
+              Foo
+            </a>
+          </IcLink>
+        </IcBreadcrumb>
+        <IcBreadcrumb>
+          <IcLink>
+            <a slot="router-item" href="/bar">
+              Bar
+            </a>
+          </IcLink>
+        </IcBreadcrumb>
+        <IcBreadcrumb current>
+          <IcLink>
+            <a slot="router-item" href="/baz">
+              Baz
+            </a>
+          </IcLink>
+        </IcBreadcrumb>
+      </IcBreadcrumbGroup>
+    </div>,
+  name: "Slotted links"
+}`,...O.parameters?.docs?.source}}},k.parameters={...k.parameters,docs:{...k.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <IcBreadcrumbGroup backBreadcrumbOnly>
+        <IcBreadcrumb pageTitle="foo" href="/foo" />
+        <IcBreadcrumb pageTitle="bar" href="/bar" />
+        <IcBreadcrumb pageTitle="baz" href="/baz" current />
+      </IcBreadcrumbGroup>
+    </div>,
+  name: "Back"
+}`,...k.parameters?.docs?.source}}},A.parameters={...A.parameters,docs:{...A.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <IcBreadcrumbGroup>
+        <IcBreadcrumb pageTitle="foo" href="/foo">
+          <SlottedSVG slot="icon" height="24px" viewBox="0 0 24 24" width="24px">
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z" />
+          </SlottedSVG>
+        </IcBreadcrumb>
+        <IcBreadcrumb pageTitle="bar" href="/bar">
+          <SlottedSVG slot="icon" height="24px" viewBox="0 0 24 24" width="24px">
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z" />
+          </SlottedSVG>
+        </IcBreadcrumb>
+        <IcBreadcrumb pageTitle="baz" href="/baz" current>
+          <SlottedSVG slot="icon" height="24px" viewBox="0 0 24 24" width="24px">
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z" />
+          </SlottedSVG>
+        </IcBreadcrumb>
+      </IcBreadcrumbGroup>
+    </div>,
+  name: "Icon"
+}`,...A.parameters?.docs?.source}}},j.parameters={...j.parameters,docs:{...j.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <IcBreadcrumbGroup collapsed>
+        <IcBreadcrumb pageTitle="foo" href="/foo" />
+        <IcBreadcrumb pageTitle="bar" href="/bar" />
+        <IcBreadcrumb pageTitle="baz" href="/baz" />
+        <IcBreadcrumb pageTitle="qux" href="/qux" />
+        <IcBreadcrumb pageTitle="quxx" href="/quxx" current />
+      </IcBreadcrumbGroup>
+    </div>,
+  name: "Collapsed"
+}`,...j.parameters?.docs?.source}}},M.parameters={...M.parameters,docs:{...M.parameters?.docs,source:{originalSource:`{
+  render: () => <div>
+      <Breadcrumbs />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/daily-tippers" element={<DailyTippers />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <div style={{
+      display: "flex",
+      gap: "10px",
+      marginTop: "16px"
+    }}>
+        <IcTypography>
+          Click the links to switch pages and update breadcrumb states:
+        </IcTypography>
+        <IcLink>
+          <Link slot="router-item" to="/">
+            Home
+          </Link>
+        </IcLink>
+        <IcLink>
+          <Link slot="router-item" to="/daily-tippers">
+            Daily Tippers
+          </Link>
+        </IcLink>
+        <IcLink>
+          <Link slot="router-item" to="/about">
+            About
+          </Link>
+        </IcLink>
+      </div>
+    </div>,
+  name: "React Router",
+  decorators: [Story => <MemoryRouter initialEntries={["/"]}>
+        <Story />
+      </MemoryRouter>]
+}`,...M.parameters?.docs?.source}}},N.parameters={...N.parameters,docs:{...N.parameters?.docs,source:{originalSource:`{
+  render: args => <IcBreadcrumbGroup backBreadcrumbOnly={args.backBreadcrumbOnly} collapsed={args.collapsed} theme={args.theme} monochrome={args.monochrome}>
+      <IcBreadcrumb current={args.current} pageTitle={args.pageTitle} href={args.href}>
+        <svg slot={args.iconSlot} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 6.19L17 10.69V18.5H15V12.5H9V18.5H7V10.69L12 6.19ZM12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L12 3.5Z" fill="currentColor"></path>
+        </svg>
+      </IcBreadcrumb>
+      <IcBreadcrumb pageTitle="Breadcrumb 2" href="/breadcrumb-2" />
+      <IcBreadcrumb pageTitle="Breadcrumb 3" href="/breadcrumb-3" />
+    </IcBreadcrumbGroup>,
+  args: defaultArgs,
+  argTypes: {
+    iconSlot: {
+      mapping: {
+        true: "icon",
+        false: ""
+      }
+    },
+    theme: {
+      options: ["inherit", "light", "dark"],
+      control: {
+        type: "inline-radio"
+      }
+    }
+  },
+  name: "Playground"
+}`,...N.parameters?.docs?.source}}},P=[`Default`,`SlottedLinks`,`Back`,`Icon`,`Collapsed`,`ReactRouter`,`Playground`]}));F();export{k as Back,j as Collapsed,D as Default,A as Icon,N as Playground,M as ReactRouter,O as SlottedLinks,P as __namedExportsOrder,E as default,F as n,y as t};
